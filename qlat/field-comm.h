3480c3d65c5c9a7223c7c977100edb826dd93316
Jiqun Tu <jt2798@columbia.edu>
2016-08-28 19:35:25 -0400

    change the field-comm.h

--- qlat/field-comm.h
+++ qlat/field-comm.h
@@ -111,4 +111,125 @@ void fetch_expanded(Field<M> &field_comm){
 	}
 }
 
+class Chart: public std::map<Coordinate, std::vector<Coordinate> > {
+public:
+	Coordinate ExpandLeft, ExpandRight;
+};
+
+// typedef std::map<Coordinate, std::vector<Coordinate> > Chart;
+
+void produce_chart(Chart &chart, const Geometry geometry, const Coordinate &local){
+	
+	Coordinate pos; // coordinate position of a site relative to this node
+	Coordinate local_pos; // coordinate position of a site relative to its home node
+	Coordinate node_pos; // home node coordinate of a site in node space
+
+	long record_size = geometry.localVolumeExpanded();
+	for(long record = 0; record < record_size; record++){
+		geometry.coordinateFromRecord(pos, record);
+		if(geometry.isLocal(pos)) continue;
+		for(int mu = 0; mu < DIM; mu++){
+			local_pos[mu] = pos[mu] % geometry.nodeSite[mu];
+			node_pos[mu] = pos[mu] / geometry.nodeSite[mu];
+			if(local_pos[mu] < 0){
+				local_pos[mu] += geometry.nodeSite[mu];
+				node_pos[mu]--;
+			}
+		}
+		chart[node_pos].push_back(local_pos);
+	}
+}
+
+template <class M>
+void fetch_expanded_chart(Field<M> &field_comm, const Chart &send_chart){
+
+// std::map<Coordinate, std::vector<Coordinate> >
+//           node_pos          local_pos
+//        it_chart->first  it_chart->second
+//                                 ^
+//                                 |
+//                              it_coor
+
+	TIMER("fetch_expanded_chart");
+
+	std::map<Coordinate, std::vector<M> > send_map;
+	std::map<Coordinate, int> send_map_consume;
+
+	Coordinate node_pos; // home node coordinate of a site in node space
+
+// populate send_map with the data that we need to send to other nodes
+	Chart::const_iterator it_chart;
+	std::vector<Coordinate>::const_iterator it_coor;
+	for(it_chart = send_chart.begin(); it_chart != send_chart.end(); it_chart++){
+		node_pos = it_chart->first;
+		std::vector<M> &vec = send_map[node_pos];
+		vec.clear();
+		for(it_coor = it_chart->second.begin(); 
+					it_coor != it_chart->second.end(); it_coor++){
+			for(int mu = 0; mu < field_comm.geo.multiplicity; mu++){
+				vec.push_back(field_comm.getElemsConst(*it_coor)[mu]);
+			}
+		}
+	}
+
+	std::vector<M> recv_vec;
+// will store data received from other nodes
+// Iterate over all the nodes to which we need to send data.
+// We ultimately copy the received data into the corresponding
+// value of sendmap.
+	typename std::map<Coordinate, std::vector<M> >::iterator it;
+	for(it = send_map.begin(); it != send_map.end(); it++){
+		node_pos = it->first;
+		std::vector<M> &send_vec = it->second;
+		long size = send_vec.size();
+		size_t size_bytes = size * sizeof(M);
+		recv_vec.resize(std::max((long)2500, size));
+
+		M *send = send_vec.data();
+		M *recv = recv_vec.data();
+
+		Coordinate coor_this, coort, coorf;
+		int id_this, idt, idf;
+		// assuming periodic boundary condition. maybe need some fixing?
+		id_this = getIdNode();
+		qlat::coordinateFromIndex(coor_this, id_this, \
+			field_comm.geo.geon.sizeNode);
+		coort = coor_this - node_pos; 
+		regularize(coort, field_comm.geo.geon.sizeNode);
+		coorf = coor_this + node_pos;
+		regularize(coorf, field_comm.geo.geon.sizeNode);
+		
+		idt = qlat::indexFromCoordinate(coort, field_comm.geo.geon.sizeNode);
+		idf = qlat::indexFromCoordinate(coorf, field_comm.geo.geon.sizeNode);
+			
+		MPI_Request req;
+		MPI_Isend((void*)send, size_bytes, MPI_BYTE, idt, 0, getComm(), &req);
+		const int ret = MPI_Recv((void*)recv, size_bytes, MPI_BYTE, \
+			idf, 0, getComm(), MPI_STATUS_IGNORE);
+		MPI_Wait(&req, MPI_STATUS_IGNORE);
+		assert(!ret);
+
+		memcpy(send, recv, size_bytes);
+
+		send_map_consume[node_pos] = 0;
+	}
+
+// Now send_map[node_pos] is the vector of data recieved from the node
+// pointed to by key.
+	for(it_chart = send_chart.begin(); it_chart != send_chart.end(); it_chart++){
+		node_pos = it_chart->first;
+		int consume = send_map_consume[node_pos];
+		std::vector<M> &vec = send_map[node_pos];
+		for(it_coor = it_chart->second.begin(); 
+					it_coor != it_chart->second.end(); it_coor++){
+			for(int mu = 0; mu < field_comm.geo.multiplicity; mu++){
+				field_comm.getElems(*it_coor)[mu] = vec[consume];
+				consume++;
+			}
+			send_map_consume[node_pos] = consume;
+		}
+	}
+
+}
+
 QLAT_END_NAMESPACE
