d1dc00e4241b38aa6b5b6cb2228433b062fb57c9
Luchang Jin <ljin.luchang@gmail.com>
2016-10-16 17:08:26 -0400

    update rng, change rng outputs

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -8,6 +8,7 @@ echo "!!!! build $name !!!!"
 mkdir -p $prefix/include
 cp -v $distfiles/timer.h $prefix/include/
 cp -v $distfiles/show.h $prefix/include/
+cp -v $distfiles/sha256.h $prefix/include/
 cp -v $distfiles/rng-state.h $prefix/include/
 cp -v $distfiles/sprng-sha256.h $prefix/include/
 
