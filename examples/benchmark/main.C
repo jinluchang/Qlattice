3f72630490d79aed28256fdbec5600dbf08b2a82
Luchang Jin <ljin.luchang@gmail.com>
2016-10-18 14:35:21 -0400

    add benchmark example

---
+++ examples/benchmark/main.C
@@ -0,0 +1,32 @@
+#include <qlat/qlat.h>
+
+#include <iostream>
+#include <complex>
+#include <vector>
+
+using namespace qlat;
+using namespace std;
+
+void test1()
+{
+  TIMER("test1");
+  const size_t size = 2 * 1024 * 1024;
+  std::vector<double> dataSend(size), dataRecv(size);
+  for (int i = 0; i < 128 * 8; ++i) {
+    TIMER_VERBOSE_FLOPS("getData"); // transferred bi-direction added in unit of Bytes
+    timer.flops += size * sizeof(double) * 2 * getNumNode() * 4; // 2: two direction, 4: four transfers
+    getDataDir(Vector<double>(dataRecv), Vector<double>(dataSend), 0);
+    getDataDir(Vector<double>(dataSend), Vector<double>(dataRecv), 0);
+    getDataDir(Vector<double>(dataRecv), Vector<double>(dataSend), 0);
+    getDataDir(Vector<double>(dataSend), Vector<double>(dataRecv), 0);
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  begin(&argc, &argv);
+  test1();
+  Timer::display();
+  end();
+  return 0;
+}
