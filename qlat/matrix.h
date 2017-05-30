76807f2243b96a72ccbe8f51c08d10c1b47fba19
HummingTree <ducknade@gmail.com>
2017-05-30 13:38:21 -0400

    add sigmas

--- qlat/matrix.h
+++ qlat/matrix.h
@@ -395,6 +395,12 @@ struct SpinMatrixConstants
   {
     return get_instance().cap_sigmas;
   }
+  static const SpinMatrix& get_cps_sigmas(int i, int j)
+  {
+  	SpinMatrixConstants smc = get_instance();
+	return (smc.cps_gammas[i]*smc.cps_gammas[j]-smc.cps_gammas[j]*smc.cps_gammas[i])/2.;
+  }
+
 };
 
 inline WilsonMatrix operator*(const ColorMatrix& cm, const WilsonMatrix& m)
