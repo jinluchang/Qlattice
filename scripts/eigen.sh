dbf7fbd58ee3d759a070de1d3996307623711e4d
Luchang Jin <ljin.luchang@gmail.com>
2018-01-11 01:58:20 -0500

    update eigen install script

--- scripts/eigen.sh
+++ scripts/eigen.sh
@@ -13,11 +13,15 @@ tar xaf $distfiles/$name-*
 rm -rf $build_dir
 mkdir -p $build_dir
 cd $build_dir
-cmake \
- -DCMAKE_INSTALL_PREFIX=$prefix \
- $src_dir/$name-*
-make -j$num_proc
-make install
+# cmake \
+#  -DCMAKE_INSTALL_PREFIX=$prefix \
+#  $src_dir/$name-*
+# make -j$num_proc
+# make install
+
+rm -rf $prefix/include/eigen3
+mkdir -p $prefix/include/eigen3
+rsync -av $src_dir/$name-*/{Eigen,signature_of_eigen3_matrix_library,unsupported} $prefix/include/eigen3
 
 cd $wd
 
