68a40fd5d280801b0e94e37d11cd14655e93c01a
Luchang Jin <ljin.luchang@gmail.com>
2019-02-04 12:17:06 -0500

    change qlat.h

--- qlat/qlat.h
+++ qlat/qlat.h
@@ -1,16 +1,27 @@
 #pragma once
 
 #include <qlat/config.h>
+
 #include <qlat/utils.h>
+
 #include <qlat/utils-coordinate.h>
+
 #include <qlat/cache.h>
+
 #include <qlat/crc32.h>
+
 #include <qlat/mvector.h>
+
 #include <qlat/matrix.h>
+
 #include <qlat/mpi.h>
+
 #include <qlat/utils-io.h>
+
 #include <qlat/latio.h>
+
 #include <qlat/field.h>
+
 #include <qlat/field-utils.h>
 
 #ifndef QLAT_FFTW_OFF
@@ -18,9 +29,13 @@
 #endif
 
 #include <qlat/field-rng.h>
+
 #include <qlat/field-comm.h>
+
 #include <qlat/field-serial-io.h>
+
 #include <qlat/field-dist-io.h>
+
 #include <qlat/field-expand.h>
 
 #ifndef QLAT_FFTW_OFF
@@ -36,16 +51,25 @@
 #include <qlat/qcd-utils.h>
 
 #ifndef QLAT_FFTW_OFF
+
 #include <qlat/qcd-gauge-transformation.h>
+
 #include <qlat/qcd-gauge-transformation-boundary.h>
+
 #include <qlat/qcd-smear.h>
+
 #include <qlat/qcd-topology.h>
+
 #include <qlat/fermion-action.h>
+
 #endif
 
 #ifndef QLAT_FFTW_OFF
+
 #include <qlat/compressed-eigen-io.h>
+
 #include <qlat/dslash.h>
+
 #endif
 
 
