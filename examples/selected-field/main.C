1347f163300bca5dd0a8b1bbf4711cde8ce35352
Luchang Jin <ljin.luchang@gmail.com>
2019-08-14 17:36:59 -0400

    create demo for selected field

--- examples/selected-field/main.C
+++ examples/selected-field/main.C
@@ -2,6 +2,110 @@
 
 using namespace qlat;
 
+inline void demo()
+{
+  TIMER_VERBOSE("demo");
+  const Coordinate total_site(4,4,4,8);
+  Geometry geo;
+  geo.init(total_site, 1);
+  qmkdir_info("huge-data");
+  //
+  displayln_info(fname + ssprintf(": create splittable random number generator 'rs'"));
+  RngState rs = RngState("selected-field");
+  //
+  displayln_info(fname + ssprintf(": init field 'f'"));
+  Field<Complex> f;
+  f.init(geo, 2);
+  set_zero(f);
+  //
+  displayln_info(fname + ssprintf(": compute crc32=%06X.", field_crc32(f)));
+  //
+  displayln_info(fname + ssprintf(": fill with random numbers"));
+  set_u_rand_double(f, rs.split("f-init"));
+  //
+  displayln_info(fname + ssprintf(": compute crc32=%06X.", field_crc32(f)));
+  //
+  displayln_info(fname + ssprintf(": save to disk"));
+  write_field(f, "huge-data/f.field");
+  //
+  displayln_info(fname + ssprintf(": clear field 'f' in memory"));
+  set_zero(f);
+  //
+  displayln_info(fname + ssprintf(": compute crc32=%06X.", field_crc32(f)));
+  //
+  displayln_info(fname + ssprintf(": read from disk"));
+  read_field(f, "huge-data/f.field");
+  //
+  displayln_info(fname + ssprintf(": compute crc32=%06X.", field_crc32(f)));
+  //
+  const long n_per_tslice = 16;
+  displayln_info(fname + ssprintf(": random select points on each time slice"));
+  displayln_info(fname + ssprintf(": select %d points per time slice", n_per_tslice));
+  displayln_info(fname + ssprintf(": selection is stored in 'fsel' of type 'FieldSelection'"));
+  FieldSelection fsel;
+  set_field_selection(fsel, total_site, n_per_tslice, rs.split("free-4nt8").split(0));
+  //
+  displayln_info(fname + ssprintf(": save selection 'fsel' to disk as a field"));
+  write_field_selection(fsel, "huge-data/fsel.field");
+  //
+  fsel.init();
+  displayln_info(fname + ssprintf(": read selection 'fsel' from disk as a field"));
+  read_field_selection(fsel, "huge-data/fsel.field", n_per_tslice);
+  //
+  displayln_info(fname + ssprintf(": possible to only keep selected points non-zero for field 'f'."));
+  displayln_info(fname + ssprintf(": (DOES NOT CHANGE THE SIZE IN MOMEORY)"));
+  only_keep_selected_points(f, fsel);
+  //
+  displayln_info(fname + ssprintf(": compute crc32=%06X.", field_crc32(f)));
+  //
+  displayln_info(fname + ssprintf(": declare 'sf' or type 'SelectedField<Complex>'"));
+  SelectedField<Complex> sf;
+  //
+  displayln_info(fname + ssprintf(": 'sf' only store the selected points' data of 'f'"));
+  set_selected_field(sf, f, fsel);
+  //
+  displayln_info(fname + ssprintf(": clear field 'f' in memory"));
+  set_zero(f);
+  //
+  displayln_info(fname + ssprintf(": compute crc32=%06X.", field_crc32(f)));
+  //
+  displayln_info(fname + ssprintf(": restore the selected points data to 'f' from 'sf'"));
+  set_field_selected(f, sf, fsel);
+  //
+  displayln_info(fname + ssprintf(": compute crc32=%06X.", field_crc32(f)));
+  //
+  displayln_info(fname + ssprintf(": only save the selected points data of 'f' to disk"));
+  displayln_info(fname + ssprintf(": (NOTE THAT 'f', NOT 'sf', IS USED HERE)"));
+  write_selected_field(f, "huge-data/f.sfield", fsel);
+  //
+  displayln_info(fname + ssprintf(": clear field 'f' in memory"));
+  set_zero(f);
+  //
+  displayln_info(fname + ssprintf(": compute crc32=%06X.", field_crc32(f)));
+  //
+  displayln_info(fname + ssprintf(": only read the selected points data of 'f' from disk"));
+  displayln_info(fname + ssprintf(": (NOTE THAT 'f', NOT 'sf', IS USED HERE)"));
+  read_selected_field(f, "huge-data/f.sfield", fsel);
+  //
+  displayln_info(fname + ssprintf(": compute crc32=%06X.", field_crc32(f)));
+  //
+  displayln_info(fname + ssprintf(": compute the sum of all the selected points in 'f'"));
+  Complex sum = 0.0;
+  for (long index = 0; index < f.geo.local_volume(); ++index) {
+    const long idx = fsel.f_local_idx.get_elems_const(index)[0];
+    if (idx < 0) {
+      continue;
+    }
+    const Coordinate xl = f.geo.coordinate_from_index(index);
+    const Vector<Complex> fv = f.get_elems_const(xl);
+    // f.geto.multiplicity = 2 is the number of elements per site
+    for (int m = 0; m < f.geo.multiplicity; ++m) {
+      sum += fv[m];
+    }
+  }
+  displayln_info(fname + ssprintf(": sum is %s", show(sum).c_str()));
+}
+
 inline void test(const std::string& tag, const long n_per_tslice)
 {
   TIMER_VERBOSE("test");
@@ -71,6 +175,7 @@ inline void test(const std::string& tag, const long n_per_tslice)
 int main(int argc, char* argv[])
 {
   begin(&argc, &argv);
+  demo();
   test("16", 16);
   test("all", -1);
   Timer::display();
