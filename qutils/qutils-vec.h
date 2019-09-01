090aa51db4593a0a796762cf690e4b18d4cc83de
Luchang Jin <ljin.luchang@gmail.com>
2019-09-01 00:42:05 -0400

    shuffle code around and update log

--- qutils/qutils-vec.h
+++ qutils/qutils-vec.h
@@ -390,6 +390,199 @@ void to_from_big_endian_64(Vector<M> v)
   to_from_big_endian_64((void*)v.data(), v.data_size());
 }
 
+inline void from_big_endian_32(char* str, const size_t len)
+// obsolete
+{
+  to_from_big_endian_32(str, len);
+}
+
+inline void from_big_endian_64(char* str, const size_t len)
+// obsolete
+{
+  to_from_big_endian_64(str, len);
+}
+
+template <class M, int N>
+void assign(std::array<M, N>& vec, const Array<M, N>& src)
+{
+  memcpy(vec.data(), src.data(), src.data_size());
+}
+
+template <class M, int N>
+void assign(std::array<M, N>& vec, const Vector<M>& src)
+{
+  qassert(N == src.size());
+  memcpy(vec.data(), src.data(), src.data_size());
+}
+
+template <class M, int N>
+void assign(std::vector<M>& vec, const Array<M, N>& src)
+{
+  vec.resize(src.size());
+  memcpy(vec.data(), src.data(), src.data_size());
+}
+
+template <class M>
+void assign(std::vector<M>& vec, const Vector<M>& src)
+{
+  vec.resize(src.size());
+  memcpy(vec.data(), src.data(), src.data_size());
+}
+
+template <class M>
+void assign(Vector<M> vec, const Vector<M>& src)
+{
+  qassert(vec.size() == src.size());
+  memcpy(vec.data(), src.data(), src.data_size());
+}
+
+template <class M, int N>
+void assign(Vector<M> vec, const Array<M, N>& src)
+{
+  qassert(vec.size() == N);
+  memcpy(vec.data(), src.data(), src.data_size());
+}
+
+template <class M, int N>
+void assign(Array<M, N> vec, const Array<M, N>& src)
+{
+  memcpy(vec.data(), src.data(), src.data_size());
+}
+
+template <class M, int N>
+void assign(Array<M, N> vec, const Vector<M>& src)
+{
+  qassert(src.size() == N);
+  memcpy(vec.data(), src.data(), src.data_size());
+}
+
+template <class M, class N>
+void assign_truncate(M& x, const N& y)
+{
+  if (sizeof(M) <= sizeof(N)) {
+    std::memcpy((void*)&x, (void*)&y, sizeof(M));
+  } else {
+    // if M has a larger size, than leave the extra space untouched
+    std::memcpy((void*)&x, (void*)&y, sizeof(N));
+  }
+}
+
+inline bool is_integer(const double& x)
+{
+  const double diff = x - (long)x;
+  return 1e-6 > diff || diff > 1 - 1e-6;
+}
+
+template <class M>
+inline bool is_integer(const std::vector<M>& v)
+{
+  for (int i = 0; i < (int)v.size(); ++i) {
+    if (!is_integer(v[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+template <class M, unsigned long N>
+inline bool is_integer(const std::array<M, N>& v)
+{
+  for (int i = 0; i < N; ++i) {
+    if (!is_integer(v[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+template <class M, int N>
+Array<M, N> operator+=(Array<M, N> v, const Array<M, N> v1)
+{
+  for (int i = 0; i < N; ++i) {
+    v.p[i] += v1.p[i];
+  }
+  return v;
+}
+
+template <class M, int N>
+Array<M, N> operator-=(Array<M, N> v, const Array<M, N> v1)
+{
+  for (int i = 0; i < N; ++i) {
+    v.p[i] -= v1.p[i];
+  }
+  return v;
+}
+
+template <class M, int N>
+Array<M, N> operator*=(Array<M, N> v, const double factor)
+{
+  for (int i = 0; i < N; ++i) {
+    v.p[i] *= factor;
+  }
+  return v;
+}
+
+template <class M, int N>
+Array<M, N> operator*=(Array<M, N> v, const Complex factor)
+{
+  for (int i = 0; i < N; ++i) {
+    v.p[i] *= factor;
+  }
+  return v;
+}
+
+template <class M>
+Vector<M> operator+=(Vector<M> v, const Vector<M> v1)
+{
+  qassert(v.size() == v1.size());
+  for (long i = 0; i < v.size(); ++i) {
+    v.p[i] += v1.p[i];
+  }
+  return v;
+}
+
+template <class M>
+Vector<M> operator-=(Vector<M> v, const Vector<M> v1)
+{
+  qassert(v.size() == v1.size());
+  for (long i = 0; i < v.size(); ++i) {
+    v.p[i] -= v1.p[i];
+  }
+  return v;
+}
+
+template <class M>
+Vector<M> operator*=(Vector<M> v, const double factor)
+{
+  for (long i = 0; i < v.size(); ++i) {
+    v.p[i] *= factor;
+  }
+  return v;
+}
+
+template <class M>
+Vector<M> operator*=(Vector<M> v, const Complex factor)
+{
+  for (long i = 0; i < v.size(); ++i) {
+    v.p[i] *= factor;
+  }
+  return v;
+}
+
+template <class M>
+inline void random_permute(std::vector<M>& vec, const RngState& rs_)
+{
+  RngState rs = rs_;
+  const long size = (long)vec.size();
+  M tmp;
+  for (long k = 0; k < size; ++k) {
+    const long kk = rand_gen(rs) % (size - k);
+    tmp = vec[k];
+    vec[k] = vec[k + kk];
+    vec[k + kk] = tmp;
+  }
+}
+
 template <class M>
 void set_u_rand_double(Vector<M> v, const RngState& rs, const double upper = 1.0,
                       const double lower = -1.0)
