f5400ac1ecbd312f6ff7ad6395cb8e4bb372270f
Luchang Jin <ljin.luchang@gmail.com>
2020-11-13 16:26:52 -0500

    add metropolis

--- qlat/hmc.h
+++ qlat/hmc.h
@@ -9,6 +9,30 @@
 namespace qlat
 {  //
 
-
+inline bool metropolis_accept(double& accept_prob, const double delta_h,
+                              const RngState& rs_)
+// only compute at get_id_node() == 0
+// broad_cast the result to all nodes
+{
+  TIMER_VERBOSE("metropolis_accept");
+  double flag = 0.0;
+  accept_prob = 0;
+  if (get_id_node() == 0) {
+    if (delta_h <= 0.0) {
+      flag = 1.0;
+      accept_prob = 1.0;
+    } else {
+      RngState rs = rs_;
+      const double rand_num = u_rand_gen(rs, 0.0, 1.0);
+      accept_prob = std::exp(-delta_h);
+      if (rand_num <= accept_prob) {
+        flag = 1.0;
+      }
+    }
+  }
+  bcast(get_data_one_elem(accept_prob));
+  bcast(get_data_one_elem(flag));
+  return flag > 0.5;
+}
 
 }  // namespace qlat
