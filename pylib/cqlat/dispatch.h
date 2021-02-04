94b361cc8c943c978d156f1019274e8d2e00ea05
Luchang Jin <ljin.luchang@gmail.com>
2021-02-04 00:03:27 -0500

    small

--- pylib/cqlat/dispatch.h
+++ pylib/cqlat/dispatch.h
@@ -3,15 +3,17 @@
 #define FIELD_DISPATCH(p_ret, fname, ctype, ...)                            \
   {                                                                         \
     if ("ColorMatrix" == (ctype)) {                                         \
-      (p_ret) = fname<ColorMatrix>(__VA_ARGS__);                  \
+      (p_ret) = fname<ColorMatrix>(__VA_ARGS__);                            \
     } else if ("WilsonMatrix" == (ctype)) {                                 \
-      (p_ret) = fname<WilsonMatrix>(__VA_ARGS__);                 \
+      (p_ret) = fname<WilsonMatrix>(__VA_ARGS__);                           \
     } else if ("WilsonVector" == (ctype)) {                                 \
-      (p_ret) = fname<WilsonVector>(__VA_ARGS__);                 \
+      (p_ret) = fname<WilsonVector>(__VA_ARGS__);                           \
     } else if ("double" == (ctype)) {                                       \
-      (p_ret) = fname<double>(__VA_ARGS__);                       \
+      (p_ret) = fname<double>(__VA_ARGS__);                                 \
     } else if ("Complex" == (ctype)) {                                      \
-      (p_ret) = fname<Complex>(__VA_ARGS__);                      \
+      (p_ret) = fname<Complex>(__VA_ARGS__);                                \
+    } else if ("AdjointColorMatrix" == (ctype)) {                           \
+      (p_ret) = fname<AdjointColorMatrix>(__VA_ARGS__);                     \
     } else {                                                                \
       pqerr("%s %s='%s' does not exist.", #fname, #ctype, (ctype).c_str()); \
       (p_ret) = NULL;                                                       \
