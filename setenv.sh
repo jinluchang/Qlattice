07860c162d446579d99c18975afd00d0d583bdaa
Luchang Jin <ljin.luchang@gmail.com>
2021-02-11 03:04:51 -0500

    small update

--- setenv.sh
+++ setenv.sh
@@ -2,18 +2,11 @@ if [ -z "$prefix" ] ; then
     prefix=$HOME/qlat-build/1.0
 fi
 
-if [ "$(uname -m)" = ppc64 ] ; then
-    arch=bgq
-elif [ "$(uname -m)" = x86_64 ] ; then
+if [ "$(uname -m)" = x86_64 ] ; then
     arch=amd64
-elif [ "$(uname -m)" = alpha ] ; then
-    arch=taihu
 else
     arch=unknown
 fi
-
-export HOME=~
-export LANG=en_US.UTF-8
 
 add-to-colon-list () {
     local name="$1"
@@ -33,30 +26,5 @@ add-to-colon-list () {
     fi
 }
 
-if [ $arch = bgq ] ; then
-    add-to-colon-list PATH "/bgsys/drivers/ppcfloor/gnu-linux/bin"
-    add-to-colon-list PATH "/bgsys/drivers/ppcfloor/comm/gcc/bin"
-fi
-
-add-to-colon-list PATH "$prefix/bin"
 add-to-colon-list PYTHONPATH "$prefix/pylib"
-add-to-colon-list LD_LIBRARY_PATH "$prefix/lib"
-add-to-colon-list LD_LIBRARY_PATH "$prefix/lib64"
-add-to-colon-list LIBRARY_PATH "$prefix/lib"
-add-to-colon-list LIBRARY_PATH "$prefix/lib64"
-add-to-colon-list C_INCLUDE_PATH "$prefix/include/ncurses"
-add-to-colon-list C_INCLUDE_PATH "$prefix/include"
-add-to-colon-list CPLUS_INCLUDE_PATH "$prefix/include/ncurses"
-add-to-colon-list CPLUS_INCLUDE_PATH "$prefix/include"
-add-to-colon-list PKG_CONFIG_PATH "$prefix/lib/pkgconfig"
-
-export MANPATH="$(man --path)"
-add-to-colon-list MANPATH "$prefix/share/man"
-
-if [ -z "$TEXINPUTS" ] ; then
-    export TEXINPUTS=".:"
-fi
-add-to-colon-list TEXINPUTS "$prefix/share/texmf/tex/latex/local//"
-add-to-colon-list TEXINPUTS "."
-export TEXINPUTS="$TEXINPUTS":
 
