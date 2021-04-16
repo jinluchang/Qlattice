3c87674ac41e919056385fa22f990480cd2792ac
Luchang Jin <ljin.luchang@gmail.com>
2021-04-16 02:14:51 -0400

    update LICENSE

--- scripts/download.sh
+++ scripts/download.sh
@@ -12,4 +12,10 @@ wget -c "https://zlib.net/zlib-1.2.11.tar.gz"
 
 wget -c "https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.bz2"
 
+wget -O c-lime.tar.gz -c "https://github.com/usqcd-software/c-lime/tarball/master"
+
+wget -O Grid.tar.gz -c "https://github.com/waterret/Grid/tarball/feature/gpt"
+
+wget -O gpt.tar.gz -c "https://github.com/waterret/gpt/tarball/master"
+
 sha256sum *.tar.* > sha256sums.txt
