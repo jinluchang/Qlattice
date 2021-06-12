7b92b6f40490941cf8849a8197ce85fbeb2cc066
Luchang Jin <ljin.luchang@gmail.com>
2021-06-12 18:23:57 -0400

    extend cqlat instead of adding new .so

--- pylib/apps/contract-wall-point/qlat_ext.py
+++ pylib/apps/contract-wall-point/qlat_ext.py
@@ -1,2 +1,15 @@
 from qlat import *
-from qcontract import *
+
+from cqlat import \
+        setup, \
+        setup_log_idx, \
+        clear_all_data_cache, \
+        set_data_path, \
+        compute_chvp, \
+        compute_meson_chvp, \
+        compute_meson_snk_src, \
+        compute_meson_vv, \
+        compute_meson_vv_meson, \
+        compute_psel_fsel_distribution, \
+        compute_three_point_func, \
+        compute_two_point_func
