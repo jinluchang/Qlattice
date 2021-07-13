bc1250f4395fca195f4029727f237f2b43e99902
Luchang Jin <ljin.luchang@gmail.com>
2021-07-13 17:13:07 -0400

    add more basis_size in gpt

--- scripts/gpt.sh
+++ scripts/gpt.sh
@@ -14,6 +14,19 @@ tar xaf $distfiles/gpt.tar.gz
 
 mv waterret-gpt-* gpt
 cd gpt/lib/cgpt
+
+echo "BASIS_SIZE(4)" > lib/basis_size.h
+echo "BASIS_SIZE(10)" >> lib/basis_size.h
+echo "BASIS_SIZE(30)" >> lib/basis_size.h
+echo "BASIS_SIZE(100)" >> lib/basis_size.h
+echo "BASIS_SIZE(500)" >> lib/basis_size.h
+
+echo "SPIN(4)" > lib/spin_color.h
+echo "COLOR(3)" >> lib/spin_color.h
+echo "SPIN_COLOR(4,3)" >> lib/spin_color.h
+
+./clean
+
 ./make "$prefix"/Grid/build "$num_proc"
 
 cd $wd
