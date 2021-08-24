0caab3551885f4ba8b7168e5e1ad36b5b1935669
Luchang Jin <ljin.luchang@gmail.com>
2021-08-24 13:56:00 -0400

    improve build

--- conf.sh
+++ conf.sh
@@ -6,10 +6,10 @@ if [ -z $num_proc ] ; then
     num_proc=2
 fi
 
-wd=$(pwd)
-distfiles=$wd/distfiles
-temp_dir=$wd/temp
-src_dir=$temp_dir/src
-build_dir=$temp_dir/build
+wd="$(pwd)"
+distfiles="$wd/distfiles"
+temp_dir="$prefix/temp"
+src_dir="$temp_dir/src"
+build_dir="$temp_dir/build"
 
 rm -rf $temp_dir || true
