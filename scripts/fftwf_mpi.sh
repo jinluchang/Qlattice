1c2f8b262128ee34255ebe39da4c8f0d3216f10c
Gen Wang <26831146+aroadant@users.noreply.github.com>
2021-09-08 21:46:03 -0700

    scripts for fftw mpi compile

---
+++ scripts/fftwf_mpi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+. conf.sh
+
+name=fftw
+
+echo "!!!! build $name !!!!"
+
+rm -rf $src_dir || true
+mkdir -p $src_dir || true
+cd $src_dir
+tar xzf $distfiles/$name-*.tar.gz
+
+export CC="gcc -fPIC"
+
+cd $name-*
+./configure \
+    --prefix=$prefix \
+    --enable-float \
+    --enable-mpi \
+#    MPICC=cc
+#     --enable-openmp
+
+make -j$num_proc
+make install
+
+cd $wd
+echo "!!!! $name build !!!!"
+
+rm -rf $temp_dir
