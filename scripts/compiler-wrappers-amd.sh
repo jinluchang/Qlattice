f735a75c76ff652e054db07568b6444e4aedbd27
Luchang Jin <ljin.luchang@gmail.com>
2021-10-21 18:09:51 -0400

    improve compiler wrappers

--- scripts/compiler-wrappers-amd.sh
+++ scripts/compiler-wrappers-amd.sh
@@ -14,10 +14,19 @@ cat - >"$prefix/bin/compiler-options.py" << EOF
 #!/usr/bin/env python3
 import sys
 import subprocess as p
+def process_remove_arg(argv)
+    name = "--remove-arg="
+    name_len = len(name)
+    args_to_remove = []
+    for arg in argv:
+        if arg[:name_len] == name:
+            args_to_remove.append(arg)
+            args_to_remove.append(arg[name_len:])
+    for x in args_to_remove:
+        while x in argv:
+            argv.remove(x)
 argv = sys.argv.copy()
-arg_to_remove = "-xcore-avx2"
-while arg_to_remove in argv:
-    argv.remove(arg_to_remove)
+process_remove_arg(argv)
 p.run(argv)
 EOF
 chmod +x "$prefix/bin/compiler-options.py"
