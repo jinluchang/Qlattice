85282cc331b375b1bf4ee026210ff221b8b5cdf4
Masaaki Tomii <masaaki.tomii1987@gmail.com>
2021-10-26 16:02:55 -0400

    practicalize auto_contractor_3f4f_matching

--- pylib/auto_contractor/simulation.py
+++ pylib/auto_contractor/simulation.py
@@ -691,14 +691,14 @@ def auto_contractor_3f4f_matching(job_tag, traj, get_prop, num_trials):
     exprs_ops = exprs_odd_ops + exprs_even_ops
     exprs_src = [
             vol * mk_k_0("t2_1") + "K0",
-            vol * mk_kpi_0_i1half("t2_1", "t2_2") + "Kpi_0_I1half",
-            vol * mk_kpi_0_i3halves("t2_1", "t2_2") + "Kpi_0_I3halves",
+            #vol * mk_kpi_0_i1half("t2_1", "t2_2") + "Kpi_0_I1half",
+            #vol * mk_kpi_0_i3halves("t2_1", "t2_2") + "Kpi_0_I3halves",
             ]
     exprs_snk = [
-            vol**2 * mk_pipi_i20("t1_1", "t1_2", True) + "pipi_I2",
-            vol**2 * mk_pipi_i0("t1_1", "t1_2", True) + "pipi_I0",
-            vol * mk_sigma("t1_1", True) + "sigma",
-            vol * mk_pi_0("t1_1", True) + "pi0",
+            #vol**2 * mk_pipi_i20("t1_1", "t1_2", True) + "pipi_I2",
+            #vol**2 * mk_pipi_i0("t1_1", "t1_2", True) + "pipi_I0",
+            #vol * mk_sigma("t1_1", True) + "sigma",
+            #vol * mk_pi_0("t1_1", True) + "pi0",
             mk_expr(1) + "1",
             ]
     exprs = []
@@ -772,7 +772,7 @@ def auto_contractor_3f4f_matching(job_tag, traj, get_prop, num_trials):
                         ratio_imag = None
                     else:
                         ratio_imag = v[0].imag / v[1].imag
-                    q.displayln_info(f"{name_fac} {k}:\n  {v}, ({ratio_real}, {ratio_imag})")
+                    q.displayln_info(f"{k}:\n  {v}, ({ratio_real}, {ratio_imag})")
                     ###
                     [ a, e, ] = v
                     f.write(a)
