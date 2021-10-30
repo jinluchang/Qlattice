42d20d7879e9f3218b5c0d3d48279c15c6bc62b2
Luchang Jin <ljin.luchang@gmail.com>
2021-10-30 15:47:25 -0400

    add simple gpt test

---
+++ scripts/gpt-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. conf.sh
+
+name=gpt-test
+
+{
+
+echo "!!!! build $name !!!!"
+
+grid_options="--dslash-asm --shm-hugepages --shm 4050"
+
+geo_options="--grid 16.16.16.16 --mpi 1.1.1.1"
+
+OMP_NUM_THREADS=4 $prefix/gpt/benchmarks/dslash.py $grid_options $geo_options --Ls 12 --N 10 
+
+cd $wd
+echo "!!!! $name build !!!!"
+
+rm -rf $temp_dir || true
+
+} |& tee $prefix/log.$name.txt
