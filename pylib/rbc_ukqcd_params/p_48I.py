2f9099c4ba36589f6693ed478abd87ba1a62af79
Luchang Jin <ljin.luchang@gmail.com>
2021-10-30 15:05:07 -0400

    small changes

--- pylib/rbc_ukqcd_params/p_48I.py
+++ pylib/rbc_ukqcd_params/p_48I.py
@@ -15,7 +15,7 @@ dict_params["load_config_params"] = { "twist_boundary_at_boundary":[ 0.0, 0.0, 0
 def mk_fermion_params(inv_type, inv_acc):
     params = {}
     params["M5"] = 1.8
-    params["boundary_phases"] = [1.0, 1.0, 1.0, 1.0] # twist boundary after loading configuration
+    params["boundary_phases"] = [ 1.0, 1.0, 1.0, 1.0, ] # twist boundary after loading configuration
     params["b"] = 1.5
     params["c"] = 0.5
     if inv_type == 0:
@@ -24,6 +24,8 @@ def mk_fermion_params(inv_type, inv_acc):
     elif inv_type == 1:
         # params["mass"] = 0.03580
         params["mass"] = 0.0362
+    elif inv_type == 2:
+        params["mass"] = 0.2
     else:
         assert False
     if inv_acc == 0 or inv_acc == 1:
@@ -53,9 +55,9 @@ def mk_fermion_params(inv_type, inv_acc):
 
 def mk_dict_fermion_params():
     params = {}
-    for inv_type in [0, 1,]:
+    for inv_type in [ 0, 1, 2, ]:
         params[inv_type] = {}
-        for inv_acc in [0, 1, 2,]:
+        for inv_acc in [ 0, 1, 2, ]:
             params[inv_type][inv_acc] = mk_fermion_params(inv_type, inv_acc)
     return params
 
