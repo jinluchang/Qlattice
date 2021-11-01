4a31d2443f10306a23a3c2c7a3b92c938aa949aa
Masaaki Tomii <masaaki.tomii1987@gmail.com>
2021-11-01 11:06:51 -0400

    adding pickle structure to auto_contractor_3f4f_matching

--- pylib/auto_contractor/simulation.py
+++ pylib/auto_contractor/simulation.py
@@ -738,9 +738,11 @@ def auto_contractor_kpipi_corr_81oprs(job_tag, traj, get_prop, num_trials):
 
 @q.timer
 def auto_contractor_3f4f_matching(job_tag, traj, get_prop, num_trials):
-    total_site = ru.get_total_site(job_tag)
-    vol = total_site[0] * total_site[1] * total_site[2]
-    exprs_odd_ops = [
+    @q.timer
+    def calc_cexpr():
+        total_site = ru.get_total_site(job_tag)
+        vol = total_site[0] * total_site[1] * total_site[2]
+        exprs_odd_ops = [
             vol * mk_Q0_b81("x", "odd") + "Q0_b81(o)",
             vol * mk_Q1_b81("x", "odd") + "Q1_b81(o)",
             vol * mk_Q2_b81("x", "odd") + "Q2_b81(o)",
@@ -750,8 +752,8 @@ def auto_contractor_3f4f_matching(job_tag, traj, get_prop, num_trials):
             vol * mk_Q6_b81("x", "odd") + "Q6_b81(o)",
             vol * mk_Q7_b81("x", "odd") + "Q7_b81(o)",
             vol * mk_Q8_b81("x", "odd") + "Q8_b81(o)",
-            ]
-    exprs_even_ops = [
+        ]
+        exprs_even_ops = [
             vol * mk_Q0_b81("x", "even") + "Q0_b81(e)",
             vol * mk_Q1_b81("x", "even") + "Q1_b81(e)",
             vol * mk_Q2_b81("x", "even") + "Q2_b81(e)",
@@ -761,30 +763,34 @@ def auto_contractor_3f4f_matching(job_tag, traj, get_prop, num_trials):
             vol * mk_Q6_b81("x", "even") + "Q6_b81(e)",
             vol * mk_Q7_b81("x", "even") + "Q7_b81(e)",
             vol * mk_Q8_b81("x", "even") + "Q8_b81(e)",
-            ]
-    exprs_ops = exprs_odd_ops + exprs_even_ops
-    exprs_src = [
+        ]
+        exprs_ops = exprs_odd_ops + exprs_even_ops
+        exprs_src = [
             vol * mk_k_0("t2_1") + "K0",
             vol * mk_kpi_0_i1half("t2_1", "t2_2") + "Kpi_0_I1half",
             vol * mk_kpi_0_i3halves("t2_1", "t2_2") + "Kpi_0_I3halves",
-            ]
-    exprs_snk = [
+        ]
+        exprs_snk = [
             vol**2 * mk_pipi_i20("t1_1", "t1_2", True) + "pipi_I2",
             vol**2 * mk_pipi_i0("t1_1", "t1_2", True) + "pipi_I0",
             vol * mk_sigma("t1_1", True) + "sigma",
             vol * mk_pi_0("t1_1", True) + "pi0",
             mk_expr(1) + "1",
-            ]
-#    exprs_src_snk = [
-#        vol**2 * mk_
-#        ]
-    exprs = []
-    for expr_src in exprs_src:
-        for expr_snk in exprs_snk:
-            for expr_op in exprs_ops:
-                exprs.append(expr_snk * expr_op * expr_src)
-    diagram_type_dict = dict()
-    cexpr = contract_simplify_compile(*exprs, is_isospin_symmetric_limit = True, diagram_type_dict = diagram_type_dict)
+        ]
+        #    exprs_src_snk = [
+        #        vol**2 * mk_
+        #        ]
+        exprs = []
+        for expr_src in exprs_src:
+            for expr_snk in exprs_snk:
+                for expr_op in exprs_ops:
+                    exprs.append(expr_snk * expr_op * expr_src)
+        diagram_type_dict = dict()
+        cexpr = contract_simplify_compile(*exprs, is_isospin_symmetric_limit = True, diagram_type_dict = diagram_type_dict)
+        q.displayln_info(display_cexpr(cexpr))
+        cexpr.collect_op()
+        return cexpr
+    cexpr = q.pickle_cache_call(calc_cexpr, f"cache/auto_contractor_cexpr/3f4f-cexpr.{vol}.pickle")
     q.displayln_info(display_cexpr(cexpr))
     cexpr.collect_op()
     q.displayln_info(display_cexpr_raw(cexpr))
