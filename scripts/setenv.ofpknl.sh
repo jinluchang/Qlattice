3af22893eff10e6021f5e525ef3bbc7a71f1e57a
Luchang Jin <ljin.luchang@gmail.com>
2022-01-19 09:25:45 -0500

    add target

---
+++ scripts/setenv.ofpknl.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+. conf.sh
+
+name=setenv
+
+{
+
+echo "!!!! build $name !!!!"
+
+mkdir -p "$prefix"
+
+cat - setenv.sh >"$prefix/setenv.sh" << EOF
+echo "Sourcing '$prefix/setenv.sh'"
+if [ -z "\$prefix" ] ; then
+    prefix="$prefix"
+fi
+if [ -z "\$num_proc" ] ; then
+    num_proc=4
+fi
+export PYTHONPATH=
+CC_OLD="\$CC"
+CXX_OLD="\$CXX"
+module purge
+module add intel/2019.5.281
+module add impi/2019.5.281
+module add gcc/7.5.0
+module add cmake/3.14.5
+module add hdf5/1.10.5
+module add gsl/2.5
+module list
+export CC="\$CC_OLD"
+export CXX="\$CXX_OLD"
+EOF
+
+./scripts/compiler-wrappers.sh
+
+. "$prefix/setenv.sh" >"$prefix/log.setenv.txt" 2>&1
+
+echo "!!!! $name build !!!!"
+
+rm -rf $temp_dir || true
+
+} |& tee $prefix/log.$name-build.txt
