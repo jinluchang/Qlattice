3e27a7bf5d61a5606caf32aadb74ef67209c0bae
Luchang Jin <ljin.luchang@gmail.com>
2022-01-28 00:05:50 -0500

    small

--- pylib/apps/mk-lanc/run.py
+++ pylib/apps/mk-lanc/run.py
@@ -11,22 +11,12 @@ import pprint
 
 import os
 
-def get_save_path(fn):
-    return os.path.join("results", fn)
+from jobs import *
 
-def get_load_path(fn):
-    if fn is None:
-        return None
-    path_list = [
-            "results",
-            "../mk-gf-gt/results",
-            "/gpfs/alpine/lgt116/proj-shared/ljin",
-            ]
-    for path in path_list:
-        p = os.path.join(path, fn)
-        if q.does_file_exist_sync_node(p):
-            return p
-    return None
+load_path_list[:] = [
+        "results",
+        "../mk-gf-gt/results",
+        ]
 
 @q.timer
 def check_job(job_tag, traj):
@@ -60,68 +50,6 @@ def check_job(job_tag, traj):
     return False
 
 @q.timer
-def compute_eig(gf, job_tag, inv_type = 0, inv_acc = 0, *, path = None):
-    # return a function ``get_eig''
-    # ``get_eig()'' return the ``eig''
-    load_eig = ru.load_eig_lazy(get_load_path(path), job_tag)
-    if load_eig is not None:
-        return load_eig
-    # evec, evals = ru.mk_eig(gf, job_tag, inv_type, inv_acc)
-    basis, cevec, smoothed_evals = ru.mk_ceig(gf, job_tag, inv_type, inv_acc)
-    eig = [ basis, cevec, smoothed_evals ]
-    ru.save_ceig(get_save_path(path + ".partial"), eig, job_tag, inv_type, inv_acc);
-    q.qrename_info(get_save_path(path + ".partial"), get_save_path(path))
-    test_eig(gf, eig, job_tag, inv_type)
-    def get_eig():
-        return eig
-    return get_eig
-
-@q.timer
-def test_eig(gf, eig, job_tag, inv_type):
-    geo = gf.geo()
-    src = q.FermionField4d(geo)
-    q.displayln_info(f"src norm {src.qnorm()}")
-    src.set_rand(q.RngState("test_eig:{id(inv)}"))
-    sol_ref = ru.get_inv(gf, job_tag, inv_type, inv_acc = 2, eig = eig, eps = 1e-10, mpi_split = False, timer = False) * src
-    q.displayln_info(f"sol_ref norm {sol_ref.qnorm()} with eig")
-    for inv_acc in [0, 1, 2]:
-        sol = ru.get_inv(gf, job_tag, inv_type, inv_acc, eig = eig, mpi_split = False, timer = False) * src
-        sol -= sol_ref
-        q.displayln_info(f"sol diff norm {sol.qnorm()} inv_acc={inv_acc} with eig")
-        sol = ru.get_inv(gf, job_tag, inv_type, inv_acc, mpi_split = False, timer = False) * src
-        sol -= sol_ref
-        q.displayln_info(f"sol diff norm {sol.qnorm()} inv_acc={inv_acc} without eig")
-
-@q.timer_verbose
-def run_gf(job_tag, traj):
-    path_gf = get_load_path(f"configs/{job_tag}/ckpoint_lat.{traj}")
-    if path_gf is None:
-        if job_tag[:5] == "test-":
-            gf = ru.mk_sample_gauge_field(job_tag, f"{traj}")
-            q.qmkdir_info(get_save_path(f"configs"))
-            q.qmkdir_info(get_save_path(f"configs/{job_tag}"))
-            path_gf = get_save_path(f"configs/{job_tag}/ckpoint_lat.{traj}")
-            # gf.save(path_gf)
-            qg.save_gauge_field(gf, path_gf)
-        else:
-            assert False
-    get_gf = ru.load_config_lazy(job_tag, path_gf)
-    return get_gf
-
-@q.timer_verbose
-def run_eig(job_tag, traj, get_gf):
-    if None in [ get_gf, ]:
-        return None
-    get_eig = ru.load_eig_lazy(get_load_path(f"eig/{job_tag}/traj={traj}"), job_tag)
-    if get_eig is None and get_gf is not None:
-        if q.obtain_lock(f"locks/{job_tag}-{traj}-run-eig"):
-            q.qmkdir_info(get_save_path(f"eig"))
-            q.qmkdir_info(get_save_path(f"eig/{job_tag}"))
-            get_eig = compute_eig(get_gf(), job_tag, inv_type = 0, path = f"eig/{job_tag}/traj={traj}")
-            q.release_lock()
-    return get_eig
-
-@q.timer
 def run_job(job_tag, traj):
     if check_job(job_tag, traj):
         return
@@ -139,8 +67,7 @@ qg.begin_with_gpt()
 
 # ADJUST ME
 job_tags = [
-        "test-4nt8",
-        "test-4nt16",
+        "test-4nt8", "test-4nt16",
         # "test-8nt16",
         # "test-16nt32",
         # "test-32nt64",
