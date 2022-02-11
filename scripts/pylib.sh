c8cc70e88e4fe1dba4c7f489014054f838686461
Luchang Jin <ljin.luchang@gmail.com>
2022-02-10 21:20:37 -0500

    simplify scripts/qlat.sh

--- scripts/pylib.sh
+++ scripts/pylib.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./scripts/qlat-header.sh
+
 . conf.sh
 
 name=pylib
@@ -7,18 +9,6 @@ name=pylib
 {
 
 echo "!!!! build $name !!!!"
-
-mkdir -pv $prefix/pylib
-rm -rfv $prefix/pylib/qlat
-rm -rfv $prefix/pylib/rbc_ukqcd_params
-rm -rfv $prefix/pylib/auto_contractor
-rm -rfv $prefix/pylib/*.py
-rm -rfv $prefix/pylib/Makefile.inc
-cp -rpv pylib/qlat $prefix/pylib/
-cp -rpv pylib/rbc_ukqcd_params $prefix/pylib/
-cp -rpv pylib/auto_contractor $prefix/pylib/
-cp -pv pylib/*.py $prefix/pylib/
-cp -pv pylib/Makefile.inc $prefix/pylib/
 
 mkdir -pv "$prefix"/gpt/lib/gpt || true
 rsync -av --delete $distfiles/gpt/lib/gpt/ "$prefix"/gpt/lib/gpt/
--- scripts/qlat.sh
+++ scripts/qlat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./scripts/qlat-header.sh
+
 . conf.sh
 
 name=qlat
@@ -7,30 +9,6 @@ name=qlat
 {
 
 echo "!!!! build $name !!!!"
-
-mkdir -pv $prefix/include
-rm -rfv $prefix/include/qutils
-rm -rfv $prefix/include/qlat
-cp -rpv qutils $prefix/include/
-cp -rpv qlat $prefix/include/
-cp -pv qlat-setup.h $prefix/include
-
-mkdir -pv $prefix/pylib
-rm -rfv $prefix/pylib/qlat
-rm -rfv $prefix/pylib/rbc_ukqcd_params
-rm -rfv $prefix/pylib/auto_contractor
-rm -rfv $prefix/pylib/*.py
-rm -rfv $prefix/pylib/Makefile.inc
-cp -rpv pylib/qlat $prefix/pylib/
-cp -rpv pylib/rbc_ukqcd_params $prefix/pylib/
-cp -rpv pylib/auto_contractor $prefix/pylib/
-cp -pv pylib/*.py $prefix/pylib/
-cp -pv pylib/Makefile.inc $prefix/pylib/
-
-cp pylib/apps/lat-io-glimpse/lat-io-glimpse $prefix/bin/
-cp pylib/apps/gauge-fix-coulomb/gauge-fix-coulomb $prefix/bin/
-
-( cd ./pylib/cqlat ; ./update.sh )
 
 mkdir -pv $prefix/pylib
 rm -rfv $prefix/pylib/cqlat
