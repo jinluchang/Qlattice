70ecc1bcbaae56a516cb54b90cdeb39d506694e9
Luchang Jin <ljin.luchang@gmail.com>
2022-02-12 15:40:53 -0500

    small

--- pylib/apps/mk-smear-prop/run.py
+++ pylib/apps/mk-smear-prop/run.py
@@ -187,6 +187,7 @@ def run_job(job_tag, traj):
 rup.dict_params["test-4nt8"]["trajs"] = list(range(1000, 1400, 100))
 rup.dict_params["test-4nt16"]["trajs"] = list(range(1000, 1400, 100))
 rup.dict_params["48I"]["trajs"] = list(range(3000, 500, -5))
+rup.dict_params["24D"]["trajs"] = list(range(1000, 10000, 10))
 rup.dict_params["16IH2"]["trajs"] = list(range(1000, 10000, 50))
 rup.dict_params["32IfineH"]["trajs"] = list(range(1000, 10000, 50))
 
