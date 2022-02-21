0edd8d1290b6aa6fd599c339d9f5c72a6318302a
Luchang Jin <ljin.luchang@gmail.com>
2022-02-21 13:48:06 -0500

    add mk-topo

---
+++ pylib/apps/mk-topo/run.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+# Need --mpi X.X.X.X --mpi X.X.X runtime option
+
+import qlat as q
+import gpt as g
+import qlat_gpt as qg
+import rbc_ukqcd as ru
+import rbc_ukqcd_params as rup
+import pprint
+
+import os
+
+from jobs import *
+
+load_path_list[:] = [
+        "results",
+        "../qcddata",
+        os.path.join(os.getenv("HOME"), "qcddata"),
+        "../mk-gf-gt/results",
+        os.path.join(os.getenv("HOME"), "Qlat-sample-data/mk-gf-gt/results"),
+        ]
+
+@q.timer
+def run_topo(job_tag, traj, get_gf):
+    if get_gf is None:
+        return
+    fn = f"topo-measure/{job_tag}/traj={traj}.pickle"
+    if get_load_path(fn) is not None:
+        return
+    if q.obtain_lock(f"locks/{job_tag}-{traj}-topo"):
+        gf = get_gf()
+        topo_list = q.smear_measure_topo(gf)
+        q.save_pickle_obj(topo_list, get_save_path(fn))
+        q.release_lock()
+
+@q.timer_verbose
+def run_job(job_tag, traj):
+    fns_produce = [
+            f"topo-measure/{job_tag}/traj={traj}.pickle",
+            ]
+    fns_need = [
+            (f"configs/{job_tag}/ckpoint_lat.{traj}", f"configs/{job_tag}/ckpoint_lat.IEEE64BIG.{traj}",),
+            ]
+    if job_tag[:5] == "test-":
+        fns_need = []
+    if not check_job(job_tag, traj, fns_produce, fns_need):
+        return
+    #
+    get_gf = run_gf(job_tag, traj)
+    #
+    run_topo(job_tag, traj, get_gf)
+    #
+    q.clean_cache()
+    q.timer_display()
+
+rup.dict_params["test-4nt8"]["trajs"] = list(range(1000, 1400, 100))
+rup.dict_params["test-4nt16"]["trajs"] = list(range(1000, 1400, 100))
+rup.dict_params["48I"]["trajs"] = list(range(500, 3000, 5))
+rup.dict_params["16IH2"]["trajs"] = list(range(500, 5000, 10))
+rup.dict_params["32IfineH"]["trajs"] = list(range(1000, 10000, 50))
+
+qg.begin_with_gpt()
+
+# ADJUST ME
+job_tags = [
+        "test-4nt8", "test-4nt16",
+        # "32IfineH",
+        # "16IH2",
+        # "24D",
+        # "48I",
+        ]
+
+q.check_time_limit()
+
+for job_tag in job_tags:
+    q.displayln_info(pprint.pformat(rup.dict_params[job_tag]))
+    for traj in rup.dict_params[job_tag]["trajs"]:
+        run_job(job_tag, traj)
+
+qg.end_with_gpt()
