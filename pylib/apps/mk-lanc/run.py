e0856bc4c8ebffa7130c9e4c39589b88a62d4db7
Luchang Jin <ljin.luchang@gmail.com>
2022-02-25 01:55:15 -0500

    small

--- pylib/apps/mk-lanc/run.py
+++ pylib/apps/mk-lanc/run.py
@@ -45,19 +45,16 @@ rup.dict_params["test-4nt16"]["trajs"] = list(range(1000, 1400, 100))
 rup.dict_params["32Dfine"]["trajs"] = list(range(500, 3000, 10))
 rup.dict_params["16IH2"]["trajs"] = list(range(500, 10000, 10))
 rup.dict_params["32IfineH"]["trajs"] = list(range(500, 10000, 50))
+rup.dict_params["24IH1"]["trajs"] = list(range(500, 10000, 10))
+rup.dict_params["24IH2"]["trajs"] = list(range(500, 10000, 10))
+rup.dict_params["32IH2"]["trajs"] = list(range(500, 10000, 10))
 
 qg.begin_with_gpt()
 
 # ADJUST ME
 job_tags = [
         "test-4nt8", "test-4nt16",
-        # "test-8nt16",
-        # "test-16nt32",
-        # "test-32nt64",
-        # "test-48nt96",
-        # "test-64nt128",
-        # "test-96nt192",
-        # "test-128nt256",
+        # "24IH2", "24IH1", "32IH2",
         # "16IH2",
         # "24D",
         # "32Dfine",
