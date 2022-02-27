85d0d4e3e460c00b893bc2a0c3667123b92f317b
Luchang Jin <ljin.luchang@gmail.com>
2022-02-27 02:16:20 -0500

    small

--- docs/monitor-log.sh
+++ docs/monitor-log.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+export
+log="$PJM_STDOUT_PATH"
 {
+    export PS_FORMAT=user,pid,ppid,pgid,pmem,pcpu,etime,cputime,comm,cmd
     while : ; do
-        if [ "$(find "$log" -mmin +15)" ] ; then
+        if [ "$(find "$log" -mmin +5)" ] ; then
             echo Program Hang. Killing everything
+            ps -u $(whoami)
+            free -g
+            uptime
         else
-            sleep 180
+            {
+                echo
+                echo monitor
+                uptime
+                ps -u $(whoami)
+                free -g
+            } >> "$log"-monitor
+            sleep 10
             continue
         fi
         sync
         sleep 5
-        mv "$log" failed-"$log"
+        mv "$log" "$log"-failed
         kill -9 -1
     done
 } &
