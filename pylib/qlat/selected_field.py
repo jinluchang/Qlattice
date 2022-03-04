7863f0cda71e70e8830147bcec67cd845a838abb
Luchang Jin <ljin.luchang@gmail.com>
2022-03-03 21:43:42 -0500

    add selected field set_elem

--- pylib/qlat/selected_field.py
+++ pylib/qlat/selected_field.py
@@ -98,6 +98,49 @@ class SelectedField:
         else:
             return np.array(c.get_elem_sfield(self, idx, m))
 
+    def set_elems(self, idx, val):
+        # val should be np.ndarray or bytes. e.g. np.array([1, 2, 3], dtype = complex).tobytes()
+        if isinstance(val, bytes):
+            return c.set_elems_sfield(self, idx, val)
+        elif isinstance(val, np.ndarray):
+            return self.set_elems(self, idx, val.tobytes())
+        else:
+            assert False
+
+    def set_elem(self, idx, m, val):
+        # val should be np.ndarray or bytes. e.g. np.array([1, 2, 3], dtype = complex).tobytes()
+        if isinstance(val, bytes):
+            return c.set_elem_sfield(self, idx, m, val)
+        elif isinstance(val, np.ndarray):
+            return self.set_elem(self, idx, m, val.tobytes())
+        else:
+            assert False
+
+    def __getitem__(self, i):
+        # i can be (idx, m,) or idx
+        if isinstance(i, tuple) and len(i) == 2 and isinstance(i[0], int):
+            idx, m = i
+            return self.get_elem(idx, m)
+        elif isinstance(i, int):
+            idx = i
+            return self.get_elems(idx)
+        else:
+            assert False
+            return None
+
+    def __setitem__(self, i, val):
+        # i can be (idx, m,) or idx
+        # val should be np.ndarray or bytes. e.g. np.array([1, 2, 3], dtype = complex).tobytes()
+        if isinstance(i, tuple) and len(i) == 2 and isinstance(i[0], int):
+            idx, m = i
+            return self.set_elem(idx, m, val)
+        elif isinstance(i, int):
+            idx = i
+            return self.set_elems(idx, val)
+        else:
+            assert False
+            return None
+
     def sparse(self, sel):
         # deprecated
         if isinstance(sel, PointSelection):
