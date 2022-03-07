37e299dd9ed42f7ff56672a9d2b9895144fc9b6a
Luchang Jin <ljin.luchang@gmail.com>
2022-03-07 01:34:02 -0500

    separate ama in auto_contractor

--- pylib/auto_contractor/eval.py
+++ pylib/auto_contractor/eval.py
@@ -20,6 +20,7 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 from auto_contractor.compile import *
+from auto_contractor.ama import *
 import gpt as g
 import numpy as np
 import qlat as q
@@ -56,108 +57,6 @@ def g5_herm(x):
                     as_mspincolor(x)))
                 * g.gamma[5])
     return x_h
-
-class AmaVal:
-
-    def __init__(self, val = None, corrections = None):
-        # should have the following:
-        # self.val: sloppy value
-        # self.corrections: list [ (val, description_dict,), ... ]
-        # description_dict:
-        # { source_specification:
-        #   (accuracy_level_relative_to_the_basic_accuracy,
-        #    probability_of_having_this_accuracy,),
-        # }
-        self.val = val
-        if corrections is None:
-            self.corrections = []
-        else:
-            self.corrections = corrections
-
-def ama_apply1(f, x):
-    if not isinstance(x, AmaVal):
-        return f(x)
-    elif isinstance(x, AmaVal):
-        val = f(x.val)
-        corrections = [ (f(v), d,) for v, d in x.corrections ]
-        return AmaVal(val, corrections)
-    else:
-        assert False
-
-def merge_description_dict(d1, d2):
-    sd1 = set(d1)
-    sd2 = set(d2)
-    common_keys = sd1 & sd2
-    for key in common_keys:
-        if d1[key] != d2[key]:
-            return None
-    new_keys = sd2 - sd1
-    d = d1.copy()
-    for key in new_keys:
-        d[key] = d2[key]
-    return d
-
-def ama_apply2(f, x, y):
-    if not isinstance(x, AmaVal) and not isinstance(y, AmaVal):
-        return f(x, y)
-    elif isinstance(x, AmaVal) and not isinstance(y, AmaVal):
-        def f1(x1):
-            return f(x1, y)
-        return ama_apply1(f1, x)
-    elif not isinstance(x, AmaVal) and isinstance(y, AmaVal):
-        def f1(y1):
-            return f(x, y1)
-        return ama_apply1(f1, y)
-    elif isinstance(x, AmaVal) and isinstance(y, AmaVal):
-        val = f(x.val, y.val)
-        corrections = []
-        for v_x, d_x in x.corrections:
-            for v_y, d_y in y.corrections:
-                d = merge_description_dict(d_x, d_y)
-                if d is not None:
-                    corrections.append((f(v_x, v_y), d,))
-        return AmaVal(val, corrections)
-    else:
-        assert False
-
-def ama_extract(x):
-    if not isinstance(x, AmaVal):
-        return x
-    elif isinstance(x, AmaVal):
-        val = x.val
-        corrections = x.corrections
-        assert isinstance(corrections, list)
-        assert corrections
-        # keys = [ source_specification, ... ]
-        keys = list(corrections[0][1].keys())
-        def get_level_prob(key):
-            s = set([ d[key] for v, d in corrections ])
-            return sorted(list(s))
-        dict_level_prob = { k: get_level_prob(k) for k in keys }
-        for k, v in dict_level_prob.items():
-            assert len(v) >= 2
-        # dict_level[key] = list of accuracy levels for this key (source_specification)
-        dict_level = { k: [ l for l, prob in v ] for k, v in dict_level_prob.items() }
-        # dict_prob[key] = list of probability_of_having_this_accuracy
-        dict_prob = { k: [ prob for l, prob in v ] for k, v in dict_level_prob.items() }
-        # dict_val[(level, ...)] = v
-        dict_val = { tuple([ d[k][0] for k in keys ]): v for v, d in corrections }
-        def ama_corr(fixed_levels, remaining_keys):
-            if not remaining_keys:
-                return dict_val[tuple(fixed_levels)]
-            else:
-                key = remaining_keys[0]
-                rest_keys = remaining_keys[1:]
-                levels = dict_level[key]
-                probs = dict_prob[key]
-                vals = [ ama_corr(fixed_levels + [ l, ], rest_keys) for l in levels ]
-                corr = vals[0]
-                for i in range(1, len(vals)):
-                    corr += (vals[i] - vals[i - 1]) / probs[i]
-                return corr
-        return ama_corr([], keys)
-    else:
-        assert False
 
 def eval_op_term_expr(expr, variable_dict, positions_dict, get_prop):
     def l_eval(x):
@@ -390,4 +289,3 @@ def positions_dict_maker_example_2(idx):
 if __name__ == "__main__":
     print(positions_dict_maker_example_1(0))
     print(positions_dict_maker_example_2(0))
-
