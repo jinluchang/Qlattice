a532ce41f926e0ae37bac9ea571f7508d18471fd
Luchang Jin <ljin.luchang@gmail.com>
2022-03-11 01:17:12 -0500

    small

--- pylib/apps/mk-topo/run.py
+++ pylib/apps/mk-topo/run.py
@@ -57,7 +57,7 @@ def run_job(job_tag, traj):
 tag = "trajs"
 rup.dict_params["test-4nt8"][tag] = list(range(1000, 1400, 100))
 rup.dict_params["test-4nt16"][tag] = list(range(1000, 1400, 100))
-rup.dict_params["48I"][tag] = list(range(3000, 500, -5))
+rup.dict_params["48I"][tag] = list(range(1000, 3000, 5))
 rup.dict_params["24D"][tag] = list(range(1000, 10000, 10))
 rup.dict_params["32Dfine"][tag] = list(range(1000, 10000, 10))
 rup.dict_params["16IH2"][tag] = list(range(1000, 10000, 10))
