abfd6599467e8021eaece218b3a8c133c0a7e467
Luchang Jin <ljin.luchang@gmail.com>
2022-05-07 16:32:15 -0400

    small update

--- pylib/apps/auto-contract-meson-qed/load_data.py
+++ pylib/apps/auto-contract-meson-qed/load_data.py
@@ -291,13 +291,11 @@ def load_prop_wsrc_psel(job_tag, traj, flavor, *, wi, psel, fsel, fselc, gt):
         sp_prop = q.PselProp(psel)
         sp_prop.load(get_load_path(fn_sp))
         sp_prop = gt_inv * sp_prop
-        # convert to GPT/Grid prop mspincolor order
         cache_psel[f"{tag} ; wsrc ; psel"] = sp_prop
         # load wsnk prop
         fn_spw = os.path.join(path_sp, f"{tag} ; wsnk.lat")
         spw_prop = q.PselProp(psel_ts)
         spw_prop.load(get_load_path(fn_spw))
-        # convert to GPT/Grid prop mspincolor order
         cache_psel_ts[f"{tag} ; wsrc_wsnk ; psel_ts"] = spw_prop
         count[inv_acc] += 1
     assert count[1] == total_site[3]
@@ -338,7 +336,6 @@ def load_prop_wsrc_fsel(job_tag, traj, flavor, *, wi, psel, fsel, fselc, gt):
         sc_prop = q.SelProp(fselc)
         sc_prop.load_double_from_float(sfr, tag)
         sc_prop = gt_inv * sc_prop
-        # convert to GPT/Grid prop mspincolor order
         cache_fsel[f"{tag} ; wsrc ; fsel"] = sc_prop
         # check psel psnk prop
         sp_prop_diff = q.PselProp(psel)
@@ -389,7 +386,6 @@ def load_prop_psrc_psel(job_tag, traj, flavor, *, psel, fsel, fselc):
         # load psel psnk prop
         sp_prop = q.PselProp(psel)
         sp_prop.load(fn_sp_load)
-        # convert to GPT/Grid prop mspincolor order
         cache_psel[f"{tag} ; psrc ; psel"] = sp_prop
         # load wsnk prop
         fn_spw = os.path.join(path_sp, f"{tag} ; wsnk.lat")
@@ -397,7 +393,6 @@ def load_prop_psrc_psel(job_tag, traj, flavor, *, psel, fsel, fselc):
         if fn_spw_load is not None:
             spw_prop = q.PselProp(psel_ts)
             spw_prop.load(fn_spw_load)
-            # convert to GPT/Grid prop mspincolor order
             cache_psel_ts[f"{tag} ; psrc_wsnk ; psel_ts"] = spw_prop
         count[inv_acc] += 1
     cache_prob[f"type={flavor_inv_type} ; accuracy=0 ; psrc ; prob"] = count[0] / len(xg_list)
@@ -441,7 +436,6 @@ def load_prop_psrc_fsel(job_tag, traj, flavor, *, psel, fsel, fselc):
         # load fsel psnk prop
         sc_prop = q.SelProp(fselc)
         sc_prop.load_double_from_float(sfr, tag)
-        # convert to GPT/Grid prop mspincolor order
         cache_fsel[f"{tag} ; psrc ; fsel"] = sc_prop
         # check psel psnk prop
         sp_prop_diff = q.PselProp(psel)
