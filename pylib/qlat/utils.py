5ffdb33f58a62e57ec1026ddf6a306f1c498eb95
Luchang Jin <ljin.luchang@gmail.com>
2022-07-10 02:48:08 -0400

    make parallel map silent

--- pylib/qlat/utils.py
+++ pylib/qlat/utils.py
@@ -32,7 +32,11 @@ def show_memory_usage():
     malloc_stats_info()
 
 @timer
-def parallel_map(q_mp_proc, func, iterable, *, chunksize = 1, process_initialization = process_initialization):
+def parallel_map(q_mp_proc, func, iterable,
+        *,
+        chunksize = 1,
+        process_initialization = process_initialization,
+        is_verbose = False):
     displayln_info(f"parallel_map(q_mp_proc={q_mp_proc})")
     if q_mp_proc == 0:
         return list(map(func, iterable))
@@ -43,17 +47,25 @@ def parallel_map(q_mp_proc, func, iterable, *, chunksize = 1, process_initializa
     gc.collect()
     gc.freeze()
     with mp.Pool(q_mp_proc, process_initialization, []) as p:
-        p.apply(show_memory_usage)
+        if is_verbose:
+            p.apply(show_memory_usage)
         res = p.map(call_pool_function, iterable, chunksize = chunksize)
-        p.apply(show_memory_usage)
-        p.apply(timer_display)
+        if is_verbose:
+            p.apply(show_memory_usage)
+            p.apply(timer_display)
     gc.unfreeze()
     gc.collect()
     pool_function = None
     return res
 
 @timer
-def parallel_map_sum(q_mp_proc, func, iterable, *, sum_function = None, sum_start = 0, chunksize = 1, process_initialization = process_initialization):
+def parallel_map_sum(q_mp_proc, func, iterable,
+        *,
+        sum_function = None,
+        sum_start = 0,
+        chunksize = 1,
+        process_initialization = process_initialization,
+        is_verbose = False):
     # iterable = [ i1, i2, ... ]
     # va1, vb1, ... = func(i1)
     # return [ sum([va1, va2, ...]), sum([vb1, vb2, ...]), ... ]
@@ -69,15 +81,17 @@ def parallel_map_sum(q_mp_proc, func, iterable, *, sum_function = None, sum_star
     gc.collect()
     gc.freeze()
     with mp.Pool(q_mp_proc, process_initialization, []) as p:
-        p.apply(show_memory_usage)
+        if is_verbose:
+            p.apply(show_memory_usage)
         res = p.imap(call_pool_function, iterable, chunksize = chunksize)
         if sum_start == 0:
             ret = sum_function(res)
         else:
             # ret = sum_function(res, start = sum_start)
             ret = sum_function(res, sum_start)
-        p.apply(show_memory_usage)
-        p.apply(timer_display)
+        if is_verbose:
+            p.apply(show_memory_usage)
+            p.apply(timer_display)
     gc.unfreeze()
     gc.collect()
     pool_function = None
