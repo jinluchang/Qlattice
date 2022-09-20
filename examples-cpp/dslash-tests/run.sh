04b5d00c5f523fc4b237c8cb010a0fe4e6892688
Luchang Jin <ljin.luchang@gmail.com>
2022-09-19 23:09:41 -0400

    small

--- examples-cpp/dslash-tests/run.sh
+++ examples-cpp/dslash-tests/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+pwd
+ls
 q_verbose=10 time mpirun --oversubscribe -x OMP_NUM_THREADS=2 --np 2 ./qlat.x | tee log.full
 cat log.full | grep -v '^Timer\|^check_status:\|^display_geo_node : id_node =' > log
