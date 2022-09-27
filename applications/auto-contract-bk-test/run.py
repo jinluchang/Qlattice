b9cafd9fc8e8e5c21e83d11a10008be8b593f24c
Luchang Jin <ljin.luchang@gmail.com>
2022-09-27 02:16:14 -0400

    add operator to bk test

--- applications/auto-contract-bk-test/run.py
+++ applications/auto-contract-bk-test/run.py
@@ -71,14 +71,34 @@ def mk_bpi_vv_aa(p : str):
         s = s + v1 * v2
     return s + f"Opi_{{VV+AA}}"
 
+def mk_bkpi1_vv_aa(p : str):
+    s = 0
+    for mu in range(4):
+        v1 = mk_vec_mu("d", "u", p, mu) - mk_vec5_mu("u'", "u", p, mu)
+        v2 = mk_vec_mu("u'", "s", p, mu) - mk_vec5_mu("u'", "s", p, mu)
+        s = s + v1 * v2
+    return s + f"Okpi_{{VV+AA}}"
+
+def mk_bkpi2_vv_aa(p : str):
+    s = 0
+    for mu in range(4):
+        v1 = mk_vec_mu("u'", "u", p, mu) - mk_vec5_mu("u'", "u", p, mu)
+        v2 = mk_vec_mu("d", "s", p, mu) - mk_vec5_mu("d", "s", p, mu)
+        s = s + v1 * v2
+    return s + f"Okpi_{{VV+AA}}"
+
 @q.timer
 def get_cexpr_meson_bk_bpi_corr():
     def calc_cexpr():
         exprs = [
-                mk_k_0_bar("t_2", True) * mk_bk_vv_aa("x") * mk_k_0("t_1")
-                + "k0_bar^dag * b_k  * k0 ",
-                mk_pi_p("t_2", True) * mk_bpi_vv_aa("x") * mk_pi_m("t_1")
-                + "pi+^dag    * b_pi * pi-",
+                mk_meson("d", "s", "t_2") * mk_bk_vv_aa("x") * mk_meson("d", "s", "t_1")
+                + "(i dbar g5 s)  * (sbar  gmu (1-g5) d)(sbar  gmu (1-g5) d) * (i dbar g5 s)",
+                mk_meson("d", "u", "t_2") * mk_bpi_vv_aa("x") * mk_meson("d", "u", "t_1")
+                + "(i dbar g5 u)  * (ubar  gmu (1-g5) d)(ubar  gmu (1-g5) d) * (i dbar g5 u)",
+                mk_meson("u", "u'", "t_2") * mk_bkpi1_vv_aa("x") * mk_meson("s", "d", "t_1")
+                + "(i ubar g5 u') * (dbar  gmu (1-g5) u)(u'bar gmu (1-g5) s) * (i sbar g5 d)",
+                mk_meson("u", "u'", "t_2") * mk_bkpi2_vv_aa("x") * mk_meson("s", "d", "t_1")
+                + "(i ubar g5 u') * (u'bar gmu (1-g5) u)(dbar  gmu (1-g5) s) * (i sbar g5 d)",
                 ]
         cexpr = contract_simplify_compile(*exprs, is_isospin_symmetric_limit = True)
         q.displayln_info(display_cexpr(cexpr))
