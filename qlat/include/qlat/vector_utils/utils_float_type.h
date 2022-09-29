4f945399d561edfbbdcd0e444f7f862e9990b4f9
aroadant <aroadant@aroadant.home>
2022-09-29 13:35:26 +0200

    check push v1a new

--- qlat/include/qlat/vector_utils/utils_float_type.h
+++ qlat/include/qlat/vector_utils/utils_float_type.h
@@ -131,7 +131,7 @@ inline void print_NONE(const char *filename)
 #endif
 
 
-#define CKPOINT abort_r("CHECK POINT \n");
+#define QLAT_VEC_CKPOINT abort_r("QLAT CHECK POINT \n");
 
 #ifdef QLAT_USE_ACC
 // *************** FOR ERROR CHECKING *******************
