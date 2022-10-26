ded2cd99876f925384a503a319ea1ac76add89f1
Luchang Jin <ljin.luchang@gmail.com>
2022-10-26 10:57:12 -0400

    add more packages to download

--- scripts/download-core.sh
+++ scripts/download-core.sh
@@ -24,6 +24,8 @@ dget "fftw-3.3.10.tar.gz" "http://www.fftw.org/fftw-3.3.10.tar.gz"
 
 dget "c-lime.tar.gz" "https://github.com/usqcd-software/c-lime/tarball/master"
 
+dget "hdf5-1.10.7.tar.bz2" "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.7/src/hdf5-1.10.7.tar.bz2"
+
 if [ -d Grid-lehner ] ; then
     ( cd Grid-lehner ; git pull https://github.com/jinluchang/Grid.git )
 else
@@ -40,6 +42,30 @@ if [ -d gpt ] ; then
     ( cd gpt ; git pull https://github.com/jinluchang/gpt.git )
 else
     git clone https://github.com/jinluchang/gpt.git
+fi
+
+if [ -d Grid-tblum ] ; then
+    ( cd Grid-tblum ; git pull )
+else
+    git clone https://github.com/tblum2/Grid.git Grid-tblum
+fi
+
+if [ -e Grid-tblum/configure ] ; then
+    echo "Grid-tblum bootstrapped."
+else
+    ( cd Grid-tblum ; git clean -f ; ./bootstrap.sh )
+fi
+
+if [ -d Hadrons-tblum ] ; then
+    ( cd Hadrons-tblum ; git pull )
+else
+    git clone https://github.com/tblum2/Hadrons.git Hadrons-tblum
+fi
+
+if [ -e Hadrons-tblum/configure ] ; then
+    echo "Hadrons-tblum bootstrapped."
+else
+    ( cd Hadrons-tblum ; git clean -f ; ./bootstrap.sh )
 fi
 
 cd $wd
