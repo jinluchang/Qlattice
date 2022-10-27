8366576f4c9af118a318013dedbf30cdb8299ed7
aroadant <26831146+aroadant@users.noreply.github.com>
2022-10-28 01:24:34 +0200

    add more mpi ranks

--- examples-cpp/qlat-smear-tests/run.sh
+++ examples-cpp/qlat-smear-tests/run.sh
@@ -2,5 +2,5 @@
 
 pwd
 ls
-q_verbose=10 time mpirun --oversubscribe -x OMP_NUM_THREADS=2 --np 2 ./qlat.x >log.full
+q_verbose=10 time mpirun --oversubscribe -x OMP_NUM_THREADS=2 --np 4 ./qlat.x >log.full
 cat log.full | grep -v '^Timer\|^check_status:\|^display_geo_node : id_node =' >log
