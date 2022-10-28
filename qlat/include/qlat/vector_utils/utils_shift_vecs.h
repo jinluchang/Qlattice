5fd3a5bdc8c48f94d7f84df4564d91e92bac2bcb
aroadant <26831146+aroadant@users.noreply.github.com>
2022-10-28 23:29:19 +0200

    update shift

--- qlat/include/qlat/vector_utils/utils_shift_vecs.h
+++ qlat/include/qlat/vector_utils/utils_shift_vecs.h
@@ -53,7 +53,6 @@ struct shift_vec{
   std::vector<qlat::vector_acc<LInt > > sendoffx;
   std::vector<qlat::vector_acc<LInt > > buffoffa;
   std::vector<qlat::vector_acc<LInt > > buffoffb;
-  std::vector<unsigned int > CON_SEND, CON_BUFF;
 
   std::vector<size_t > MPI_size;
   unsigned int MPI_off;
@@ -66,23 +65,11 @@ struct shift_vec{
 
   move_index mv_civ;
 
-  void* zeroP;LInt zeroP_Size;
-  void* bufsP;LInt bufsP_Size;
-  void* bufrP;LInt bufrP_Size;
-
-  std::vector<void* > sendbufP;
-  std::vector<void* > recvbufP;
-
-  std::vector<LInt > bufP_Size;
-  ////std::vector<size_t > recvbufP_Size;
-
-  //std::vector<Ftype > sendbuf;
-  //std::vector<Ftype > recvbuf;
-
-  //std::vector<int > currsend;
-  //std::vector<int > currspls;
-  //std::vector<int > currrecv;
-  //std::vector<int > currrpls;
+  qlat::vector_gpu<char > zeroP;
+  qlat::vector_gpu<char > bufsP;
+  qlat::vector_gpu<char > bufrP;
+  std::vector<qlat::vector_gpu<char > > sendbufP;
+  std::vector<qlat::vector_gpu<char > > recvbufP;
 
   void* gauge;
   int gbfac;int gd0;
@@ -151,32 +138,23 @@ shift_vec::shift_vec(fft_desc_basic &fds, bool GPU_set)
   vol  = fds.vol;Nvol = fds.Nvol;
 
   Nx=fds.Nx;Ny=fds.Ny;Nz=fds.Nz;
-  //mx=fds.mx;my=fds.my;mz=fds.mz;mt=fds.mt;
   Nv = fds.Nv;nv = fds.nv;
-  //mv = fds.mv;
-  //orderN = fds.orderN;
 
   N0 = fds.Nv[fds.orderN[0]];N1 = fds.Nv[fds.orderN[1]];N2 = fds.Nv[fds.orderN[2]];
   Nt = fds.Nt;
 
-  sendbufP.resize(8);recvbufP.resize(8);bufP_Size.resize(8);MPI_size.resize(8);
-  CON_SEND.resize(8);CON_BUFF.resize(8);
-  for(int i=0;i<8;i++){
-    sendbufP[i] = NULL;recvbufP[i] = NULL;bufP_Size[i] = 0;MPI_size[i] = 0;
-    CON_SEND[i] = 1;CON_BUFF[i] = 1;
-  }
+  sendbufP.resize(8);recvbufP.resize(8);
 
   flag_shift_set = false;bsize = 0;
   dir_cur = 0;biva = -1;civ = -1;
   periodic = 1;
 
   MPI_off = 0;MPI_curr = MPI_CHAR;
-  ///srcP = NULL;resP = NULL;
 
   shift_set();
 
-  zeroP = NULL;bufsP = NULL;bufrP = NULL;
-  zeroP_Size = 0;bufsP_Size = 0;bufrP_Size = 0;
+  //zeroP = NULL;bufsP = NULL;bufrP = NULL;
+  //zeroP_Size = 0;bufsP_Size = 0;bufrP_Size = 0;
 
   gauge = NULL;
   gbfac = 1; gd0 = 1;Conj = false;src_gauge = false;
@@ -194,8 +172,6 @@ void shift_vec::shift_set()
   sendoffa.resize(8);
   sendoffb.resize(8);
   sendoffx.resize(8);
-
-  ////MPI_size.resize(8);
 
   for(int diru=0;diru<8;diru++)
   {
@@ -229,16 +205,8 @@ void shift_vec::shift_set()
         }
       }
     }
-    //double flag = 0.0;
-    ///if(count != (1+Nmpi)){print0("rank %8d. map wrong %8d. \n",rank,count);flag=1.0;}abort_sum(flag);
 
     Length = Nt*Nx*Ny*Nz;
-
-    //buffoffa[diru].resize(0);
-    //buffoffb[diru].resize(0);
-    //sendoffa[diru].resize(0);
-    //sendoffb[diru].resize(0);
-    //sendoffx[diru].resize(0);
 
     sendoffa[diru].resize(0);
     sendoffb[diru].resize(0);
@@ -271,24 +239,15 @@ void shift_vec::shift_set()
       {
         buffoffVa.push_back(off0);
         buffoffVb.push_back(off1);
-        //buffoffa[diru].push_back(off0);
-        //buffoffb[diru].push_back(off1);
-
       }
       else{
         p[dir] = (p[dir] + Nv[dir])%Nv[dir];
         off1 = ((p[3]*N0+p[fd.orderN[0]])*N1+p[fd.orderN[1]])*N2+p[fd.orderN[2]];
         sendoffVa.push_back(off0);
         sendoffVb.push_back(off1);
-        //sendoffa[diru].push_back(off0);
-        //sendoffb[diru].push_back(off1);
-
       }
     }
 
-    //for(LInt ix=0;ix<sendoffa[diru].size();ix++){sendoffx[diru].push_back(ix);}
-
-    //CON_SEND = 1; CON_BUFF = 1;
     /////May check cotinious
 
     sendoffa[diru].resize(sendoffVa.size());
@@ -309,10 +268,6 @@ void shift_vec::shift_set()
       buffoffb[diru][ix] = buffoffVb[ix];
     }
 
-
-    ///reorder_civ((char*) &sendoff[diru][0], (char*) &sendoff[diru][0], 1, 2, sendoff[diru].size()/2, 1, sizeof(sendoff[diru][0]));
-    /////reorder_civ((char*) &buffoff[diru][0], (char*) &buffoff[diru][0], 1, 2, buffoff[diru].size(), 1, sizeof(buffoff[diru][0]));
-
   }
   flag_shift_set = true;
 }
@@ -325,23 +280,11 @@ void shift_vec::set_MPI_size(int biva_or, int civ_or, int dir_or )
   /////zeroP = NULL;bufsP = NULL;bufrP = NULL;
   /////====set up bufs for shift
   LInt Ng = Nt*N0*N1*N2;
-  if(zeroP_Size != Ng){
-  free_buf(zeroP, GPU);zeroP=NULL;
-  if(GPU){gpuMalloc(zeroP, Ng, Ty);}
-  else{zeroP = aligned_alloc_no_acc(Ng * sizeof(Ty));}zeroP_Size = Ng;
-  ////Ty* zero = (Ty*) zeroP;
-  zero_Ty((Ty*) zeroP, Ng, GPU, true);
-  }
+  zeroP.resize(Ng, GPU);
+  zeroP.set_zero();
 
-  if(bufsP_Size != Ng*biva_or*civ_or){
-  free_buf(bufsP, GPU);bufsP=NULL;
-  if(GPU){gpuMalloc(bufsP, Ng*biva_or*civ_or, Ty);}
-  else{bufsP = aligned_alloc_no_acc(Ng*biva_or*civ_or * sizeof(Ty));}bufsP_Size = Ng*biva_or*civ_or;}
-
-  if(bufrP_Size != Ng*biva_or*civ_or){
-  free_buf(bufrP, GPU);bufrP=NULL;
-  if(GPU){gpuMalloc(bufrP, Ng*biva_or*civ_or, Ty);}
-  else{bufrP = aligned_alloc_no_acc(Ng*biva_or*civ_or * sizeof(Ty));}bufrP_Size = Ng*biva_or*civ_or;}
+  bufsP.resize(Ng*biva_or*civ_or);
+  bufrP.resize(Ng*biva_or*civ_or);
   /////====set up bufs for shift
 
   ////==assign current direction
@@ -349,11 +292,8 @@ void shift_vec::set_MPI_size(int biva_or, int civ_or, int dir_or )
   ////==assign current direction
   if(biva_or == biva and civ_or == civ and bsize == sizeof(Ty)){
     if(sendoffa[dir_cur].size() == 0){return ;}else
-    {if(bufP_Size[dir_cur] == (LInt) biva_or*civ_or*(sendoffa[dir_cur].size())*CON_SEND[dir_cur]){return  ;}}
+    {if(sendbufP[dir_cur].size() == (LInt) biva_or*civ_or*(sendoffa[dir_cur].size())){return  ;}}
   }
-
-  //Length = Nt*N0*N1*N2;
-  //flag_set_MPI_size = 1;
 
   if(sizeof(Ty) != bsize){
     bsize = sizeof(Ty);
@@ -367,18 +307,9 @@ void shift_vec::set_MPI_size(int biva_or, int civ_or, int dir_or )
   civ  = civ_or;
   ////===assign biva and civ
 
-  MPI_size[dir_cur] = biva*civ*(sendoffa[dir_cur].size())*CON_SEND[dir_cur];
-  if(bufP_Size[dir_cur] != MPI_size[dir_cur])
-  {
-    clear_mem_dir(dir_cur);
-    if(GPU){gpuMalloc(sendbufP[dir_cur], MPI_size[dir_cur], Ty);}
-    else{sendbufP[dir_cur] = aligned_alloc_no_acc(MPI_size[dir_cur] * sizeof(Ty));}
-
-    if(GPU){gpuMalloc(recvbufP[dir_cur], MPI_size[dir_cur], Ty);}
-    else{recvbufP[dir_cur] = aligned_alloc_no_acc(MPI_size[dir_cur] * sizeof(Ty));}
-
-    bufP_Size[dir_cur] = MPI_size[dir_cur];
-  }
+  MPI_size[dir_cur] = biva*civ*(sendoffa[dir_cur].size());
+  sendbufP[dir_cur].resize(MPI_size[dir_cur]);
+  recvbufP[dir_cur].resize(MPI_size[dir_cur]);
 }
 
 template<typename Ty>
@@ -399,7 +330,7 @@ void shift_vec::print_info()
   for(int di=0;di<8;di++)
   {
     print0("dir %d, bufsize %ld, MPI_size %ld, sendsize %ld, copysize %ld \n", 
-            di, long(bufP_Size[di]), long(MPI_size[di]), sendoffa[di].size(), buffoffa[di].size());
+            di, long(sendbufP[di].size()), long(MPI_size[di]), sendoffa[di].size(), buffoffa[di].size());
   }
   fflush_MPI();
 
@@ -409,9 +340,9 @@ template<typename Ty, int flag>
 void shift_vec::write_send_recv(Ty* src, Ty* res)
 {
   TIMERA("shift_vec::write_send_recv");
-  if(sendoffa[dir_cur].size() != 0 and bufP_Size[dir_cur] == 0){print0("Memeory not set for dir %d", dir_cur);abort_r();}
-  Ty* s_tem = (Ty*) (sendbufP[dir_cur]);
-  Ty* r_tem = (Ty*) (recvbufP[dir_cur]);
+  if(sendoffa[dir_cur].size() != 0 and sendbufP[dir_cur].size() == 0){print0("Memeory not set for dir %d", dir_cur);abort_r();}
+  Ty* s_tem = (Ty*) (sendbufP[dir_cur].data());
+  Ty* r_tem = (Ty*) (recvbufP[dir_cur].data());
   LInt writeN = sendoffa[dir_cur].size();
   if(src == NULL){abort_r("buf not defined");}
   ////print_info();
@@ -423,7 +354,7 @@ void shift_vec::write_send_recv(Ty* src, Ty* res)
   LInt* s0 = (LInt*) qlat::get_data(sendoffa[dir_cur]).data();
   LInt* s1 = (LInt*) qlat::get_data(sendoffx[dir_cur]).data();
   cpy_data_from_index( &s_tem[bi*writeN*civ], &src[bi*Length*civ], 
-       s1, s0, sendoffa[dir_cur].size(), CON_SEND[dir_cur]*civ, GPU, false);
+       s1, s0, sendoffa[dir_cur].size(), civ, GPU, false);
   }
   qacc_barrier(dummy);
 
@@ -436,7 +367,7 @@ void shift_vec::write_send_recv(Ty* src, Ty* res)
   LInt* s1 = (LInt*) qlat::get_data(sendoffb[dir_cur]).data();
   LInt* s0 = (LInt*) qlat::get_data(sendoffx[dir_cur]).data();
   cpy_data_from_index( &res[bi*Length*civ], &r_tem[bi*writeN*civ], 
-        s1, s0, sendoffb[dir_cur].size(), CON_SEND[dir_cur]*civ, GPU, false);
+        s1, s0, sendoffb[dir_cur].size(), civ, GPU, false);
   }
   qacc_barrier(dummy);
 
@@ -448,7 +379,7 @@ void shift_vec::write_send_recv(Ty* src, Ty* res)
   LInt* s1 = (LInt*) qlat::get_data(buffoffb[dir_cur]).data();
   LInt* s0 = (LInt*) qlat::get_data(buffoffa[dir_cur]).data();
   cpy_data_from_index( &res[bi*Length*civ], &src[bi*Length*civ], 
-       s1, s0, buffoffa[dir_cur].size(), CON_BUFF[dir_cur]*civ, GPU, false);
+       s1, s0, buffoffa[dir_cur].size(), civ, GPU, false);
   }
   qacc_barrier(dummy);
   }
@@ -641,7 +572,7 @@ void shift_vec::call_MPI(Ty *src, Ty *res,int dir_or)
   if(!Conj)mult_gauge<Ty, false >((void*) src, dir_gauge);
   if( Conj)mult_gauge<Ty, true  >((void*) src, dir_gauge);}
 
-  Ty* s_tem= (Ty*) sendbufP[dir_cur]; Ty* r_tem= (Ty*) recvbufP[dir_cur];
+  Ty* s_tem= (Ty*) sendbufP[dir_cur].data(); Ty* r_tem= (Ty*) recvbufP[dir_cur].data();
 
   MPI_Request request;
   int tags = omp_get_thread_num()*Nmpi + rank;
@@ -739,7 +670,7 @@ void shift_vec::shift_vecs(std::vector<Ty* > &src,std::vector<Ty* > &res,std::ve
   }
 
   int size_vec = biva_or*civ_or;
-  Ty* zero = (Ty*) zeroP;
+  Ty* zero = (Ty*) zeroP.data();
 
   //print0("Flag %d, civ %d %d , biva %d \n",int(flag_shift_set),civ_or,civ, biva);
   //if(flag_shift_set == false){if(civ_or==1)set_MPI_size<Ty >(1,12);if(civ_or != 1)set_MPI_size<Ty >(1, civ_or);}
@@ -751,8 +682,8 @@ void shift_vec::shift_vecs(std::vector<Ty* > &src,std::vector<Ty* > &res,std::ve
   int count = 0;
   int flagend = 0;
 
-  Ty* vec_s = (Ty*) bufsP;
-  Ty* vec_r = (Ty*) bufrP;
+  Ty* vec_s = (Ty*) bufsP.data();
+  Ty* vec_r = (Ty*) bufrP.data();
 
   for(int fftn=0;fftn< (size_vec+biva*civ-1)/(biva*civ);fftn++)
   {
@@ -861,14 +792,6 @@ void shift_vec::shift_vecP(Ty* src, Ty* res, std::vector<int >& iDir, int civ_or
 template<typename Ty>
 void shift_vec::shift_Evec(std::vector<qlat::vector_acc<Ty > > &srcE,std::vector<qlat::vector_acc<Ty > > &srcEf,std::vector<int >& iDir,int civ_or)
 {
-  //////#if PRINT_TIMER>4
-  //TIMER_FLOPS("shift_Evec");
-  //{
-  //int count = 1; for(int di=0;di<iDir.size();di++){count += int(std::abs(iDir[di]));}
-  //timer.flops += count * srcE.size() * get_data(srcE[0]).data_size() ;
-  //}
-  //////#endif
-
   int flag_abort=0;
   if(srcE.size()==0){print0("Cannot do it with srcE.size()==0");flag_abort=1;}
   if(civ_or<=0){print0("Cannot do it with civ_or==0");flag_abort=1;}
@@ -925,10 +848,8 @@ void shift_vec::shift_vecs_dir(std::vector<qlat::FieldM<Ty , civ_> >& src, std::
 }
 
 void shift_vec::clear_mem_dir(int dir){
-  free_buf(sendbufP[dir],GPU);sendbufP[dir]=NULL;
-  free_buf(recvbufP[dir],GPU);recvbufP[dir]=NULL;
-  bufP_Size[dir] = 0;
-  //MPI_size[dir_cur] = 0;
+  sendbufP[dir].resize(0);
+  recvbufP[dir].resize(0);
 }
 
 shift_vec::~shift_vec(){
@@ -939,9 +860,9 @@ shift_vec::~shift_vec(){
   for(int dir=0;dir<8;dir++){clear_mem_dir(dir);}
   MPI_size.resize(0);
 
-  free_buf(zeroP, GPU);free_buf(bufsP, GPU);free_buf(bufrP, GPU);
-  zeroP_Size = 0;bufsP_Size = 0;bufrP_Size = 0;
-  zeroP = NULL;bufsP = NULL;bufrP = NULL;
+  zeroP.resize(0);
+  bufsP.resize(0);
+  bufrP.resize(0);
 
   rank_sr.resize(0);
   buffoffa.resize(0);
@@ -951,31 +872,6 @@ shift_vec::~shift_vec(){
   sendoffx.resize(0);
 
 }
-
-//template <class Ty>
-//void shift_fieldM(shift_vec& svec, Ty* src, Ty* res, const int dir, const int civ)
-//{
-//  if(src.size() < 1)return;
-//  int biva_or = src.size();
-//  std::vector<int > iDir(4);
-//  for(int i=0;i<4;i++){iDir[i] = 0;}
-//  if(dir >= 0){iDir[  dir ] = 1;}
-//  if(dir <  0){iDir[-dir-1] = -1;}
-//
-//  long Nvol = svec.Nvol;
-//  qassert(src.size() == res.size());
-//  qassert(src.size()%Nvol == 0);
-//
-//  std::vector<Ty* > srcP;std::vector<Ty* > resP;
-//  srcP.resize(biva_or);resP.resize(biva_or);
-//  for(int bi=0;bi<biva_or;bi++){
-//    srcP[bi] = (Ty*) src.qlat::get_data(src[bi]).data();
-//    resP[bi] = (Ty*) src.qlat::get_data(res[bi]).data();
-//  }
-//
-//  svec.shift_vecs(srcP, resP, iDir , civ);
-//}
-
 
 template <class Ty, int civ>
 void shift_fieldM(shift_vec& svec, std::vector<qlat::FieldM<Ty, civ> >& src, std::vector<qlat::FieldM<Ty, civ> >& res, std::vector<int >& iDir)
