db65561bf7c6497b89f195874b932eeacc36c986
Luchang Jin <ljin.luchang@gmail.com>
2022-11-06 06:43:47 -0500

    update

--- scripts/build.default-gpt.sh
+++ scripts/build.default-gpt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Need to have gcc, eigen3, mpi, fftw3-mpi, python3 (numpy, simpy, psutil, meson), openssl, ninja, zlib, gsl installed beforehand.
+# Need to have gcc, eigen3, mpi, fftw3, fftw3-mpi, python3 (numpy, simpy, psutil, meson), openssl, ninja, zlib, gsl installed beforehand.
 
-# Ubuntu packages: libgsl-dev zlib1g-dev libssl-dev libeigen3-dev libopenmpi-dev libfftw3-mpi-dev python3-sympy python3-numpy python3-scipy python3-psutil libmpfr-dev meson ninja-build
+# Ubuntu packages: libgsl-dev zlib1g-dev libssl-dev libeigen3-dev libopenmpi-dev libfftw3-dev libfftw3-mpi-dev python3-sympy python3-numpy python3-scipy python3-psutil libmpfr-dev meson ninja-build
 
 set -e
 
