e43da59611a9869ced8702e61ddeb94e90fdd255
Luchang Jin <ljin.luchang@gmail.com>
2022-11-07 00:15:56 -0500

    simplify weight

--- applications/auto-contract-meson-decay/run.py
+++ applications/auto-contract-meson-decay/run.py
@@ -978,13 +978,13 @@ def auto_contract_meson_jwjj(job_tag, traj, get_prop, get_psel, get_fsel):
         t = (t_op - t_meson) % t_size
         return abs(arr_meson_corr[meson_type, t].item())
     def get_prop_norm_sqrt(*args):
-        is_sloppy = False
-        return ama_extract(get_prop(*args, is_norm_sqrt = True), is_sloppy = is_sloppy)
+        is_sloppy = True
+        return abs(ama_extract(get_prop(*args, is_norm_sqrt = True), is_sloppy = is_sloppy))
     def get_estimate(xg_snk, xg1_src, xg2_src, t_1, t_2):
         corr1 = get_corr(xg_snk[3], t_1, 0)
-        corr1s = get_corr(xg_snk[3], t_1, 1)
+        # corr1s = get_corr(xg_snk[3], t_1, 1)
         corr2 = get_corr(xg_snk[3], t_2, 0)
-        corr2s = get_corr(xg_snk[3], t_2, 1)
+        # corr2s = get_corr(xg_snk[3], t_2, 1)
         w = ("point-snk", xg_snk,)
         x_1 = ("point", xg1_src,)
         x_2 = ("point", xg2_src,)
@@ -996,37 +996,39 @@ def auto_contract_meson_jwjj(job_tag, traj, get_prop, get_psel, get_fsel):
         p2t2 = get_prop_norm_sqrt("l", x_2, t_2s)
         wt1 = get_prop_norm_sqrt("l", w, t_1s)
         wt2 = get_prop_norm_sqrt("l", w, t_2s)
-        p1t1s = get_prop_norm_sqrt("s", x_1, t_1s)
-        p2t1s = get_prop_norm_sqrt("s", x_2, t_1s)
-        p1t2s = get_prop_norm_sqrt("s", x_1, t_2s)
-        p2t2s = get_prop_norm_sqrt("s", x_2, t_2s)
-        wt1s = get_prop_norm_sqrt("s", w, t_1s)
-        wt2s = get_prop_norm_sqrt("s", w, t_2s)
         wp1 = get_prop_norm_sqrt("l", x_1, w)
         wp2 = get_prop_norm_sqrt("l", x_2, w)
-        wp1s = get_prop_norm_sqrt("s", x_1, w)
-        wp2s = get_prop_norm_sqrt("s", x_2, w)
         p1p2 = get_prop_norm_sqrt("l", x_1, x_2)
-        p1p2s = get_prop_norm_sqrt("s", x_1, x_2)
+        # p1t1s = get_prop_norm_sqrt("s", x_1, t_1s)
+        # p2t1s = get_prop_norm_sqrt("s", x_2, t_1s)
+        # p1t2s = get_prop_norm_sqrt("s", x_1, t_2s)
+        # p2t2s = get_prop_norm_sqrt("s", x_2, t_2s)
+        # wt1s = get_prop_norm_sqrt("s", w, t_1s)
+        # wt2s = get_prop_norm_sqrt("s", w, t_2s)
+        # wp1s = get_prop_norm_sqrt("s", x_1, w)
+        # wp2s = get_prop_norm_sqrt("s", x_2, w)
+        # p1p2s = get_prop_norm_sqrt("s", x_1, x_2)
         values = [
                 2 * (p1t1 * p2t1 / corr1 + p1t2 * p2t2 / corr2) * (wp1 * wp2),
-                2 * (p1t1s * p2t1 / corr1s + p1t2s * p2t2 / corr2s) * (wp1s * wp2),
-                2 * (p1t1 * p2t1s / corr1s + p1t2 * p2t2s / corr2s) * (wp1 * wp2s),
+                # 2 * (p1t1s * p2t1 / corr1s + p1t2s * p2t2 / corr2s) * (wp1s * wp2),
+                # 2 * (p1t1 * p2t1s / corr1s + p1t2 * p2t2s / corr2s) * (wp1 * wp2s),
                 5 * (p1t1 * wt1 / corr1 + p1t2 * wt2 / corr2) * (p1p2 * wp2),
-                4 * (p1t1 * wt1s / corr1s + p1t2 * wt2s / corr2s) * (p1p2 * wp2),
-                1 * (p1t1s * wt1 / corr1s + p1t2s * wt2 / corr2s) * (p1p2s * wp2s),
+                # 4 * (p1t1 * wt1s / corr1s + p1t2 * wt2s / corr2s) * (p1p2 * wp2),
+                # 1 * (p1t1s * wt1 / corr1s + p1t2s * wt2 / corr2s) * (p1p2s * wp2s),
                 5 * (p2t1 * wt1 / corr1 + p2t2 * wt2 / corr2) * (p1p2 * wp1),
-                4 * (p2t1 * wt1s / corr1s + p2t2 * wt2s / corr2s) * (p1p2 * wp1),
-                1 * (p2t1s * wt1 / corr1s + p2t2s * wt2 / corr2s) * (p1p2s * wp1s),
+                # 4 * (p2t1 * wt1s / corr1s + p2t2 * wt2s / corr2s) * (p1p2 * wp1),
+                # 1 * (p2t1s * wt1 / corr1s + p2t2s * wt2 / corr2s) * (p1p2s * wp1s),
                 ]
-        q.displayln_info(6, f"get_estimate: {values}")
-        return sum(values)
+        value = sum(values)
+        assert value > 0
+        # q.displayln_info(6, f"get_estimate: {value} {values}")
+        return value
     @q.timer
     def get_weight(idx_snk, xg_snk, xg1_src, xg2_src, t_1, t_2):
         # return weight for this point (1 / prob or zero)
         est = get_estimate(xg_snk, xg1_src, xg2_src, t_1, t_2)
         prob = est / threshold
-        q.displayln_info(5, f"get_weight: {prob} {est} {threshold}")
+        # q.displayln_info(5, f"get_weight: {prob} {est} {threshold}")
         if prob >= 1:
             return 1
         rand = u_rand_prob.get_elem(idx_snk).item()
@@ -1097,7 +1099,7 @@ def auto_contract_meson_jwjj(job_tag, traj, get_prop, get_psel, get_fsel):
         return counts, values
     q.timer_fork(0)
     res_count, res_sum = q.glb_sum(
-            q.parallel_map_sum(feval, load_data(), sum_function = sum_function, chunksize = 16))
+            q.parallel_map_sum(feval, load_data(), sum_function = sum_function, chunksize = 64))
     q.displayln_info("timer_display for auto_contract_meson_jj")
     q.timer_display()
     q.timer_merge()
