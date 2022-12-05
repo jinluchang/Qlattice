2f0736980af86e5ffcafe3bcc0dc8b88248c949e
Luchang Jin <ljin.luchang@gmail.com>
2022-12-04 23:23:30 -0500

    small improvement

--- qlat-utils/pylib/qlat_utils/cp.pyx
+++ qlat-utils/pylib/qlat_utils/cp.pyx
@@ -1,6 +1,6 @@
 # cython: c_string_type=unicode, c_string_encoding=utf8
 
-from .cp cimport *
+from . cimport __init__ as cp
 
 def timer_display(tag = ""):
-    Timer.display(tag)
+    cp.Timer.display(tag)
