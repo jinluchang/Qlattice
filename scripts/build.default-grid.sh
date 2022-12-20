37ed59082cfb93890197e0e1a658652c5d968af4
Luchang Jin <ljin.luchang@gmail.com>
2022-12-20 00:57:53 -0500

    small

---
+++ scripts/build.default-grid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Need to have gcc, eigen3, mpi, fftw3, fftw3-mpi, python3 (numpy, simpy, psutil, meson), openssl, ninja, zlib, gsl installed beforehand.
+
+# Ubuntu packages: libgsl-dev zlib1g-dev libssl-dev libeigen3-dev libopenmpi-dev libfftw3-dev libfftw3-mpi-dev python3-sympy python3-numpy python3-scipy python3-psutil libmpfr-dev meson ninja-build
+
+set -e
+
+{
+
+./scripts/setenv.default.sh
+
+./scripts/c-lime.sh
+./scripts/grid.avx2.sh
+
+} 2>&1 | tee $prefix/log.build.txt
