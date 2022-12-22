0270aeb523902c05ea141a00203e458aa17063eb
Luchang Jin <ljin.luchang@gmail.com>
2022-12-22 15:40:04 -0500

    add grid neonv8

---
+++ scripts/grid.neonv8.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+. scripts/conf.sh
+
+name=Grid
+
+{
+
+    time {
+
+    echo "!!!! build $name !!!!"
+
+    mkdir -p "$prefix"/$name || true
+
+    rsync -av --delete $distfiles/$name-lehner/ "$prefix"/$name/
+
+    cd "$prefix/$name"
+
+    INITDIR="$(pwd)"
+    rm -rfv "${INITDIR}/Eigen/Eigen/unsupported"
+    rm -rfv "${INITDIR}/Grid/Eigen"
+    ln -vs "${INITDIR}/Eigen/Eigen" "${INITDIR}/Grid/Eigen"
+    ln -vs "${INITDIR}/Eigen/unsupported/Eigen" "${INITDIR}/Grid/Eigen/unsupported"
+
+    export CXXFLAGS="$CXXFLAGS -fPIC"
+
+    mkdir build
+    cd build
+    ../configure \
+        --enable-simd=NEONv8 \
+        --enable-alloc-align=4k \
+        --enable-comms=mpi-auto \
+        --enable-gparity=no \
+        --with-lime="$prefix" \
+        --with-fftw="$prefix" \
+        --prefix="$prefix"
+
+    make -j$num_proc
+    make install
+
+    cd $wd
+    echo "!!!! $name build !!!!"
+
+    rm -rf $temp_dir || true
+
+}
+
+} 2>&1 | tee $prefix/log.$name.txt
