38c86bbf7b91d9ceea688ccc7ab47d702ac25292
Luchang Jin <ljin.luchang@gmail.com>
2022-12-25 01:05:30 -0500

    add more functions to lib

--- qlat/include/qlat/lib/mat.h
+++ qlat/include/qlat/lib/mat.h
@@ -12,4 +12,14 @@ Complex mat_tr(const WilsonMatrix& m1, const SpinMatrix& m2);
 Complex mat_tr(const SpinMatrix& m1, const WilsonMatrix& m2);
 Complex mat_tr(const SpinMatrix& m1, const SpinMatrix& m2);
 
+SpinMatrix operator*(const SpinMatrix& m1, const SpinMatrix& m2);
+SpinMatrix operator*(const Complex& a, const SpinMatrix& m);
+SpinMatrix operator*(const SpinMatrix& m, const Complex& a);
+
+WilsonMatrix operator*(const WilsonMatrix& m1, const WilsonMatrix& m2);
+WilsonMatrix operator*(const Complex& a, const WilsonMatrix& m);
+WilsonMatrix operator*(const WilsonMatrix& m, const Complex& a);
+WilsonMatrix operator*(const SpinMatrix& m1, const WilsonMatrix& m2);
+WilsonMatrix operator*(const WilsonMatrix& m1, const SpinMatrix& m2);
+
 }  // namespace qlat
--- qlat/lib/mat.cpp
+++ qlat/lib/mat.cpp
@@ -31,4 +31,44 @@ Complex mat_tr(const SpinMatrix& m1, const SpinMatrix& m2)
   return matrix_trace(m1, m2);
 }
 
+SpinMatrix operator*(const SpinMatrix& m1, const SpinMatrix& m2)
+{
+  return operator*<4, Complex >(m1, m2);
+}
+
+SpinMatrix operator*(const Complex& a, const SpinMatrix& m)
+{
+  return operator*<4, Complex >(a, m);
+}
+
+SpinMatrix operator*(const SpinMatrix& m, const Complex& a)
+{
+  return operator*<4, Complex >(m, a);
+}
+
+WilsonMatrix operator*(const WilsonMatrix& m1, const WilsonMatrix& m2)
+{
+  return operator*<12, Complex >(m1, m2);
+}
+
+WilsonMatrix operator*(const Complex& a, const WilsonMatrix& m)
+{
+  return operator*<12, Complex >(a, m);
+}
+
+WilsonMatrix operator*(const WilsonMatrix& m, const Complex& a)
+{
+  return operator*<12, Complex >(m, a);
+}
+
+WilsonMatrix operator*(const SpinMatrix& m1, const WilsonMatrix& m2)
+{
+  return operator*<Real>(m1, m2);
+}
+
+WilsonMatrix operator*(const WilsonMatrix& m1, const SpinMatrix& m2)
+{
+  return operator*<Real>(m1, m2);
+}
+
 }  // namespace qlat
