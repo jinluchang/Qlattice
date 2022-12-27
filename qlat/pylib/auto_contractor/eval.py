77209424f7226d5df55448f6a4c19c4724570026
Luchang Jin <ljin.luchang@gmail.com>
2022-12-27 03:15:52 -0500

    small fix

--- qlat/pylib/auto_contractor/eval.py
+++ qlat/pylib/auto_contractor/eval.py
@@ -42,7 +42,7 @@ import subprocess
 def load_prop(x):
     if isinstance(x, tuple):
         assert len(x) == 2 and x[0] == "g5_herm"
-        return ama_apply1(as_wilson_matrix_g5_herm(x[1]))
+        return ama_apply1(as_wilson_matrix_g5_herm, x[1])
     return ama_apply1(as_wilson_matrix, x)
 
 @q.timer
