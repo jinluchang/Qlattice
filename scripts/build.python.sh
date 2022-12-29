69e29de218e23dc65e03101f13407211322eb5c0
Luchang Jin <ljin.luchang@gmail.com>
2022-12-29 17:28:28 -0500

    small

--- scripts/build.python.sh
+++ scripts/build.python.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+# Need to have gcc, openssl, zlib, gsl, openblas, perl, ffi installed beforehand.
+
+# Ubuntu packages: libgsl-dev zlib1g-dev libssl-dev libmpfr-dev gcc perl libopenblas-dev libffi-dev
+
 set -e
 
 {
 
 ./scripts/setenv.default.sh
 
-./scripts/perl.sh
-./scripts/openssl.sh
-./scripts/libffi.sh
-./scripts/openblas.sh
 ./scripts/python.sh
 ./scripts/python-pip.sh
 ./scripts/re2c.sh
