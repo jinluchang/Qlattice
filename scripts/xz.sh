126a4ab699b7ff1042b99a3318639e03b4fa1c73
Luchang Jin <ljin.luchang@gmail.com>
2023-01-29 02:09:26 -0500

    update some scripts

--- scripts/xz.sh
+++ scripts/xz.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-. scripts/res/conf.sh
-
 name=xz
+source qcore/set-prefix.sh $name
 
-{
+{ time {
+    echo "!!!! build $name !!!!"
+    source qcore/conf.sh ..
 
-echo "!!!! build $name !!!!"
+    rm -rf $src_dir || true
+    mkdir -p $src_dir || true
+    cd $src_dir
+    tar xaf $distfiles/$name-*
 
-rm -rf $src_dir || true
-mkdir -p $src_dir || true
-cd $src_dir
-tar xaf $distfiles/$name-*
+    rm -rf $build_dir || true
+    mkdir -p $build_dir || true
+    cd $build_dir
 
-rm -rf $build_dir || true
-mkdir -p $build_dir || true
-cd $build_dir
+    $src_dir/$name-*/configure \
+        --prefix=$prefix
 
-$src_dir/$name-*/configure \
-    --prefix=$prefix
-make -j$num_proc
-make install
+    make -j$num_proc
+    make install
 
-cd $wd
-echo "!!!! $name build !!!!"
-
-rm -rf $temp_dir || true
-
-} 2>&1 | tee $prefix/log.$name.txt
+    mk-setenv.sh
+    echo "!!!! $name build !!!!"
+    rm -rf $temp_dir || true
+} } 2>&1 | tee $prefix/log.$name.txt
