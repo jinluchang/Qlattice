224971207567772a798495c8a27e0f1e3f0b7252
Luchang Jin <ljin.luchang@gmail.com>
2023-01-30 04:08:27 -0500

    hadrons uses temp_dir

--- scripts/hadrons-tblum.sh
+++ scripts/hadrons-tblum.sh
@@ -5,16 +5,12 @@ name=Hadrons-tblum
 source qcore/set-prefix.sh $name
 
 { time {
-
     echo "!!!! build $name !!!!"
-
     source qcore/conf.sh ..
 
-    mkdir -p "$prefix"/src || true
-
-    time-run rsync -a --delete $distfiles/$name/ "$prefix"/src
-
-    cd "$prefix/src"
+    mkdir -p "$src_dir" || true
+    time-run rsync -a --delete $distfiles/$name "$src_dir"/
+    cd "$src_dir/$name"
 
     if which qlat-include >/dev/null 2>&1 ; then
         for v in $(qlat-include) ; do
@@ -23,9 +19,7 @@ source qcore/set-prefix.sh $name
     fi
 
     mkdir build
-
     cd build
-
     time-run ../configure \
         --with-grid="$(find-library.py libGrid.a)" \
         --prefix="$prefix"
@@ -33,12 +27,7 @@ source qcore/set-prefix.sh $name
     time-run make -j "$num_proc"
     time-run make install
 
-    cd "$wd"
-
     mk-setenv.sh
-
     echo "!!!! $name build !!!!"
-
     rm -rf $temp_dir || true
-
 } } 2>&1 | tee $prefix/log.$name.txt
--- scripts/hadrons.sh
+++ scripts/hadrons.sh
@@ -5,21 +5,15 @@ name=Hadrons
 source qcore/set-prefix.sh $name
 
 { time {
-
     echo "!!!! build $name !!!!"
-
     source qcore/conf.sh ..
 
-    mkdir -p "$prefix"/src || true
-
-    time-run rsync -a --delete $distfiles/$name/ "$prefix"/src
-
-    cd "$prefix/src"
+    mkdir -p "$src_dir" || true
+    time-run rsync -a --delete $distfiles/$name "$src_dir"/
+    cd "$src_dir/$name"
 
     mkdir build
-
     cd build
-
     time-run ../configure \
         --with-grid="$(find-library.py libGrid.a)" \
         --prefix="$prefix" \
@@ -27,12 +21,7 @@ source qcore/set-prefix.sh $name
     time-run make -j "$num_proc"
     time-run make install
 
-    cd $wd
-
     mk-setenv.sh
-
     echo "!!!! $name build !!!!"
-
     rm -rf $temp_dir || true
-
 } } 2>&1 | tee $prefix/log.$name.txt
