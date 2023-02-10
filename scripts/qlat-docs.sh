1a027fba0b9fc885130daf13f65b1cb78699c562
Luchang Jin <ljin.luchang@gmail.com>
2023-02-10 03:53:49 -0500

    add missing latex package

--- scripts/qlat-docs.sh
+++ scripts/qlat-docs.sh
@@ -9,18 +9,18 @@ source qcore/set-prefix.sh $name
     source qcore/conf.sh ..
 
     mkdir -p "$build_dir"
-    rsync -a --delete "$wd"/docs/ "$build_dir"/
+    time-run rsync -a --delete "$wd"/docs/ "$build_dir"/
 
     cd "$build_dir"
 
     rm -rfv generated _build
 
-    make html || true
-    make latexpdf || true
+    time-run make html || true
+    time-run make latexpdf || true
 
     mkdir -p "$prefix"/share/doc/qlat
 
-    rsync -a --delete "$build_dir"/_build/ "$prefix"/share/doc/qlat
+    time-run rsync -a --delete "$build_dir"/_build/ "$prefix"/share/doc/qlat
 
     echo "!!!! $name build !!!!"
     rm -rf "$temp_dir" || true
