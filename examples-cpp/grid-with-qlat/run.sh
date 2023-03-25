29c9470323adb17931d16a5e5a7aa9864db57569
Luchang Jin <ljin.luchang@gmail.com>
2023-03-25 16:23:48 -0400

    update examples cpp run.sh

--- examples-cpp/grid-with-qlat/run.sh
+++ examples-cpp/grid-with-qlat/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pwd
-q_verbose=10 time mpirun --oversubscribe -x OMP_NUM_THREADS=2 --np 2 ./qlat.x >log.out 2>log.err
-cat log.out | grep -v '^Timer\|^check_status:\|^display_geo_node : id_node =\|^Grid : Message' >log
+q_verbose=10 OMP_NUM_THREADS=2 time mpirun --np 2 ./qlat.x >log.out 2>log.err
+cat log.out | grep -v '^Timer\|^check_status:\|^display_geo_node : id_node =' >log
 cat log.out log.err > log.full
