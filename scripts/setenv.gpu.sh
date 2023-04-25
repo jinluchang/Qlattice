2105ad26599bfa9202e05e2b31fad34f92def87d
Luchang Jin <ljin.luchang@gmail.com>
2023-04-24 22:42:02 -0400

    small

--- scripts/setenv.gpu.sh
+++ scripts/setenv.gpu.sh
@@ -13,7 +13,8 @@ cat >"$prefix/setenv.sh" <<EOF
 if [ -z "\$num_proc" ] ; then
     export num_proc=2
 fi
-export NVCC_OPTIONS="-w -std=c++14 -arch=sm_86 --expt-extended-lambda --expt-relaxed-constexpr -fopenmp -fno-strict-aliasing"
+export NVCC_ARCH="sm_86"
+export NVCC_OPTIONS="-w -std=c++14 -arch=\$NVCC_ARCH --expt-extended-lambda --expt-relaxed-constexpr -fopenmp -fno-strict-aliasing"
 export QLAT_CXX="NVCC.py -ccbin CXX.sh \$NVCC_OPTIONS"
 export QLAT_MPICXX="NVCC.py -ccbin MPICXX.sh \$NVCC_OPTIONS"
 export QLAT_CXXFLAGS="--NVCC-compile" # -fPIC 
