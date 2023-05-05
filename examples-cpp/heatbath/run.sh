935653ba92aaaa09bfaabca79ae06a32247c23ab
Luchang Jin <ljin.luchang@gmail.com>
2023-05-05 16:31:07 -0400

    small fix the scripts

--- examples-cpp/heatbath/run.sh
+++ examples-cpp/heatbath/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pwd
-q_verbose=10 OMP_NUM_THREADS=2 time mpiexec -n 2 $MPI_OPTIONS ./qlat.x >log.out 2>log.err || q_verbose=10 OMP_NUM_THREADS=2 time mpiexec -n 2 --oversubscribe $MPI_OPTIONS ./qlat.x >log.out 2>log.err
-cat log.out | grep -v '^Grid :\|^Timer\|^check_status:\|^display_geo_node : id_node =' >log
+q_verbose=10 OMP_NUM_THREADS=2 time mpiexec -n 4 $MPI_OPTIONS ./qlat.x >log.out 2>log.err || q_verbose=10 OMP_NUM_THREADS=2 time mpiexec -n 4 --oversubscribe $MPI_OPTIONS ./qlat.x >log.out 2>log.err
+cat log.out | grep -v '^Grid :\|^Timer\|^check_status:\|^display_geometry_node : id_node =' >log
 cat log.out log.err > log.full
