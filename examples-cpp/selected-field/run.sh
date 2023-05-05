b98944e6f38034db4c8d4d7f829987251c20514c
Luchang Jin <ljin.luchang@gmail.com>
2023-05-05 15:33:06 -0400

    update run.sh

--- examples-cpp/selected-field/run.sh
+++ examples-cpp/selected-field/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pwd
-q_verbose=10 OMP_NUM_THREADS=2 time mpiexec $MPI_OPTIONS -n 4 ./qlat.x >log.out 2>log.err || q_verbose=10 OMP_NUM_THREADS=2 time mpiexec $MPI_OPTIONS -n 4 --oversubscribe ./qlat.x >log.out 2>log.err
+q_verbose=10 OMP_NUM_THREADS=2 time mpiexec -n 4 $MPI_OPTIONS ./qlat.x >log.out 2>log.err || q_verbose=10 OMP_NUM_THREADS=2 time mpiexec $MPI_OPTIONS -n 2 --oversubscribe ./qlat.x >log.out 2>log.err
 cat log.out | grep -v '^Timer\|^check_status:\|^display_geo_node : id_node =' >log
 cat log.out log.err > log.full
