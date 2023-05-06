11ec915069250a32c1480003f766ee28f2167a0c
Luchang Jin <ljin.luchang@gmail.com>
2023-05-06 14:28:09 -0400

    improve gpt-test.sh

--- scripts/gpt-test.sh
+++ scripts/gpt-test.sh
@@ -10,13 +10,25 @@ source qcore/set-prefix.sh $name
 
     cd "$prefix"
 
+    value="$PYTHONPATH"
+    if [ -n "$value" ] ; then
+        IFS=':' read -a vs <<< "$value"
+        for v in "${vs[@]}" ; do
+            if [ -d "$v"/"gpt" ] ; then
+                gpt_path="$v"
+                echo "gpt_path='$gpt_path'"
+                break
+            fi
+        done
+    fi
+
     # grid_options="--dslash-asm --shm-hugepages --shm 4050"
     # grid_options="--dslash-asm"
     grid_options=""
 
     geo_options="--grid 16.16.16.16 --mpi 1.1.1.1"
 
-    echo 'import numpy' | cat - "$prefix"/../gpt/src/benchmarks/dslash.py > dslash.py
+    cat "$gpt_path"/../../../src/benchmarks/dslash.py > dslash.py
     OMP_NUM_THREADS=4 time-run python3 dslash.py $grid_options $geo_options --Ls 12 --N 10
 
     echo "!!!! $name build !!!!"
