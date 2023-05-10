281cf5e677354ba0ad65e3e228f03e8a2c7bae37
Luchang Jin <ljin.luchang@gmail.com>
2023-05-10 14:09:37 -0400

    update examples

---
+++ examples-py/jackknife-random.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import qlat as q
+import numpy as np
+import functools
+
+q.begin_with_mpi()
+
+q.default_g_jk_kwargs["jk_type"] = "rjk"
+q.default_g_jk_kwargs["n_rand_sample"] = 1024
+q.default_g_jk_kwargs["rng_state"] = q.RngState("rejk")
+
+@functools.cache
+def get_trajs(job_tag):
+    return list(range(25))
+
+rs = q.RngState("seed")
+job_tag = "test1"
+trajs = get_trajs(job_tag)
+
+data_list = np.zeros((len(trajs), 5,)) # can be list or np.array
+rs.g_rand_fill(data_list)
+jk_list = q.g_jk(data_list)
+jk_idx_list = [ "avg", ] + [ (job_tag, traj) for traj in trajs ]
+jk_list = q.g_rejk(jk_list, jk_idx_list)
+avg, err = q.g_jk_avg_err(jk_list)
+
+q.displayln_info(f"CHECK: {avg}")
+q.displayln_info(f"CHECK: {err}")
+
+q.displayln_info(f"CHECK: finished successfully.")
+
+q.end_with_mpi()
---
+++ examples-py/jackknife-super.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import qlat as q
+import numpy as np
+import functools
+
+q.begin_with_mpi()
+
+q.default_g_jk_kwargs["jk_type"] = "super"
+@functools.cache
+def get_all_jk_idx():
+    all_job_tag = [ 'test1', 'test2', ]
+    jk_idx_list = [ 'avg', ]
+    for job_tag in all_job_tag:
+        trajs = get_trajs(job_tag)
+        for traj in trajs:
+            jk_idx_list.append((job_tag, traj,))
+    return jk_idx_list
+q.default_g_jk_kwargs["get_all_jk_idx"] = get_all_jk_idx
+
+@functools.cache
+def get_trajs(job_tag):
+    return list(range(25))
+
+rs = q.RngState("seed")
+job_tag = "test1"
+trajs = get_trajs(job_tag)
+
+data_list = np.zeros((len(trajs), 5,)) # can be list or np.array
+rs.g_rand_fill(data_list)
+jk_list = q.g_jk(data_list)
+jk_idx_list = [ "avg", ] + [ (job_tag, traj) for traj in trajs ]
+jk_list = q.g_rejk(jk_list, jk_idx_list)
+avg, err = q.g_jk_avg_err(jk_list)
+
+q.displayln_info(f"CHECK: {avg}")
+q.displayln_info(f"CHECK: {err}")
+
+q.displayln_info(f"CHECK: finished successfully.")
+
+q.end_with_mpi()
