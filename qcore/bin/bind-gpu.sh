2e9ada2029059419f0fb907884b1b0e1b81e8d55
Luchang Jin <ljin.luchang@gmail.com>
2023-05-20 23:55:15 -0400

    fix many scripts

--- qcore/bin/bind-gpu.sh
+++ qcore/bin/bind-gpu.sh
@@ -2,6 +2,10 @@
 
 NGPU="$(nvidia-smi -L | wc -l)"
 
+if [ -z "$NGPU" ] ; then
+    NGPU=1
+fi
+
 if [ -n "$OMPI_COMM_WORLD_LOCAL_RANK" ] ; then
     export CUDA_VISIBLE_DEVICES="$((OMPI_COMM_WORLD_LOCAL_RANK % NGPU))"
 fi
