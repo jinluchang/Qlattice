d758bac90e045be07d0fed6e787af7d32a3e105c
Luchang Jin <ljin.luchang@gmail.com>
2023-05-21 02:10:14 -0400

    small fix

--- applications/mk-psrc-prop/run.py
+++ applications/mk-psrc-prop/run.py
@@ -79,7 +79,6 @@ def run_job(job_tag, traj):
     def run_with_eig_strange():
         get_eig = run_eig_strange(job_tag, traj_gf, get_gf)
         run_get_inverter(job_tag, traj, inv_type = 1, get_gf = get_gf, get_eig = get_eig)
-        run_prop(inv_type = 1, get_eig = get_eig)
         run_prop_psrc(job_tag, traj, inv_type = 1, get_gf = get_gf, get_eig = get_eig, get_gt = get_gt, get_psel = get_psel, get_fsel = get_fsel)
     #
     run_with_eig()
