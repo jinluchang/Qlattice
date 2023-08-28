165c4fa792bfc09094c334df9fd150be7da6c4f8
Luchang Jin <ljin.luchang@gmail.com>
2023-08-28 15:33:44 -0400

    bug fix in auto check (diagram type association)

--- applications/auto-check/run.py
+++ applications/auto-check/run.py
@@ -15,6 +15,7 @@ import qlat_gpt as qg
 
 from qlat_scripts.v1 import *
 
+# ADJUST ME
 is_cython = False
 
 # ----
@@ -679,6 +680,7 @@ def get_cexpr_meson_meson_i0_j0_corr_wf():
     fn_base = f"cache/auto_contract_cexpr/{fname}"
     def calc_cexpr():
         diagram_type_dict = dict()
+        #
         diagram_type_dict[()] = 'TypeV'
         diagram_type_dict[((('x1_1', 'x1_2'), 1),)] = 'TypeV'
         diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x1_3', 'x1_4'), 1))] = 'TypeV'
@@ -687,7 +689,6 @@ def get_cexpr_meson_meson_i0_j0_corr_wf():
         diagram_type_dict[((('x2_1', 'x2_2'), 1), (('x2_3', 'x2_4'), 1))] = 'TypeV'
         diagram_type_dict[((('x2_1', 'x2_2'), 1), (('x2_3p', 'x2_4p'), 1))] = 'TypeV'
         diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x2_1', 'x2_2'), 1))] = 'TypeV'
-        diagram_type_dict[((('x1_1', 'x2_1'), 1), (('x2_2', 'x1_2'), 1))] = 'TypeV'
         diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x2_1', 'x2_2'), 1), (('x2_3', 'x2_4'), 1))] = 'TypeV'
         diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x2_1', 'x2_2'), 1), (('x2_3p', 'x2_4p'), 1))] = 'TypeV'
         diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x1_3', 'x1_4'), 1), (('x2_1', 'x2_2'), 1))] = 'TypeV'
@@ -700,14 +701,16 @@ def get_cexpr_meson_meson_i0_j0_corr_wf():
         diagram_type_dict[((('x1_1', 'x2_1'), 1), (('x2_2', 'x1_2'), 1), (('x2_3', 'x2_4'), 1))] = 'TypeR'
         diagram_type_dict[((('x1_1', 'x2_1'), 1), (('x2_2', 'x1_2'), 1), (('x2_3p', 'x2_4p'), 1))] = 'TypeR'
         diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x1_3', 'x2_1'), 1), (('x2_2', 'x1_4'), 1))] = 'TypeR'
+        diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x1_3p', 'x2_1'), 1), (('x2_2', 'x1_4p'), 1))] = 'TypeR'
         diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x1_3', 'x2_1'), 1), (('x2_2', 'x1_4'), 1), (('x2_3', 'x2_4'), 1))] = 'TypeR'
         diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x1_3', 'x2_1'), 1), (('x2_2', 'x1_4'), 1), (('x2_3p', 'x2_4p'), 1))] = 'TypeR'
-        diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x1_3p', 'x2_1'), 1), (('x2_2', 'x1_4p'), 1))] = 'TypeR'
         diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x1_3p', 'x2_1'), 1), (('x2_2', 'x1_4p'), 1), (('x2_3', 'x2_4'), 1))] = 'TypeR'
         diagram_type_dict[((('x1_1', 'x1_2'), 1), (('x1_3p', 'x2_1'), 1), (('x2_2', 'x1_4p'), 1), (('x2_3p', 'x2_4p'), 1))] = 'TypeR'
         #
+        diagram_type_dict[((('x1_1', 'x2_1'), 1), (('x2_2', 'x1_2'), 1))] = 'TypeD'
         diagram_type_dict[((('x1_1', 'x2_1'), 1), (('x1_2', 'x2_2'), 1), (('x2_3', 'x1_3'), 1), (('x2_4', 'x1_4'), 1))] = 'TypeD'
         diagram_type_dict[((('x1_1', 'x2_1'), 1), (('x1_2', 'x2_2'), 1), (('x2_3p', 'x1_3p'), 1), (('x2_4p', 'x1_4p'), 1))] = 'TypeD'
+        #
         mom0 = q.Coordinate([ 0, 0, 0, 0, ])
         src_op_list = [
                 mk_sigma_wf("x1_1", "x1_2", mom0),
@@ -984,6 +987,7 @@ set_param("test-4nt16", "measurement", "auto_contract_meson_meson_i0_j0_corr_wf"
 
 qg.begin_with_gpt()
 
+# ADJUST ME
 job_tags = [
         "test-4nt16",
         # "test-4nt64",
