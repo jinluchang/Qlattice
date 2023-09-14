22faac24fad879127292d331b882ce09ea1c6e52
Luchang Jin <ljin.luchang@gmail.com>
2023-09-14 00:10:33 -0400

    add frontier scripts

---
+++ scripts/build.frontier.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+{
+    ./scripts/setenv.frontier.sh
+    ./scripts/qcore.sh
+
+    ./scripts/gsl.sh
+    ./scripts/cuba.sh
+    ./scripts/zlib.sh
+    ./scripts/eigen.sh
+    ./scripts/perl.sh
+    ./scripts/openssl.sh
+    ./scripts/libffi.sh
+    ./scripts/openblas.sh
+    ./scripts/hdf5.sh
+
+    ./scripts/python.sh
+    ./scripts/python-pip.sh
+    ./scripts/re2c.sh
+    ./scripts/ninja.sh
+    ./scripts/ninja-script.sh
+    ./scripts/python-meson.sh
+    ./scripts/python-meson-py.sh
+    ./scripts/python-packages.sh
+
+    ./scripts/automake.sh
+    ./scripts/gmp.sh
+    ./scripts/mpfr.sh
+    ./scripts/fftw.sh
+    ./scripts/c-lime.sh
+
+    ./scripts/qmp.sh
+    ./scripts/qio.sh
+    ./scripts/cps.sh
+
+    ./scripts/grid-clehner.avx2.sh
+    ./scripts/gpt.sh
+
+    ./scripts/qlat-utils.sh
+    ./scripts/qlat.sh
+    ./scripts/qlat-grid.sh
+    ./scripts/qlat-cps.sh
+
+    ./scripts/qlat-examples-py.sh
+    ./scripts/qlat-examples-cpp.sh
+    ./scripts/qlat-examples-py-gpt.sh
+    ./scripts/qlat-examples-py-cps.sh
+    ./scripts/qlat-examples-cpp-grid.sh
+
+    date
+} 2>&1 | tee $prefix/log.build.txt
---
+++ scripts/setenv.frontier.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+name=setenv-frontier
+
+source qcore/set-prefix.sh
+
+{ time {
+    echo "!!!! build $name !!!!"
+    source qcore/conf.sh
+
+#
+cat >"$prefix/setenv.sh" <<EOF
+if [ -z "\$num_proc" ] ; then
+    export num_proc=8
+fi
+module purge
+module load PrgEnv-gnu
+module list
+if [ -z "\$USE_COMPILER" ] ; then
+    export USE_COMPILER=gcc
+fi
+EOF
+
+    #
+    "$wd"/qcore/bin/mk-setenv-dir.sh --keep
+    echo "!!!! $name build !!!!"
+} } 2>&1 | tee $prefix/log.$name.txt
