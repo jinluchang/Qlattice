6663392225d5047ce04e8016923e2e1cd3d853cf
Luchang Jin <ljin.luchang@gmail.com>
2023-10-01 02:11:09 -0400

    python-jupyter allow failures

--- scripts/python-jupyter.sh
+++ scripts/python-jupyter.sh
@@ -13,30 +13,30 @@ source qcore/set-prefix.sh $name
 
     opts="--verbose --upgrade"
 
-    time-run pip3 install $opts h5py
-    time-run pip3 install $opts pandas
-    time-run pip3 install $opts xarray
-    time-run pip3 install $opts matplotlib
-    time-run pip3 install $opts vpython
-    time-run pip3 install $opts plotly
-    time-run pip3 install $opts seaborn
-    time-run pip3 install $opts notebook
-    time-run pip3 install $opts dash
-    time-run pip3 install $opts virtualenv
-    time-run pip3 install $opts squarify
-    time-run pip3 install $opts mathjax
-    time-run pip3 install $opts lz4
-    time-run pip3 install $opts pycuda
-    time-run pip3 install $opts pytools
-    time-run pip3 install $opts Sphinx
-    time-run pip3 install $opts myst-parser
-    time-run pip3 install $opts torch torchvision torchaudio
-    time-run pip3 install $opts ipywidgets
-    time-run pip3 install $opts transformers
-    time-run pip3 install $opts xformers
-    time-run pip3 install $opts jupyterlab
-    time-run pip3 install $opts jupyterlab-dash
-    time-run pip3 install $opts jupyterhub
+    time-run pip3 install $opts h5py || true
+    time-run pip3 install $opts pandas || true
+    time-run pip3 install $opts xarray || true
+    time-run pip3 install $opts matplotlib || true
+    time-run pip3 install $opts vpython || true
+    time-run pip3 install $opts plotly || true
+    time-run pip3 install $opts seaborn || true
+    time-run pip3 install $opts notebook || true
+    time-run pip3 install $opts dash || true
+    time-run pip3 install $opts virtualenv || true
+    time-run pip3 install $opts squarify || true
+    time-run pip3 install $opts mathjax || true
+    time-run pip3 install $opts lz4 || true
+    time-run pip3 install $opts pycuda || true
+    time-run pip3 install $opts pytools || true
+    time-run pip3 install $opts Sphinx || true
+    time-run pip3 install $opts myst-parser || true
+    time-run pip3 install $opts torch torchvision torchaudio || true
+    time-run pip3 install $opts ipywidgets || true
+    time-run pip3 install $opts transformers || true
+    time-run pip3 install $opts xformers || true
+    time-run pip3 install $opts jupyterlab || true
+    time-run pip3 install $opts jupyterlab-dash || true
+    time-run pip3 install $opts jupyterhub || true
     # time-run pip3 install $opts jupyterlab-vpython
 
     mk-setenv.sh
