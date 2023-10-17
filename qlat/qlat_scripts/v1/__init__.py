54f4bdb39351defe62f57bf12957c2fcd1769a02
Luchang Jin <ljin.luchang@gmail.com>
2023-10-16 21:56:51 -0400

    small

--- qlat/qlat_scripts/v1/__init__.py
+++ qlat/qlat_scripts/v1/__init__.py
@@ -2,6 +2,7 @@ from .rbc_ukqcd_params import *
 from .rbc_ukqcd import *
 from .jobs import *
 from .params import *
+from .params_meas import *
 from .load_data import *
 from .gen_data import *
 from .auto_check import *
