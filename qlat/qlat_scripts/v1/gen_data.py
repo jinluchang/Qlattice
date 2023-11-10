a5afe9c65a4a68d6fd3d4ebb2a0c903fcc1f42de
Luchang Jin <ljin.luchang@gmail.com>
2023-11-10 17:12:08 -0500

    change some format

--- qlat/qlat_scripts/v1/gen_data.py
+++ qlat/qlat_scripts/v1/gen_data.py
@@ -5,7 +5,7 @@ from .jobs import *
 # -----------------------------------------------------------------------------
 
 @q.timer
-def run_get_inverter(job_tag, traj, *, inv_type, get_gf, get_gt = None, get_eig = None):
+def run_get_inverter(job_tag, traj, *, inv_type, get_gf, get_gt=None, get_eig=None):
     if None in [ get_gf, ]:
         return
     if get_gt is None:
@@ -16,7 +16,7 @@ def run_get_inverter(job_tag, traj, *, inv_type, get_gf, get_gt = None, get_eig 
     gt = get_gt()
     eig = get_eig()
     for inv_acc in [ 0, 1, 2, ]:
-        ru.get_inv(gf, job_tag, inv_type, inv_acc, gt = gt, eig = eig)
+        ru.get_inv(gf, job_tag, inv_type, inv_acc, gt=gt, eig=eig)
 
 # -----------------------------------------------------------------------------
 
@@ -45,12 +45,12 @@ def compute_prop_wsrc(gf, gt, tslice, job_tag, inv_type, inv_acc, *,
     q.check_stop()
     q.check_time_limit()
     q.displayln_info(f"compute_prop_wsrc: idx={idx} tslice={tslice}", job_tag, inv_type, inv_acc)
-    inv = ru.get_inv(gf, job_tag, inv_type, inv_acc, gt = gt, eig = eig)
+    inv = ru.get_inv(gf, job_tag, inv_type, inv_acc, gt=gt, eig=eig)
     total_site = q.Coordinate(get_param(job_tag, "total_site"))
     geo = q.Geometry(total_site, 1)
     src = q.mk_wall_src(geo, tslice)
-    prop = compute_prop_1(inv, src, tag = tag, sfw = sfw, path_sp = path_sp,
-                          psel = psel, fsel = fsel, fselc = fselc)
+    prop = compute_prop_1(inv, src, tag=tag, sfw=sfw, path_sp=path_sp,
+                          psel=psel, fsel=fsel, fselc=fselc)
 
 @q.timer_verbose
 def compute_prop_wsrc_all(job_tag, traj, *,
@@ -60,21 +60,21 @@ def compute_prop_wsrc_all(job_tag, traj, *,
     path_s = f"{job_tag}/prop-wsrc-{inv_type_name}/traj-{traj}"
     path_sp = f"{job_tag}/psel-prop-wsrc-{inv_type_name}/traj-{traj}"
     finished_tags = q.properly_truncate_fields(get_save_path(path_s + ".acc"))
-    sfw = q.open_fields(get_save_path(path_s + ".acc"), "a", q.Coordinate([ 1, 2, 2, 4, ]))
-    for inv_acc in [ 2, 1 ]:
+    sfw = q.open_fields(get_save_path(path_s + ".acc"), "a", q.Coordinate([ 2, 2, 2, 4, ]))
+    for inv_acc in [ 2, 1, ]:
         for p in wi:
-            idx, tslice, inv_type_p, inv_acc_p = p
+            idx, tslice, inv_type_p, inv_acc_p=p
             if inv_type_p == inv_type and inv_acc_p == inv_acc:
                 compute_prop_wsrc(gf, gt, tslice, job_tag, inv_type, inv_acc,
-                        idx = idx, sfw = sfw, path_sp = path_sp,
-                        psel = psel, fsel = fsel, fselc = fselc, eig = eig,
-                        finished_tags = finished_tags)
+                        idx=idx, sfw=sfw, path_sp=path_sp,
+                        psel=psel, fsel=fsel, fselc=fselc, eig=eig,
+                        finished_tags=finished_tags)
     sfw.close()
     q.qtouch_info(get_save_path(os.path.join(path_sp, "checkpoint.txt")))
     # q.qtouch_info(get_save_path(os.path.join(path_sp, "checkpoint ; wsnk.txt")))
     q.qrename_info(get_save_path(path_s + ".acc"), get_save_path(path_s))
-    q.qar_create_info(get_save_path(path_sp + ".qar"), get_save_path(path_sp), is_remove_folder_after = True)
-    # q.qar_create_info(get_save_path(path_s + ".qar"), get_save_path(path_s), is_remove_folder_after = True)
+    q.qar_create_info(get_save_path(path_sp + ".qar"), get_save_path(path_sp), is_remove_folder_after=True)
+    # q.qar_create_info(get_save_path(path_s + ".qar"), get_save_path(path_s), is_remove_folder_after=True)
 
 @q.timer
 def run_prop_wsrc(job_tag, traj, *, inv_type, get_gf, get_eig, get_gt, get_psel, get_fsel, get_wi):
@@ -96,8 +96,8 @@ def run_prop_wsrc(job_tag, traj, *, inv_type, get_gf, get_eig, get_gt, get_psel,
         psel = get_psel()
         wi = get_wi()
         compute_prop_wsrc_all(job_tag, traj,
-                              inv_type = inv_type, gf = gf, gt = gt, wi = wi,
-                              psel = psel, fsel = fsel, fselc = fselc, eig = eig)
+                              inv_type=inv_type, gf=gf, gt=gt, wi=wi,
+                              psel=psel, fsel=fsel, fselc=fselc, eig=eig)
         q.release_lock()
 
 # -----------------------------------------------------------------------------
@@ -134,8 +134,8 @@ def compute_prop_psrc(job_tag, xg_src, inv_type, inv_acc, *,
     total_site = q.Coordinate(get_param(job_tag, "total_site"))
     geo = q.Geometry(total_site, 1)
     src = q.mk_point_src(geo, xg_src)
-    prop = compute_prop_2(inv, src, tag = tag, sfw = sfw, path_sp = path_sp,
-                          psel = psel, fsel = fsel, fselc = fselc, gt = gt)
+    prop = compute_prop_2(inv, src, tag=tag, sfw=sfw, path_sp=path_sp,
+                          psel=psel, fsel=fsel, fselc=fselc, gt=gt)
 
 @q.timer_verbose
 def compute_prop_psrc_all(job_tag, traj, *,
@@ -145,28 +145,28 @@ def compute_prop_psrc_all(job_tag, traj, *,
     path_s = f"{job_tag}/prop-psrc-{inv_type_name}/traj-{traj}"
     path_sp = f"{job_tag}/psel-prop-psrc-{inv_type_name}/traj-{traj}"
     finished_tags = q.properly_truncate_fields(get_save_path(path_s + ".acc"))
-    sfw = q.open_fields(get_save_path(path_s + ".acc"), "a", q.Coordinate([ 1, 2, 2, 4, ]))
+    sfw = q.open_fields(get_save_path(path_s + ".acc"), "a", q.Coordinate([ 2, 2, 2, 4, ]))
     def comp(idx, xg_src, inv_acc):
         compute_prop_psrc(job_tag, xg_src, inv_type, inv_acc,
-                idx = idx, gf = gf, gt = gt, sfw = sfw, path_sp = path_sp,
-                psel = psel, fsel = fsel, fselc = fselc,
-                eig = eig, finished_tags = finished_tags)
+                idx=idx, gf=gf, gt=gt, sfw=sfw, path_sp=path_sp,
+                psel=psel, fsel=fsel, fselc=fselc,
+                eig=eig, finished_tags=finished_tags)
     prob1 = get_param(job_tag, "prob_acc_1_psrc")
     prob2 = get_param(job_tag, "prob_acc_2_psrc")
     rs = q.RngState(f"seed {job_tag} {traj}").split(f"compute_prop_psrc_all(ama)")
     for idx, xg_src in enumerate(psel):
         r = rs.split(f"{xg_src.to_tuple()}").u_rand_gen()
         assert 0 <= r and r <= 1
-        comp(idx, xg_src, inv_acc = 0)
+        comp(idx, xg_src, inv_acc=0)
         if r <= prob1:
-            comp(idx, xg_src, inv_acc = 1)
+            comp(idx, xg_src, inv_acc=1)
         if r <= prob2:
-            comp(idx, xg_src, inv_acc = 2)
+            comp(idx, xg_src, inv_acc=2)
     sfw.close()
     q.qtouch_info(get_save_path(os.path.join(path_sp, "checkpoint.txt")))
     q.qrename_info(get_save_path(path_s + ".acc"), get_save_path(path_s))
-    q.qar_create_info(get_save_path(path_sp + ".qar"), get_save_path(path_sp), is_remove_folder_after = True)
-    # q.qar_create_info(get_save_path(path_s + ".qar"), get_save_path(path_s), is_remove_folder_after = True)
+    q.qar_create_info(get_save_path(path_sp + ".qar"), get_save_path(path_sp), is_remove_folder_after=True)
+    # q.qar_create_info(get_save_path(path_s + ".qar"), get_save_path(path_s), is_remove_folder_after=True)
 
 @q.timer
 def run_prop_psrc(job_tag, traj, *, inv_type, get_gf, get_eig, get_gt, get_psel, get_fsel):
@@ -187,8 +187,8 @@ def run_prop_psrc(job_tag, traj, *, inv_type, get_gf, get_eig, get_gt, get_psel,
         fsel, fselc = get_fsel()
         psel = get_psel()
         compute_prop_psrc_all(job_tag, traj,
-                              inv_type = inv_type, gf = gf, gt = gt,
-                              psel = psel, fsel = fsel, fselc = fselc, eig = eig)
+                              inv_type=inv_type, gf=gf, gt=gt,
+                              psel=psel, fsel=fsel, fselc=fselc, eig=eig)
         q.release_lock()
 
 # -----------------------------------------------------------------------------
@@ -209,21 +209,21 @@ def compute_prop_rand_u1_type_acc(*, sfw, job_tag, traj, gf, eig, fsel, idx_rand
     return s_prop
 
 @q.timer_verbose
-def compute_prop_rand_u1(*, job_tag, traj, inv_type, gf, path_s, fsel, eig = None):
+def compute_prop_rand_u1(*, job_tag, traj, inv_type, gf, path_s, fsel, eig=None):
     # use fsel instead of fselc
     n_rand_u1_fsel = get_param(job_tag, "n_rand_u1_fsel")
     total_site = q.Coordinate(get_param(job_tag, "total_site"))
     geo = q.Geometry(total_site, 1)
     finished_tags = q.properly_truncate_fields(get_save_path(path_s + ".acc"))
-    sfw = q.open_fields(get_save_path(path_s + ".acc"), "a", q.Coordinate([ 1, 2, 2, 4, ]))
+    sfw = q.open_fields(get_save_path(path_s + ".acc"), "a", q.Coordinate([ 2, 2, 2, 4, ]))
     def comp(idx_rand_u1, inv_acc):
         compute_prop_rand_u1_type_acc(
-                sfw = sfw,
-                job_tag = job_tag, traj = traj,
-                gf = gf, eig = eig, fsel = fsel,
-                idx_rand_u1 = idx_rand_u1,
-                inv_type = inv_type, inv_acc = inv_acc,
-                finished_tags = finished_tags,)
+                sfw=sfw,
+                job_tag=job_tag, traj=traj,
+                gf=gf, eig=eig, fsel=fsel,
+                idx_rand_u1=idx_rand_u1,
+                inv_type=inv_type, inv_acc=inv_acc,
+                finished_tags=finished_tags,)
     rs = q.RngState(f"seed {job_tag} {traj}").split(f"compute_prop_rand_u1(ama)")
     prob1 = get_param(job_tag, "prob_acc_1_rand_u1")
     prob2 = get_param(job_tag, "prob_acc_2_rand_u1")
@@ -240,10 +240,10 @@ def compute_prop_rand_u1(*, job_tag, traj, inv_type, gf, path_s, fsel, eig = Non
             comp(idx_rand_u1, inv_acc)
     sfw.close()
     q.qrename_info(get_save_path(path_s + ".acc"), get_save_path(path_s))
-    # q.qar_create_info(get_save_path(path_s + ".qar"), get_save_path(path_s), is_remove_folder_after = True)
+    # q.qar_create_info(get_save_path(path_s + ".qar"), get_save_path(path_s), is_remove_folder_after=True)
 
 @q.timer_verbose
-def run_prop_rand_u1(job_tag, traj, *, inv_type, get_gf, get_fsel, get_eig = None):
+def run_prop_rand_u1(job_tag, traj, *, inv_type, get_gf, get_fsel, get_eig=None):
     if None in [ get_gf, get_fsel, ]:
         return
     if get_eig is None:
@@ -260,12 +260,12 @@ def run_prop_rand_u1(job_tag, traj, *, inv_type, get_gf, get_fsel, get_eig = Non
         fsel, fselc = get_fsel()
         eig = get_eig()
         compute_prop_rand_u1(
-                job_tag = job_tag, traj = traj,
-                inv_type = inv_type,
-                gf = gf,
-                path_s = path_s,
-                fsel = fsel,
-                eig = eig)
+                job_tag=job_tag, traj=traj,
+                inv_type=inv_type,
+                gf=gf,
+                path_s=path_s,
+                fsel=fsel,
+                eig=eig)
         q.release_lock()
 
 # -----------------------------------------------------------------------------
@@ -303,7 +303,7 @@ def compute_prop_smear(job_tag, xg_src, inv_type, inv_acc, *,
     q.check_stop()
     q.check_time_limit()
     q.displayln_info(f"compute_prop_smear: {job_tag} idx={idx} tag='{tag}'")
-    inv = ru.get_inv(gf, job_tag, inv_type, inv_acc, eig = eig)
+    inv = ru.get_inv(gf, job_tag, inv_type, inv_acc, eig=eig)
     total_site = q.Coordinate(get_param(job_tag, "total_site"))
     geo = q.Geometry(total_site, 1)
     coef = get_param(job_tag, "prop_smear_coef")
@@ -311,9 +311,9 @@ def compute_prop_smear(job_tag, xg_src, inv_type, inv_acc, *,
     def smear(src):
         return q.prop_smear(src, gf_ape, coef, step)
     src = smear(q.mk_point_src(geo, xg_src))
-    prop = compute_prop_3(inv, src, tag = tag, sfw = sfw, path_sp = path_sp,
-                          psel = psel, fsel = fsel, fselc = fselc, gt = gt,
-                          psel_smear = psel_smear, smear = smear)
+    prop = compute_prop_3(inv, src, tag=tag, sfw=sfw, path_sp=path_sp,
+                          psel=psel, fsel=fsel, fselc=fselc, gt=gt,
+                          psel_smear=psel_smear, smear=smear)
 
 @q.timer_verbose
 def compute_prop_smear_all(job_tag, traj, *,
@@ -324,29 +324,29 @@ def compute_prop_smear_all(job_tag, traj, *,
     path_s = f"{job_tag}/prop-smear-{inv_type_name}/traj-{traj}"
     path_sp = f"{job_tag}/psel-prop-smear-{inv_type_name}/traj-{traj}"
     finished_tags = q.properly_truncate_fields(get_save_path(path_s + ".acc"))
-    sfw = q.open_fields(get_save_path(path_s + ".acc"), "a", q.Coordinate([ 1, 2, 2, 4, ]))
+    sfw = q.open_fields(get_save_path(path_s + ".acc"), "a", q.Coordinate([ 2, 2, 2, 4, ]))
     def comp(idx, xg_src, inv_acc):
         compute_prop_smear(job_tag, xg_src, inv_type, inv_acc,
-                idx = idx, gf = gf, gt = gt, sfw = sfw, path_sp = path_sp,
-                psel = psel, fsel = fsel, fselc = fselc,
-                psel_smear = psel_smear, gf_ape = gf_ape,
-                eig = eig, finished_tags = finished_tags)
+                idx=idx, gf=gf, gt=gt, sfw=sfw, path_sp=path_sp,
+                psel=psel, fsel=fsel, fselc=fselc,
+                psel_smear=psel_smear, gf_ape=gf_ape,
+                eig=eig, finished_tags=finished_tags)
     prob1 = get_param(job_tag, "prob_acc_1_smear")
     prob2 = get_param(job_tag, "prob_acc_2_smear")
     rs = q.RngState(f"seed {job_tag} {traj}").split(f"compute_prop_smear_all(ama)")
     for idx, xg_src in enumerate(psel_smear):
         r = rs.split(f"{xg_src.to_tuple()}").u_rand_gen()
         assert 0 <= r and r <= 1
-        comp(idx, xg_src, inv_acc = 0)
+        comp(idx, xg_src, inv_acc=0)
         if r <= prob1:
-            comp(idx, xg_src, inv_acc = 1)
+            comp(idx, xg_src, inv_acc=1)
         if r <= prob2:
-            comp(idx, xg_src, inv_acc = 2)
+            comp(idx, xg_src, inv_acc=2)
     sfw.close()
     q.qtouch_info(get_save_path(os.path.join(path_sp, "checkpoint.txt")))
     q.qrename_info(get_save_path(path_s + ".acc"), get_save_path(path_s))
-    q.qar_create_info(get_save_path(path_sp + ".qar"), get_save_path(path_sp), is_remove_folder_after = True)
-    # q.qar_create_info(get_save_path(path_s + ".qar"), get_save_path(path_s), is_remove_folder_after = True)
+    q.qar_create_info(get_save_path(path_sp + ".qar"), get_save_path(path_sp), is_remove_folder_after=True)
+    # q.qar_create_info(get_save_path(path_s + ".qar"), get_save_path(path_s), is_remove_folder_after=True)
 
 @q.timer
 def run_prop_smear(job_tag, traj, *, inv_type, get_gf, get_gf_ape, get_eig, get_gt, get_psel, get_fsel, get_psel_smear):
@@ -369,8 +369,8 @@ def run_prop_smear(job_tag, traj, *, inv_type, get_gf, get_gf_ape, get_eig, get_
         psel_smear = get_psel_smear()
         gf_ape = get_gf_ape()
         compute_prop_smear_all(job_tag, traj,
-                inv_type = inv_type, gf = gf, gf_ape = gf_ape, gt = gt,
-                psel = psel, fsel = fsel, fselc = fselc, eig = eig, psel_smear = psel_smear)
+                inv_type=inv_type, gf=gf, gf_ape=gf_ape, gt=gt,
+                psel=psel, fsel=fsel, fselc=fselc, eig=eig, psel_smear=psel_smear)
         q.release_lock()
 
 # -----------------------------------------------------------------------------
