dd74e12200c0fa971302bfe37f31a7cfa87e9119
Luchang Jin <ljin.luchang@gmail.com>
2023-11-21 23:18:22 -0500

    improve q_fit_corr

--- qlat-utils/qlat_utils/q_fit_corr.py
+++ qlat-utils/qlat_utils/q_fit_corr.py
@@ -64,9 +64,9 @@ def mk_fcn(corr_data, corr_data_sigma, t_start):
     t_arr = t_start + jnp.arange(t_size, dtype=jnp.float64)
     def fcn_f(param_arr):
         assert len(param_arr.shape) == 1
-        n_param = len(param_arr)
-        assert n_param % (n_ops + 1) == 0
-        n_energies = n_param // (n_ops + 1)
+        n_params = len(param_arr)
+        assert n_params % (n_ops + 1) == 0
+        n_energies = n_params // (n_ops + 1)
         param_arr = jnp.array(param_arr, dtype=jnp.float64)
         es = param_arr[:n_energies]
         cs = param_arr[n_energies:].reshape(n_energies, n_ops)
@@ -133,21 +133,21 @@ def adaptive_minimize(fcn, step_size_list, n_step=10, max_total_steps=10000, *, 
 def minimize_scipy(fcn, maxiter=10000, *, param_arr, fixed_param_mask=None):
     import scipy
     fname = get_fname()
-    n_param = len(param_arr)
+    n_params = len(param_arr)
     if fixed_param_mask is None:
-        fixed_param_mask = np.zeros(n_param, dtype=bool)
+        fixed_param_mask = np.zeros(n_params, dtype=bool)
     p_fixed = param_arr[fixed_param_mask]
     free_param_mask = ~fixed_param_mask
     @timer
     def c_fcn(p_free):
-        p_all = np.empty(n_param, dtype=np.float64)
+        p_all = np.empty(n_params, dtype=np.float64)
         p_all[fixed_param_mask] = p_fixed
         p_all[free_param_mask] = p_free
         chisq = fcn(p_all, requires_grad=False)
         return chisq
     @timer
     def c_fcn_grad(p_free):
-        p_all = np.empty(n_param, dtype=np.float64)
+        p_all = np.empty(n_params, dtype=np.float64)
         p_all[fixed_param_mask] = p_fixed
         p_all[free_param_mask] = p_free
         chisq, grad_all = fcn(p_all, requires_grad=True)
@@ -164,23 +164,66 @@ def minimize_scipy(fcn, maxiter=10000, *, param_arr, fixed_param_mask=None):
 
 ### -----------------
 
+def jk_mini_task_in_fit_energy_amplitude(kwargs):
+    def f(*,
+          jk_idx,
+          corr_data,
+          corr_data_err,
+          t_start_fcn,
+          param_arr_mini,
+          n_step_mini_jk,
+          n_iter_mini,
+          r_amp,
+          rand_update_mask,
+          all_energies_mask,
+          fixed_energies_mask,
+          ):
+        rng = RngState(f"jk_mini_task_in_fit_energy_amplitude-seed-param").split(f"{jk_idx}")
+        n_params = len(param_arr_mini)
+        def rand_update(param_arr):
+            param_arr = param_arr.copy()
+            param_arr[rand_update_mask] = (
+                    param_arr[rand_update_mask]
+                    + (rng.u_rand_arr(n_params)[rand_update_mask] - 0.5) * r_amp)
+            return param_arr
+        fcn = mk_fcn(corr_data, corr_data_err, t_start_fcn)
+        param_arr = param_arr_mini.copy()
+        for i in range(n_step_mini_jk):
+            param_arr = rand_update(param_arr)
+            param_arr = minimize_scipy(fcn, maxiter=n_iter_mini,
+                                       param_arr=param_arr, fixed_param_mask=all_energies_mask)
+        for i in range(n_step_mini_jk):
+            param_arr = rand_update(param_arr)
+            param_arr = minimize_scipy(fcn, maxiter=n_iter_mini,
+                                       param_arr=param_arr, fixed_param_mask=fixed_energies_mask)
+        chisq, chisq_grad = fcn(param_arr)
+        return chisq, chisq_grad, param_arr
+    return f(**kwargs)
+
+def mp_initializer():
+    import qlat as q
+    q.set_verbose_level(-1)
+
 @timer_verbose
 def fit_energy_amplitude(jk_corr_data,
                          *,
                          t_start_data=0,
                          t_start_fit=4,
                          t_stop_fit=None,
+                         t_start_fcn=0,
                          fixed_energy_arr=None,
                          free_energy_arr=None,
                          n_step_mini_avg=10,
                          n_step_mini_jk=5,
                          n_iter_mini=1e3,
+                         mp_pool=None,
                         ):
     """
     return res
     #
     res['jk_chisq'] = jk_chisq
-    res['jk_chisq_grad'] = jk_chisq_grad
+    res['jk_chisq_grad'] = jk_chisq_grad # chisq_grad with respect to param_arr (for the scaled jk_corr_data)
+    res['jk_param_arr_for_scaled_corr'] = jk_param_arr_for_scaled_corr
     res['jk_param_arr'] = jk_param_arr
     #
     C^{(jk_idx)}_{i,j}(t + t_start_data) == jk_corr_data[jk_idx, i, j, t]
@@ -211,6 +254,8 @@ def fit_energy_amplitude(jk_corr_data,
     #
     jk_corr_data = jk_corr_data[:, :, :, t_start:t_stop]
     #
+    n_jk = len(jk_corr_data)
+    #
     if fixed_energy_arr is None:
         fixed_energy_arr = np.array([], dtype=np.float64)
         if free_energy_arr is None:
@@ -222,14 +267,26 @@ def fit_energy_amplitude(jk_corr_data,
     fixed_energy_arr = fixed_energy_arr.copy()
     free_energy_arr = free_energy_arr.copy()
     #
-    t_start_fcn = t_start_fit
-    #
     n_ops = jk_corr_data.shape[1]
     assert n_ops == jk_corr_data.shape[2]
     #
     op_idx_arr = np.arange(n_ops)
     op_norm_fac = 1 / np.sqrt(jk_corr_data[0, op_idx_arr, op_idx_arr, 0])
     jk_corr_data = op_norm_fac[:, None, None] * op_norm_fac[None, :, None] * jk_corr_data
+    #
+    is_close_pool = False
+    if mp_pool is None:
+        mp_map = map
+        mp_pool_n_proc = 1
+    elif isinstance(mp_pool, int):
+        mp_pool_n_proc = mp_pool
+        import multiprocessing
+        mp_pool = multiprocessing.get_context('spawn').Pool(mp_pool_n_proc, initializer=mp_initializer)
+        mp_map = mp_pool.imap
+        is_close_pool = True
+    else:
+        mp_pool_n_proc = 16
+        mp_map = mp_pool.imap
     #
     corr_data, corr_data_err = g_jk_avg_err(jk_corr_data)
     #
@@ -240,7 +297,7 @@ def fit_energy_amplitude(jk_corr_data,
     #
     fcn_avg = mk_fcn(corr_data, corr_data_err, t_start_fcn)
     #
-    rng = RngState(f"{fname}-seed-param")
+    rng = RngState(f"fit_energy_amplitude-seed-param")
     r_amp = 1e-6
     #
     c_arr = np.zeros(n_energies * n_ops, dtype=np.float64)
@@ -272,59 +329,77 @@ def fit_energy_amplitude(jk_corr_data,
     #
     def rand_update(param_arr, rng):
         param_arr = param_arr.copy()
-        param_arr[rand_update_mask] = param_arr[rand_update_mask] + (rng.u_rand_arr(n_params)[rand_update_mask] - 0.5) * r_amp
+        param_arr[rand_update_mask] = (
+                param_arr[rand_update_mask]
+                + (rng.u_rand_arr(n_params)[rand_update_mask] - 0.5) * r_amp)
         return param_arr
     #
     param_arr_mini = param_arr_initial.copy()
-    display_param_arr(param_arr_mini, fcn_avg)
     #
     displayln_info(0, f"{fname}: mini with fixed all energies")
     for i in range(n_step_mini_avg):
         param_arr_mini = rand_update(param_arr_mini, rng)
         param_arr_mini = minimize_scipy(fcn_avg, maxiter=n_iter_mini, param_arr=param_arr_mini, fixed_param_mask=all_energies_mask)
-        display_param_arr(param_arr_mini, fcn=fcn_avg, mask=all_energies_mask, verbose_level=1)
+        vl = 1
+        if i == n_step_mini_avg - 1:
+            vl = 0
+        display_param_arr(param_arr_mini, fcn=fcn_avg, mask=all_energies_mask, verbose_level=vl)
+    #
     displayln_info(0, f"{fname}: mini with fixed some energies")
     for i in range(n_step_mini_avg):
         param_arr_mini = rand_update(param_arr_mini, rng)
         param_arr_mini = minimize_scipy(fcn_avg, maxiter=n_iter_mini, param_arr=param_arr_mini, fixed_param_mask=fixed_energies_mask)
-        display_param_arr(param_arr_mini, fcn=fcn_avg, mask=fixed_energies_mask, verbose_level=1)
+        vl = 1
+        if i == n_step_mini_avg - 1:
+            vl = 0
+        display_param_arr(param_arr_mini, fcn=fcn_avg, mask=fixed_energies_mask, verbose_level=vl)
     #
-    def jk_mini(jk_idx):
-        import qlat_utils as q
-        import numpy as np
-        rng = q.RngState(f"{fname}-seed-param").split(f"{jk_idx}")
-        fcn = q.q_fit_corr.mk_fcn(jk_corr_data[jk_idx], corr_data_err, t_start_fcn)
-        param_arr = param_arr_mini.copy()
-        for i in range(n_step_mini_jk):
-            param_arr = rand_update(param_arr, rng)
-            param_arr = q.q_fit_corr.minimize_scipy(fcn, maxiter=n_iter_mini, param_arr=param_arr, fixed_param_mask=all_energies_mask)
-        for i in range(n_step_mini_jk):
-            param_arr = rand_update(param_arr, rng)
-            param_arr = q.q_fit_corr.minimize_scipy(fcn, maxiter=n_iter_mini, param_arr=param_arr, fixed_param_mask=fixed_energies_mask)
-        chisq, chisq_grad = fcn(param_arr)
-        return chisq, chisq_grad, param_arr
+    def mk_kwargs(jk_idx):
+        kwargs = dict(
+                jk_idx=jk_idx,
+                corr_data=jk_corr_data[jk_idx],
+                corr_data_err=corr_data_err,
+                t_start_fcn=t_start_fcn,
+                param_arr_mini=param_arr_mini,
+                n_step_mini_jk=n_step_mini_jk,
+                n_iter_mini=n_iter_mini,
+                r_amp=r_amp,
+                rand_update_mask=rand_update_mask,
+                all_energies_mask=all_energies_mask,
+                fixed_energies_mask=fixed_energies_mask,
+                )
+        return kwargs
     #
     displayln_info(0, f"{fname}: mini all jk samples")
+    mp_map_print_interval = mp_pool_n_proc
+    #
     jk_chisq = []
     jk_chisq_grad = []
     jk_param_arr = []
-    for idx, v in enumerate(mp_pool.imap(jk_mini, range(len(jk_corr_data)))):
-        if idx % mp_pool_n_proc == 0:
-            displayln_info(0, f"mp_pool.imap: {idx}")
+    for idx, v in enumerate(mp_map(jk_mini_task_in_fit_energy_amplitude,
+                                   map(mk_kwargs, range(n_jk)))):
+        if idx % mp_map_print_interval == 0:
+            displayln_info(0, f"map: {idx}")
         chisq, chisq_grad, param_arr = v
         jk_chisq.append(chisq)
         jk_chisq_grad.append(chisq_grad)
         jk_param_arr.append(param_arr)
+    if is_close_pool:
+        mp_pool.close()
     #
     jk_chisq = np.array(jk_chisq, dtype=np.float64)
     jk_chisq_grad = np.array(jk_chisq_grad, dtype=np.float64)
     jk_param_arr = np.array(jk_param_arr, dtype=np.float64)
     #
-    display_param_arr(jk_param_arr[0], fcn=fcn_avg, mask=fixed_energies_mask, verbose_level=0)
+    jk_param_arr_for_scaled_corr = jk_param_arr.copy()
+    jk_param_arr[:, n_energies:].reshape(n_jk, n_energies, n_ops,)[:] *= (
+            1 / op_norm_fac * np.exp(jk_param_arr[:, :n_energies, None] * (t_start_fit - t_start_fcn) / 2)
+            )
     #
     res = dict()
     res['jk_chisq'] = jk_chisq
     res['jk_chisq_grad'] = jk_chisq_grad
+    res['jk_param_arr_for_scaled_corr'] = jk_param_arr_for_scaled_corr
     res['jk_param_arr'] = jk_param_arr
     #
     displayln_info(0, f"{fname} finished")
@@ -357,7 +432,7 @@ class HmcParams:
             traj=0,
             tau=1.0,
             n_step=32,
-            n_param=None,
+            n_params=None,
             param_arr=None,
             hmc_mass_arr=None,
             hmc_mass_adaptive_rate=1/8,
@@ -367,10 +442,10 @@ class HmcParams:
             rng=None,
             ):
         """
-        Need at least n_param or param_arr
+        Need at least n_params or param_arr
         #
         tau is the MD time
-        n_param=len(param_arr)
+        n_params=len(param_arr)
         Ideally:
         omega = np.pi / 2
         <p^2> == hmc_mass_arr
@@ -389,19 +464,19 @@ class HmcParams:
         if rng is None:
             rng = RngState(f"seed-hmc-core-{traj}")
         if param_arr is None:
-            assert n_param is not None
-            n_param = n_energies * (n_ops + 1)
-            param_arr = np.zeros(n_param, dtype=float)
+            assert n_params is not None
+            n_params = n_energies * (n_ops + 1)
+            param_arr = np.zeros(n_params, dtype=float)
         else:
-            if n_param is None:
-                n_param = len(param_arr)
+            if n_params is None:
+                n_params = len(param_arr)
             else:
-                assert n_param == len(param_arr)
+                assert n_params == len(param_arr)
             param_arr = np.array(param_arr, dtype=float)
         if hmc_mass_arr is None:
-            hmc_mass_arr = np.ones(n_param, dtype=float)
+            hmc_mass_arr = np.ones(n_params, dtype=float)
         elif isinstance(hmc_mass_arr, (int, float)):
-            hmc_mass_arr = hmc_mass_arr * np.ones(n_param, dtype=float)
+            hmc_mass_arr = hmc_mass_arr * np.ones(n_params, dtype=float)
         else:
             hmc_mass_arr = np.array(hmc_mass_arr, dtype=float)
             assert hmc_mass_arr.shape == param_arr.shape
@@ -410,7 +485,7 @@ class HmcParams:
         self.traj = traj
         self.tau = tau
         self.n_step = n_step
-        self.n_param = n_param
+        self.n_params = n_params
         self.param_arr = param_arr
         self.hmc_mass_arr = hmc_mass_arr
         self.hmc_mass_adaptive_rate = hmc_mass_adaptive_rate
@@ -442,7 +517,7 @@ def hmc_traj(fcn, hmc_params):
     hmc_mass_arr = hmc_params.hmc_mass_arr
     force_sqr_avg = 0
     delta_hh_history = hmc_params.delta_hh_history
-    mom_arr = hmc_params.rng.g_rand_arr(hmc_params.n_param) * np.sqrt(hmc_mass_arr) * np.sqrt(hmc_params.temperature)
+    mom_arr = hmc_params.rng.g_rand_arr(hmc_params.n_params) * np.sqrt(hmc_mass_arr) * np.sqrt(hmc_params.temperature)
     dt = hmc_params.tau / hmc_params.n_step
     hmc_energy_initial = hmc_energy(param_arr, mom_arr, hmc_mass_arr, fcn)
     param_evolve(param_arr, mom_arr, hmc_mass_arr, dt / 2)
