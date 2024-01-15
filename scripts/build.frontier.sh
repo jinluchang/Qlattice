09b47089454dd02fc83a49ca1b0e200ddc90ea00
Luchang Jin <ljin.luchang@gmail.com>
2024-01-15 16:19:25 -0500

    more frontier pkgs

--- scripts/build.frontier.sh
+++ scripts/build.frontier.sh
@@ -4,6 +4,37 @@ set -e
 
 {
     ./scripts/setenv.frontier.sh
+    ./scripts/qcore.sh
+
+    ./scripts/gsl.sh
+    ./scripts/cuba.sh
+    ./scripts/zlib.sh
+    ./scripts/eigen.sh
+    ./scripts/perl.sh
+    ./scripts/openssl.sh
+    ./scripts/libffi.sh
+    ./scripts/openblas.sh
+    ./scripts/hdf5.sh
+
+    ./scripts/python.sh
+    ./scripts/python-pip.sh
+    ./scripts/re2c.sh
+    ./scripts/ninja.sh
+    ./scripts/ninja-script.sh
+    ./scripts/python-meson.sh
+    ./scripts/python-meson-py.sh
+    ./scripts/python-packages.sh
+
+    ./scripts/autoconf.sh
+    ./scripts/automake.sh
+    ./scripts/gmp.sh
+    ./scripts/mpfr.sh
+    ./scripts/fftw.sh
+    ./scripts/c-lime.sh
+
+    ./scripts/qmp.sh
+    ./scripts/qio.sh
+    ./scripts/cps.sh
 
     ./scripts/grid-clehner.frontier.sh
     ./scripts/gpt.sh
