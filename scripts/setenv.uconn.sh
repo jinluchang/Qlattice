90a1b97148e9aa2310a013cb0ad52e071fe2780e
Luchang Jin <ljin.luchang@gmail.com>
2024-02-01 22:22:56 -0500

    small

--- scripts/setenv.uconn.sh
+++ scripts/setenv.uconn.sh
@@ -13,7 +13,8 @@ cat >"$prefix/setenv.sh" <<EOF
 if [ -z "\$num_proc" ] ; then
     export num_proc=8
 fi
-source /etc/profile
+echo "May need to run 'source /etc/profile' if your bashrc does not include it."
+# source /etc/profile # cannot run it here as it requires interactive bash.
 module purge
 module add gcc/11.3.0
 module add openmpi/4.1.4
