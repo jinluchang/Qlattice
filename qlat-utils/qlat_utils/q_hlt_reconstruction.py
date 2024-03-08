0ca965b9cc2d067e47206303dda88e9949803149
Luchang Jin <ljin.luchang@gmail.com>
2024-03-08 17:40:43 -0500

    add e0 and atw in q_hlt_reconstruction

--- qlat-utils/qlat_utils/q_hlt_reconstruction.py
+++ qlat-utils/qlat_utils/q_hlt_reconstruction.py
@@ -41,9 +41,12 @@ def mk_hlt_params():
     # mandatory params for via sum functions
     params["e_arr"] = None
     # optional params
+    params["e0"] = 0.0
     params["lambda"] = 1.0
-    params["alpha"] = -0.01
+    params["alpha"] = -0.01 # override by "f_e_weight", see `get_f_e_weight`
     params["f_e_weight"] = None
+    params["tt_size"] = None
+    params["atw_factor"] = 1.0 # only has effects if "tt_size" is not None
     return params
 
 def get_f_e_weight(params):
@@ -62,7 +65,8 @@ def delta_from_g(g_t_arr, t_arr, e_arr):
     g_t_arr.shape == (..., t_size,)
     delta.shape[-1] == (..., n_energies,)
     """
-    return (jnp.exp(-e_arr[:, None] * t_arr) * g_t_arr[..., None, :]).sum(-1)
+    delta = (jnp.exp(-e_arr[:, None] * t_arr) * g_t_arr[..., None, :]).sum(-1)
+    return delta
 
 def aa_from_g_via_sum(g_t_arr, params):
     """
@@ -72,10 +76,16 @@ def aa_from_g_via_sum(g_t_arr, params):
     t_arr = params["t_arr"]
     e_arr = params["e_arr"]
     f_delta_target = params["f_delta_target"]
+    tt_size = params["tt_size"]
+    atw_factor = params["atw_factor"]
     delta_target = f_delta_target(e_arr)
     f_e_weight = get_f_e_weight(params)
+    e0 = params["e0"]
     e_w = f_e_weight(e_arr)
+    e_w = jnp.where(e_arr >= e0, e_w, 0.0)
     delta = delta_from_g(g_t_arr, t_arr, e_arr)
+    if tt_size is not None:
+        delta = delta + atw_factor * delta_from_g(g_t_arr, tt_size - t_arr, e_arr)
     f_e = (delta - delta_target)**2 * e_w
     s1 = f_e[0] * (e_arr[1] - e_arr[0]) / 2
     s2 = f_e[-1] * (e_arr[-1] - e_arr[-2]) / 2
@@ -123,28 +133,36 @@ def mk_g_t_arr_via_sum(params):
     g_t_arr = q.q_fit_corr.minimize_scipy(fcn, param_arr=np.zeros_like(t_arr, dtype=np.float64))
     return g_t_arr
 
-###################
-## with integration
-###################
+############## with integration ##############
 
 def build_hlt_aa_mat(params):
     tag = "aa_mat"
     if tag in params:
         return params[tag]
     f_e_weight = get_f_e_weight(params)
+    e0 = params["e0"]
     t_arr = params["t_arr"]
+    tt_size = params["tt_size"]
+    atw_factor = params["atw_factor"]
     t_size = len(t_arr)
     aa_mat = np.zeros((t_size, t_size,), dtype=np.float64)
     aa_values = {}
+    def compute_t_sum(t_sum):
+        t_sum = int(t_sum)
+        if t_sum not in aa_values:
+            def f(e):
+                return f_e_weight(e) * np.exp(-e * t_sum)
+            v = integrate.quad(f, e0, np.inf)[0]
+            aa_values[t_sum] = v
+        return aa_values[t_sum]
     for t1_idx, t1 in enumerate(t_arr):
         for t2_idx, t2 in enumerate(t_arr):
-            t_sum = int(t1 + t2)
-            if t_sum not in aa_values:
-                def f(e):
-                    return f_e_weight(e) * np.exp(-e * t_sum)
-                v = integrate.quad(f, 0.0, np.inf)[0]
-                aa_values[t_sum] = v
-            aa_mat[t1_idx, t2_idx] = aa_values[t_sum]
+            v = compute_t_sum(t1 + t2)
+            if tt_size is not None:
+                v = v + atw_factor * compute_t_sum(t1 + tt_size - t2)
+                v = v + atw_factor * compute_t_sum(tt_size - t1 + t2)
+                v = v + atw_factor * atw_factor * compute_t_sum(tt_size - t1 + tt_size - t2)
+            aa_mat[t1_idx, t2_idx] = v
     aa_mat = jnp.array(aa_mat)
     params[tag] = aa_mat
     return aa_mat
@@ -154,14 +172,26 @@ def build_hlt_f_vec(params):
     if tag in params:
         return params[tag]
     f_e_weight = get_f_e_weight(params)
+    e0 = params["e0"]
     t_arr = params["t_arr"]
+    tt_size = params["tt_size"]
+    atw_factor = params["atw_factor"]
     f_delta_target = params["f_delta_target"]
     t_size = len(t_arr)
     f_vec = np.zeros(t_size, dtype=np.float64)
+    f_values = {}
+    def compute_t(t):
+        t = int(t)
+        if t not in f_values:
+            def f(e):
+                return -2 * f_e_weight(e) * f_delta_target(e) * np.exp(-e * t)
+            v = integrate.quad(f, e0, np.inf)[0]
+            f_values[t] = v
+        return f_values[t]
     for t_idx, t in enumerate(t_arr):
-        def f(e):
-            return -2 * f_e_weight(e) * f_delta_target(e) * np.exp(-e * t)
-        v = integrate.quad(f, 0.0, np.inf)[0]
+        v = compute_t(t)
+        if tt_size is not None:
+            v = v + atw_factor * compute_t(tt_size - t)
         f_vec[t_idx] = v
     f_vec = jnp.array(f_vec)
     params[tag] = f_vec
@@ -172,10 +202,11 @@ def build_hlt_aa_const(params):
     if tag in params:
         return params[tag]
     f_e_weight = get_f_e_weight(params)
+    e0 = params["e0"]
     f_delta_target = params["f_delta_target"]
     def f(e):
         return f_e_weight(e) * f_delta_target(e)**2
-    aa_const = integrate.quad(f, 0.0, np.inf)[0]
+    aa_const = integrate.quad(f, e0, np.inf)[0]
     params[tag] = aa_const
     return aa_const
 
