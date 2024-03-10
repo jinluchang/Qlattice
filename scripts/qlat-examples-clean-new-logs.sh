7aa3222d08f40dacf5f13608724330b1ebdaa289
Luchang Jin <ljin.luchang@gmail.com>
2024-03-10 00:06:46 -0500

    small

--- scripts/qlat-examples-clean-new-logs.sh
+++ scripts/qlat-examples-clean-new-logs.sh
@@ -6,6 +6,9 @@ for log in examples-py/*.log examples-cpp/*/log ; do
     if [ -f "$log".new ] ; then
         rm -v "$log".new
     fi
+    if [ -f "$log".json.new ] ; then
+        rm -v "$log".json.new
+    fi
     if [ -f "$log".full.txt ] ; then
         rm -v "$log".full.txt
     fi
