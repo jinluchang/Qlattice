e9207016c8e2113de5613e73be8ca6b995238142
Luchang Jin <ljin.luchang@gmail.com>
2024-03-24 06:59:25 -0400

    small

--- examples-py/gpt-qlat-data-gen-hlbl.py
+++ examples-py/gpt-qlat-data-gen-hlbl.py
@@ -1409,7 +1409,7 @@ def run_hlbl_two_plus_two_chunk(
         psel_ps_prob @= fsel_ps_prob
         ps_hvp = q.SelectedPointsComplexD(psel_ps, 16)
         ps_hvp @= s_hvp
-        ps_xg_arr = q.get_mpi_chunk(psel_ps.xg_arr())
+        ps_xg_arr = q.get_mpi_chunk(list(psel_ps.xg_arr()), rng_state=q.RngState(f"get_mpi_chunk"))
         psel_lps = q.PointsSelection(ps_xg_arr, psel_ps.geo)
         psel_lps_prob = q.SelectedPointsRealD(psel_lps, 1)
         psel_lps_prob @= psel_ps_prob
