be12f2e2a349700e4ffd8b376e4956bb95fc6d54
Luchang Jin <ljin.luchang@gmail.com>
2024-05-11 20:04:27 -0400

    update examples

--- examples-py/field-selection.py
+++ examples-py/field-selection.py
@@ -14,14 +14,14 @@ total_site = q.Coordinate([4, 4, 4, 8])
 geo = q.Geometry(total_site, 1)
 q.displayln_info("CHECK: geo.show() =", geo.show())
 
-psel = q.PointsSelection([ [0,0,0,0], [0,1,2,0], ])
+psel = q.PointsSelection(total_site, [ [0,0,0,0], [0,1,2,0], ])
 
 psel.save("results/psel.txt")
 
 psel = q.PointsSelection()
 psel.load("results/psel.txt", geo)
 
-q.displayln_info("CHECK: psel.xg_arr().tolist() =", psel.xg_arr().tolist())
+q.displayln_info("CHECK: psel.xg_arr.tolist() =", psel.xg_arr.tolist())
 
 n_per_tslice = 16
 
@@ -47,16 +47,16 @@ q.displayln_info(f"CHECK: fsel info = {fsel.geo().show()} total_n_elems = {total
 
 psel = q.PointsSelection()
 psel.set_rand(total_site, 32, rs.split("psel"))
-q.displayln_info(f"CHECK: psel.n_points() = {psel.n_points()}")
+q.displayln_info(f"CHECK: psel.n_points = {psel.n_points}")
 
-xg_arr_double = np.array(psel.xg_arr(), dtype=float)
+xg_arr_double = np.array(psel.xg_arr, dtype=float)
 sig = q.get_data_sig(xg_arr_double, rs.split("sig"))
 q.displayln_info(f"CHECK: psel.xg_arr sig = {sig:.10E}")
 
 psel1 = psel.intersect(fsel)
-q.displayln_info(f"CHECK: psel1.n_points() = {psel1.n_points()}")
+q.displayln_info(f"CHECK: psel1.n_points = {psel1.n_points}")
 
-xg_arr_double = np.array(psel1.xg_arr(), dtype=float)
+xg_arr_double = np.array(psel1.xg_arr, dtype=float)
 sig = q.get_data_sig(xg_arr_double, rs.split("sig"))
 q.displayln_info(f"CHECK: psel1.xg_arr sig = {sig:.10E}")
 
