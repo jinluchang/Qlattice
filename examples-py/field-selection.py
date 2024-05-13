9c5e0574e480160b5fe5be70c24a80263b7ef315
Luchang Jin <ljin.luchang@gmail.com>
2024-05-12 21:43:07 -0400

    small

--- examples-py/field-selection.py
+++ examples-py/field-selection.py
@@ -11,7 +11,7 @@ q.qremove_all_info("results")
 q.qmkdir_info("results")
 rs = q.RngState("seed")
 total_site = q.Coordinate([4, 4, 4, 8])
-geo = q.Geometry(total_site, 1)
+geo = q.Geometry(total_site)
 q.displayln_info("CHECK: geo.show() =", geo.show())
 
 psel = q.PointsSelection(total_site, [ [0,0,0,0], [0,1,2,0], ])
