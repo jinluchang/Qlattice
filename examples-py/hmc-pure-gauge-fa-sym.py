b16225eaab6235d0ea6093d385df67e3e952bc38
Luchang Jin <ljin.luchang@gmail.com>
2024-06-21 01:25:29 +0800

    small

--- examples-py/hmc-pure-gauge-fa-sym.py
+++ examples-py/hmc-pure-gauge-fa-sym.py
@@ -794,6 +794,24 @@ set_param(job_tag, "hmc", "fa", "mass_type")("grid-2")
 set_param(job_tag, "hmc", "fa", "mass_list")([ 1.0, 1.0, 1.0, 9.0, ])
 set_param(job_tag, "hmc", "fa", "interval_list")([ 10, 10, 10, 10, ])
 
+job_tag = "32I_b2p8_fa_sym_v12"
+set_param(job_tag, "total_site")((32, 32, 32, 64,))
+set_param(job_tag, "a_inv_gev")(2.646) # 2003 lattice spacing 0309017.pdf
+set_param(job_tag, "hmc", "max_traj")(20000)
+set_param(job_tag, "hmc", "max_traj_always_accept")(100)
+set_param(job_tag, "hmc", "max_traj_reverse_test")(2)
+set_param(job_tag, "hmc", "md_time")(1.0 / 3.0)
+set_param(job_tag, "hmc", "n_step")(10)
+set_param(job_tag, "hmc", "beta")(2.80)
+set_param(job_tag, "hmc", "c1")(-0.331)
+set_param(job_tag, "hmc", "save_traj_interval")(10)
+set_param(job_tag, "hmc", "is_saving_topo_info")(True)
+set_param(job_tag, "hmc", "fa", "complete_refresh_interval")(10)
+set_param(job_tag, "hmc", "fa", "is_project_gauge_transform")(True)
+set_param(job_tag, "hmc", "fa", "mass_type")("grid-2")
+set_param(job_tag, "hmc", "fa", "mass_list")([ 1.0, 1.0, 1.0, 9.0, ])
+set_param(job_tag, "hmc", "fa", "interval_list")([ 1, 1, 1, 10, ])
+
 # ----
 
 size_node_list = [
