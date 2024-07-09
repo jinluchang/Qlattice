12a4d766a8c31578f3551cd23df6bb3c55bf50f3
Luchang Jin <ljin.luchang@gmail.com>
2024-07-10 04:19:01 +0800

    add more params

--- examples-py/hmc-pure-gauge-fa-sym.py
+++ examples-py/hmc-pure-gauge-fa-sym.py
@@ -1275,6 +1275,7 @@ set_param(job_tag, "hmc", "fa", "mass_list")([ 1.0, 1.0, 1.0, 1.0, np.inf, np.in
 set_param(job_tag, "hmc", "fa", "interval_list")([ 1, 1, 1, 1, 1, 1, 1, 1, ])
 
 job_tag = "32I_b2p8_fa_sym_v33"
+# Current best
 set_param(job_tag, "total_site")((32, 32, 32, 64,))
 set_param(job_tag, "a_inv_gev")(2.646) # 2003 lattice spacing 0309017.pdf
 set_param(job_tag, "hmc", "max_traj")(20000)
@@ -1417,6 +1418,78 @@ set_param(job_tag, "hmc", "fa", "is_project_gauge_transform")(True)
 set_param(job_tag, "hmc", "fa", "mass_type")("grid-2-eo")
 set_param(job_tag, "hmc", "fa", "mass_list")([ 1.0, 1.0, 1.0, 400.0, np.inf, np.inf, np.inf, np.inf, ])
 set_param(job_tag, "hmc", "fa", "interval_list")([ 1, 1, 1, 80, 1, 1, 1, 1, ])
+
+job_tag = "32I_b2p95_fa_sym_md8_v1"
+set_param(job_tag, "total_site")((32, 32, 32, 64,))
+set_param(job_tag, "a_inv_gev")(3.5) # rough guess
+set_param(job_tag, "hmc", "max_traj")(20000)
+set_param(job_tag, "hmc", "max_traj_always_accept")(100)
+set_param(job_tag, "hmc", "max_traj_reverse_test")(2)
+set_param(job_tag, "hmc", "md_time")(8.0)
+set_param(job_tag, "hmc", "n_step")(32 * 8)
+set_param(job_tag, "hmc", "beta")(2.95)
+set_param(job_tag, "hmc", "c1")(-0.331)
+set_param(job_tag, "hmc", "save_traj_interval")(2)
+set_param(job_tag, "hmc", "is_saving_topo_info")(True)
+set_param(job_tag, "hmc", "fa", "complete_refresh_interval")(8)
+set_param(job_tag, "hmc", "fa", "is_project_gauge_transform")(True)
+set_param(job_tag, "hmc", "fa", "mass_type")("grid-2-eo")
+set_param(job_tag, "hmc", "fa", "mass_list")([ 1.0, 1.0, 1.0, 1.0, np.inf, np.inf, np.inf, np.inf, ])
+set_param(job_tag, "hmc", "fa", "interval_list")([ 1, 1, 1, 8, 1, 1, 1, 1, ])
+
+job_tag = "32I_b2p95_fa_sym_md8_v2"
+set_param(job_tag, "total_site")((32, 32, 32, 64,))
+set_param(job_tag, "a_inv_gev")(3.5) # rough guess
+set_param(job_tag, "hmc", "max_traj")(20000)
+set_param(job_tag, "hmc", "max_traj_always_accept")(100)
+set_param(job_tag, "hmc", "max_traj_reverse_test")(2)
+set_param(job_tag, "hmc", "md_time")(8.0)
+set_param(job_tag, "hmc", "n_step")(32 * 8)
+set_param(job_tag, "hmc", "beta")(2.95)
+set_param(job_tag, "hmc", "c1")(-0.331)
+set_param(job_tag, "hmc", "save_traj_interval")(2)
+set_param(job_tag, "hmc", "is_saving_topo_info")(True)
+set_param(job_tag, "hmc", "fa", "complete_refresh_interval")(8)
+set_param(job_tag, "hmc", "fa", "is_project_gauge_transform")(True)
+set_param(job_tag, "hmc", "fa", "mass_type")("grid-2-eo")
+set_param(job_tag, "hmc", "fa", "mass_list")([ 1.0, 1.0, 1.0, 2.0, np.inf, np.inf, np.inf, np.inf, ])
+set_param(job_tag, "hmc", "fa", "interval_list")([ 1, 1, 1, 8, 1, 1, 1, 1, ])
+
+job_tag = "32I_b2p95_fa_sym_md8_v3"
+set_param(job_tag, "total_site")((32, 32, 32, 64,))
+set_param(job_tag, "a_inv_gev")(3.5) # rough guess
+set_param(job_tag, "hmc", "max_traj")(20000)
+set_param(job_tag, "hmc", "max_traj_always_accept")(100)
+set_param(job_tag, "hmc", "max_traj_reverse_test")(2)
+set_param(job_tag, "hmc", "md_time")(8.0)
+set_param(job_tag, "hmc", "n_step")(32 * 8)
+set_param(job_tag, "hmc", "beta")(2.95)
+set_param(job_tag, "hmc", "c1")(-0.331)
+set_param(job_tag, "hmc", "save_traj_interval")(2)
+set_param(job_tag, "hmc", "is_saving_topo_info")(True)
+set_param(job_tag, "hmc", "fa", "complete_refresh_interval")(8)
+set_param(job_tag, "hmc", "fa", "is_project_gauge_transform")(True)
+set_param(job_tag, "hmc", "fa", "mass_type")("grid-2-eo")
+set_param(job_tag, "hmc", "fa", "mass_list")([ 1.0, 1.0, 1.0, 4.0, np.inf, np.inf, np.inf, np.inf, ])
+set_param(job_tag, "hmc", "fa", "interval_list")([ 1, 1, 1, 8, 1, 1, 1, 1, ])
+
+job_tag = "32I_b2p95_fa_sym_md8_v4"
+set_param(job_tag, "total_site")((32, 32, 32, 64,))
+set_param(job_tag, "a_inv_gev")(3.5) # rough guess
+set_param(job_tag, "hmc", "max_traj")(20000)
+set_param(job_tag, "hmc", "max_traj_always_accept")(100)
+set_param(job_tag, "hmc", "max_traj_reverse_test")(2)
+set_param(job_tag, "hmc", "md_time")(8.0)
+set_param(job_tag, "hmc", "n_step")(32 * 8)
+set_param(job_tag, "hmc", "beta")(2.95)
+set_param(job_tag, "hmc", "c1")(-0.331)
+set_param(job_tag, "hmc", "save_traj_interval")(2)
+set_param(job_tag, "hmc", "is_saving_topo_info")(True)
+set_param(job_tag, "hmc", "fa", "complete_refresh_interval")(12)
+set_param(job_tag, "hmc", "fa", "is_project_gauge_transform")(True)
+set_param(job_tag, "hmc", "fa", "mass_type")("grid-2-eo")
+set_param(job_tag, "hmc", "fa", "mass_list")([ 1.0, 1.0, 1.0, 9.0, np.inf, np.inf, np.inf, np.inf, ])
+set_param(job_tag, "hmc", "fa", "interval_list")([ 1, 1, 1, 12, 1, 1, 1, 1, ])
 
 # ----
 
