1a90296536823115225247c49b12ba3c46c905e7
Luchang Jin <ljin.luchang@gmail.com>
2024-08-13 17:13:09 -0400

    upgrade version

--- qlat-utils/qlat_utils/lib/version_gen.sh
+++ qlat-utils/qlat_utils/lib/version_gen.sh
@@ -12,7 +12,7 @@ cd "$src_dir"
 if git describe --tags ; then
     version="$(git describe --tags)"
 else
-    version="v0.66-current" # default version
+    version="v0.67-current" # default version
 fi
 
 cd "$wd"
