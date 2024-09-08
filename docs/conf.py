572591a8fdba0e1635dd9639fb5da7f164144df5
Luchang Jin <ljin.luchang@gmail.com>
2024-09-08 03:00:02 -0400

    upgrade version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Qlattice'
 copyright = 'Luchang Jin'
 author = 'Luchang Jin'
-release = '0.69'
+release = '0.70'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- qlat-utils/qlat_utils/lib/version_gen.sh
+++ qlat-utils/qlat_utils/lib/version_gen.sh
@@ -12,7 +12,7 @@ cd "$src_dir"
 if git describe --tags ; then
     version="$(git describe --tags)"
 else
-    version="v0.69-current" # default version
+    version="v0.70-current" # default version
 fi
 
 cd "$wd"
