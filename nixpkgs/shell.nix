c401f1e090bfe7e523ddf69ae48535d49e412ec0
Luchang Jin <ljin.luchang@gmail.com>
2024-09-17 00:36:18 -0400

    small

--- nixpkgs/shell.nix
+++ nixpkgs/shell.nix
@@ -5,18 +5,20 @@
 let
   many-qlat-pkgs = import ./default.nix;
   #
-  qlat-pkgs = many-qlat-pkgs.qlat-pkgs-local;
+  # qlat-pkgs = many-qlat-pkgs.qlat-pkgs-local;
   # qlat-pkgs = many-qlat-pkgs.qlat-pkgs-cuda-local;
   # qlat-pkgs = many-qlat-pkgs.qlat-pkgs-std-clang-local;
   #
-  # qlat-pkgs = let pkgs = many-qlat-pkgs.all-pkgs-cuda-local; in [
-  #   # pkgs.qlat-nixgl
-  #   (pkgs.python3.withPackages (ps: with pkgs; [
-  #     qlat_utils
-  #     qlat
-  #     gpt-lehner
-  #   ]))
-  # ];
+  qlat-pkgs = let pkgs = many-qlat-pkgs.all-pkgs-cuda-local; in [
+    # pkgs.qlat-nixgl
+    (pkgs.python3.withPackages (ps: with pkgs; [
+      qlat_utils
+      qlat
+      qlat_cps
+      qlat_grid
+      gpt-lehner
+    ]))
+  ];
   # qlat-pkgs = let pkgs = many-qlat-pkgs.all-pkgs-local; in [
   #   (pkgs.python3.withPackages (ps: with pkgs; [
   #     qlat_utils
