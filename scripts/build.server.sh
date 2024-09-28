d43783d88a4a2457ca6eb1ceb84fd1a8cdb76571
Luchang Jin <ljin.luchang@gmail.com>
2024-09-28 18:47:01 -0400

    small

--- scripts/build.server.sh
+++ scripts/build.server.sh
@@ -58,7 +58,7 @@ set -e
     ./scripts/qlat-examples-py-cps.sh
     ./scripts/qlat-examples-cpp-grid.sh
 
-    ./scripts/hadrons.sh
+    # ./scripts/hadrons.sh
 
     date
 } 2>&1 | tee $prefix/log.build.txt
