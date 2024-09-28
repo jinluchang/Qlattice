a8c514bc7af4fd0ab9cd46247bcca52086fdbc42
Luchang Jin <ljin.luchang@gmail.com>
2024-09-27 22:44:11 -0400

    add jlab 24s

---
+++ scripts/build.jlab24s.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -e
+
+{
+    ./scripts/setenv.default.sh
+    ./scripts/qcore.sh
+
+    ./scripts/xz.sh
+    ./scripts/zlib.sh
+    ./scripts/bzip2.sh
+    ./scripts/tar.sh
+    ./scripts/gsl.sh
+    ./scripts/gmp.sh
+    ./scripts/mpfr.sh
+    ./scripts/mpc.sh
+    ./scripts/isl.sh
+    ./scripts/gcc.sh
+    ./scripts/bison.sh
+    ./scripts/binutils.sh
+    ./scripts/perl.sh
+    ./scripts/openssl.sh
+    ./scripts/cmake.sh
+    ./scripts/libffi.sh
+    ./scripts/openmpi.sh
+
+    ./scripts/hdf5.sh
+    ./scripts/openblas.sh
+    ./scripts/fftw.sh
+    ./scripts/cuba.sh
+    ./scripts/eigen.sh
+
+    rm -rfv ~/.cache/pip
+    ./scripts/python.sh
+    ./scripts/python-pip.sh
+    ./scripts/re2c.sh
+    ./scripts/ninja.sh
+    ./scripts/ninja-script.sh
+    ./scripts/python-meson.sh
+    ./scripts/python-meson-py.sh
+    ./scripts/python-packages.sh
+
+    ./scripts/autoconf.sh
+    ./scripts/automake.sh
+    ./scripts/c-lime.sh
+    ./scripts/qmp.sh
+    ./scripts/qio.sh
+    ./scripts/cps.sh
+    ./scripts/grid-clehner.avx512.sh
+    ./scripts/gpt.sh
+
+    ./scripts/qlat-packages.sh
+
+    ./scripts/qlat-examples-py.sh
+    ./scripts/qlat-examples-cpp.sh
+    ./scripts/qlat-examples-py-gpt.sh
+    ./scripts/qlat-examples-py-cps.sh
+    ./scripts/qlat-examples-cpp-grid.sh
+
+    ./scripts/hadrons.sh
+
+    date
+} 2>&1 | tee $prefix/log.build.txt
---
+++ scripts/grid-clehner.avx512.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+name=Grid-clehner
+
+source qcore/set-prefix.sh $name
+
+{ time {
+    echo "!!!! build $name !!!!"
+    source qcore/conf.sh ..
+
+    mkdir -p "$src_dir" || true
+    time-run rsync -a --delete $distfiles/$name "$src_dir"/
+    cd "$src_dir/$name"
+
+    INITDIR="$(pwd)"
+    rm -rfv "${INITDIR}/Eigen/Eigen/unsupported"
+    rm -rfv "${INITDIR}/Grid/Eigen"
+    if [ -n "$(find-header.sh Eigen/Eigen)" ] ; then
+        eigen_path="$(find-header.sh Eigen/Eigen)"
+        rsync -av --delete "$eigen_path/include/Eigen/" ${INITDIR}/Grid/Eigen/
+        rsync -av --delete "$eigen_path/include/unsupported/Eigen/" ${INITDIR}/Grid/Eigen/unsupported/
+        cd ${INITDIR}/Grid
+        echo 'eigen_files =\' > ${INITDIR}/Grid/Eigen.inc
+        find -L Eigen -type f -print | sed 's/^/  /;$q;s/$/ \\/' >> ${INITDIR}/Grid/Eigen.inc
+        cd ${INITDIR}
+    else
+        ln -vs "${INITDIR}/Eigen/Eigen" "${INITDIR}/Grid/Eigen"
+        ln -vs "${INITDIR}/Eigen/unsupported/Eigen" "${INITDIR}/Grid/Eigen/unsupported"
+    fi
+
+    export CXXFLAGS="$CXXFLAGS -fPIC -w -Wno-psabi"
+
+    opts=""
+    if [ -n "$(find-library.sh libgmp.a)" ] ; then
+        opts+=" --with-gmp=$(find-library.sh libgmp.a)"
+    fi
+    if [ -n "$(find-library.sh libmpfr.a)" ] ; then
+        opts+=" --with-mpfr=$(find-library.sh libmpfr.a)"
+    fi
+    if [ -n "$(find-library.sh libfftw3.a)" ] ; then
+        opts+=" --with-fftw=$(find-library.sh libfftw3.a)"
+    fi
+    if [ -n "$(find-library.sh liblime.a)" ] ; then
+        opts+=" --with-lime=$(find-library.sh liblime.a)"
+    fi
+    if [ -n "$(find-library.sh libcrypto.a)" ] ; then
+        opts+=" --with-openssl=$(find-library.sh libcrypto.a)"
+    fi
+    if [ -n "$(find-library.sh libhdf5_hl_cpp.a)" ] ; then
+        opts+=" --with-hdf5=$(find-library.sh libhdf5_hl_cpp.a)"
+    fi
+
+    if [ -n "$(find-library.sh libz.a)" ] ; then
+        LDFLAGS+=" -L$(find-library.sh libz.a)/lib"
+        CXXFLAGS+=" -I$(find-library.sh libz.a)/include"
+        export LDFLAGS
+        export CXXFLAGS
+    fi
+
+    mkdir build
+    cd build
+    time-run ../configure \
+        --enable-simd=AVX512 \
+        --enable-alloc-align=4k \
+        --enable-comms=mpi-auto \
+        --enable-gparity=no \
+        $opts \
+        --prefix="$prefix"
+
+    time-run make -j$num_proc
+    time-run make install
+
+    mk-setenv.sh
+    echo "!!!! $name build !!!!"
+    rm -rf "$temp_dir" || true
+} } 2>&1 | tee "$prefix/log.$name.txt"
