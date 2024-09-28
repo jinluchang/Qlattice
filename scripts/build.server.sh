48aebb82c1980cbfe9af4534a04de214d7ad2f90
Luchang Jin <ljin.luchang@gmail.com>
2024-09-28 14:56:21 -0400

    small

--- scripts/build.server.sh
+++ scripts/build.server.sh
@@ -49,11 +49,6 @@ set -e
     ./scripts/grid-clehner.avx2.sh
     ./scripts/gpt.sh
 
-    ./scripts/qlat-utils.sh
-    ./scripts/qlat.sh
-    ./scripts/qlat-cps.sh
-    ./scripts/qlat-grid.sh
-
     # ./scripts/qlat-packages.sh
     ./scripts/qlat-all.sh
 
