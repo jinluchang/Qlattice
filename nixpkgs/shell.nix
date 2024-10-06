f6805aecff4e43270c402816d766cbeda67b9888
Luchang Jin <ljin.luchang@gmail.com>
2024-10-06 03:45:57 -0400

    small

--- nixpkgs/shell.nix
+++ nixpkgs/shell.nix
@@ -6,5 +6,6 @@ let
   qlat-pkgs = import ./qlat-pkgs.nix { inherit nixpkgs; };
   many-qlat-pkgs-all = qlat-pkgs.many-qlat-pkgs-all;
   env1 = many-qlat-pkgs-all.qlat-env-local;
-  env2 = many-qlat-pkgs-all.qlat-env-cuda-local;
+  env2 = many-qlat-pkgs-all.qlat-env-std-local;
+  env3 = many-qlat-pkgs-all.qlat-env-std-clang-local;
 in env1
