3411f029fa58124b94396c5acdd477025fa6a7a3
Luchang Jin <ljin.luchang@gmail.com>
2024-10-06 15:40:38 -0400

    small fix

--- examples-py/gpt-qlat-data-gen-auto.py
+++ examples-py/gpt-qlat-data-gen-auto.py
@@ -1479,17 +1479,14 @@ def get_cexpr_tadpole_current():
         diagram_type_dict[((('x_1', 'x_2'), 1), (('x_2', 'x_1'), 1))] = 'TypeC'
         diagram_type_dict[((('x_1', 'x_1'), 1), (('x_2', 'x_2'), 1))] = None
         #
-        jj_list = [
-            e for l in [
-                [
+        jj_list = []
+        for mu in range(4):
+            jj_list += [
                     1/3 * mk_vec_mu("l", "s", "x_1", mu) * mk_scalar("s", "l", "x_2"),
                     1/3 * mk_vec_mu("s", "l", "x_1", mu) * mk_scalar("l", "s", "x_2"),
                     1/3 * mk_vec_mu("l", "l", "x_1", mu) * mk_scalar("l", "l", "x_2"),
                     1/3 * mk_vec_mu("s", "s", "x_1", mu) * mk_scalar("s", "s", "x_2"),
-                ]
-                for mu in range(4)
-            ] for e in l
-        ]
+                    ]
         #
         exprs = [
                 mk_expr(1) + f"1",
