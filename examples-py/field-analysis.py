d08d85d1818329e5448104b4e2cb9404d7e68b1a
Luchang Jin <ljin.luchang@gmail.com>
2024-10-07 15:22:13 -0400

    add field-analysis examples

--- examples-py/field-analysis.py
+++ examples-py/field-analysis.py
@@ -51,6 +51,47 @@ ff3d = q.field_convolution(f1, f2, idx1, idx2, is_only_spatial=True)
 
 json_results.append(("ff3d data sig", q.get_data_sig(ff3d, rs), check_eps,))
 
+def check_convolution_4d(f1, f2, idx1, idx2, ff, xg_rel):
+    fsel = q.FieldSelection(geo, 0)
+    psel = fsel.to_psel()
+    s = 0
+    for xg in psel:
+        xg1 = xg
+        xg2 = (xg + xg_rel) % total_site
+        v1 = f1.get_elems_xg(xg1)[0, idx1]
+        v2 = f2.get_elems_xg(xg2)[0, idx2]
+        s += v1 * v2
+    s_ref = ff.get_elems_xg(xg_rel)[0]
+    diff = s - s_ref
+    assert q.qnorm(diff) <= 1e-12 * q.qnorm(s)
+    assert q.qnorm(diff) <= 1e-12 * q.qnorm(s_ref)
+
+def check_convolution_3d(f1, f2, idx1, idx2, ff, xg_rel):
+    t = xg_rel[3]
+    xg_rel_3d = xg_rel.copy()
+    xg_rel_3d[3] = 0
+    fsel = q.FieldSelection(geo, 0)
+    psel = fsel.to_psel()
+    s = 0
+    for xg in psel:
+        if xg[3] != t:
+            continue
+        xg1 = xg
+        xg2 = (xg + xg_rel_3d) % total_site
+        v1 = f1.get_elems_xg(xg1)[0, idx1]
+        v2 = f2.get_elems_xg(xg2)[0, idx2]
+        s += v1 * v2
+    s_ref = ff.get_elems_xg(xg_rel)[0]
+    diff = s - s_ref
+    assert q.qnorm(diff) <= 1e-12 * q.qnorm(s)
+    assert q.qnorm(diff) <= 1e-12 * q.qnorm(s_ref)
+
+rsi = rs.split("c-rand-gen")
+for i in range(4):
+    xg_rel = rsi.c_rand_gen(total_site)
+    check_convolution_4d(f1, f2, idx1, idx2, ff, xg_rel)
+    check_convolution_3d(f1, f2, idx1, idx2, ff3d, xg_rel)
+
 q.check_log_json(__file__, json_results)
 
 q.timer_display()
