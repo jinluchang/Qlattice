7a846238691a02b63d1b3617a679d482d768fe26
Luchang Jin <ljin.luchang@gmail.com>
2024-11-04 00:23:49 -0500

    update example

--- examples-py/set-rand.py
+++ examples-py/set-rand.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+json_results = []
+check_eps = 1e-14
+
 import sys
 import qlat as q
 
@@ -14,21 +17,18 @@ size_node_list = [
 
 q.begin_with_mpi(size_node_list)
 
-# q.qremove_all_info("results")
-# q.qmkdir_info("results")
-
 rs = q.RngState("seed")
 rs_prop = rs.split("prop")
 
 total_site = q.Coordinate([ 4, 4, 4, 8, ])
 geo = q.Geometry(total_site)
 
-q.displayln_info(f"CHECK: total_site = {total_site}")
-q.displayln_info(f"CHECK: geo = {geo}")
+json_results.append((f"total_site = {total_site}", 0,))
+json_results.append((f"geo = {geo}", 0,))
 
 prop = q.Prop(geo)
 prop.set_rand(rs_prop, 1.0, 0.0)
-q.displayln_info(f"CHECK: prop.crc32() = {prop.crc32()} ; prop.qnorm() = {prop.qnorm():.13E}")
+json_results.append((f"prop.crc32() = {prop.crc32()} ; prop.qnorm()", prop.qnorm(),))
 
 n_points = 16
 psel = q.PointsSelection()
@@ -43,54 +43,56 @@ fselc.add_psel(psel)
 
 sc_prop = q.SelProp(fselc)
 sc_prop @= prop
-q.displayln_info(f"CHECK: sc_prop.qnorm() = {sc_prop.qnorm():.13E}")
+json_results.append((f"sc_prop.qnorm()", sc_prop.qnorm(),))
 
 sc_prop1 = q.SelProp(fselc)
 sc_prop1.set_rand(rs_prop, 1.0, 0.0)
 sc_prop1 -= sc_prop
-q.displayln_info(f"CHECK: sc_prop1.qnorm() = {sc_prop1.qnorm():.13E}")
+json_results.append((f"sc_prop1.qnorm()", sc_prop1.qnorm(),))
 
 s_prop = q.SelProp(fsel)
 s_prop @= prop
-q.displayln_info(f"CHECK: s_prop.qnorm() = {s_prop.qnorm():.13E}")
+json_results.append((f"s_prop.qnorm()", s_prop.qnorm(),))
 
 s_prop1 = q.SelProp(fsel)
 s_prop1.set_rand(rs_prop, 1.0, 0.0)
 s_prop1 -= s_prop
-q.displayln_info(f"CHECK: s_prop1.qnorm() = {s_prop1.qnorm():.13E}")
+json_results.append((f"s_prop1.qnorm()", s_prop1.qnorm(),))
 
 s_prop2 = q.SelProp(fsel)
 s_prop2 @= sc_prop
 s_prop2 -= s_prop
-q.displayln_info(f"CHECK: s_prop2.qnorm() = {s_prop2.qnorm():.13E}")
+json_results.append((f"s_prop2.qnorm()", s_prop2.qnorm(),))
 
 sp_prop = q.PselProp(psel)
 sp_prop @= prop
-q.displayln_info(f"CHECK: sp_prop.qnorm() = {sp_prop.qnorm():.13E}")
+json_results.append((f"sp_prop.qnorm()", sp_prop.qnorm(),))
 
 sp_prop1 = q.PselProp(psel)
 sp_prop1 @= sc_prop
 sp_prop1 -= sp_prop
-q.displayln_info(f"CHECK: sp_prop1.qnorm() = {sp_prop1.qnorm():.13E}")
+json_results.append((f"sp_prop1.qnorm()", sp_prop1.qnorm(),))
 
 prop_norm = q.sqrt_field(q.qnorm_field(prop))
-q.displayln_info(f"CHECK: prop_norm.qnorm() = {prop_norm.qnorm():.13E}")
+json_results.append((f"prop_norm.qnorm()", prop_norm.qnorm(),))
 
 s_prop_norm = q.sqrt_field(q.qnorm_field(s_prop))
-q.displayln_info(f"CHECK: s_prop_norm.qnorm() = {s_prop_norm.qnorm():.13E}")
+json_results.append((f"s_prop_norm.qnorm()", s_prop_norm.qnorm(),))
 
 s_prop_norm1 = q.SelectedField(q.ElemTypeRealD, fsel)
 s_prop_norm1 @= prop_norm
 s_prop_norm1 -= s_prop_norm
-q.displayln_info(f"CHECK: s_prop_norm1.qnorm() = {s_prop_norm1.qnorm():.13E}")
+json_results.append((f"s_prop_norm1.qnorm()", s_prop_norm1.qnorm(),))
 
 sp_prop_norm = q.sqrt_field(q.qnorm_field(sp_prop))
-q.displayln_info(f"CHECK: sp_prop_norm.qnorm() = {sp_prop_norm.qnorm():.13E}")
+json_results.append((f"sp_prop_norm.qnorm()", sp_prop_norm.qnorm(),))
 
 sp_prop_norm1 = q.SelectedPoints(q.ElemTypeRealD, psel)
 sp_prop_norm1 @= prop_norm
 sp_prop_norm1 -= sp_prop_norm
-q.displayln_info(f"CHECK: sp_prop_norm1.qnorm() = {sp_prop_norm1.qnorm():.13E}")
+json_results.append((f"sp_prop_norm1.qnorm()", sp_prop_norm1.qnorm(),))
+
+q.check_log_json(__file__, json_results, check_eps=check_eps)
 
 q.timer_display()
 
