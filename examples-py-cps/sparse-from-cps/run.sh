05872ca73202d8a7d3bcf4bf261b8ef1336ec597
Luchang Jin <ljin.luchang@gmail.com>
2025-01-08 12:39:33 -0500

    small

--- examples-py-cps/sparse-from-cps/run.sh
+++ examples-py-cps/sparse-from-cps/run.sh
@@ -6,14 +6,14 @@ export q_mem_cache_max_size=512 # MB
 export q_alloc_mem_max_size=$((256 * 1024)) # MB
 export q_malloc_mmap_threshold=8192
 
-grep "^CHECK: " log.txt >log.check.txt
+grep -a "^CHECK: " log.txt >log.check.txt
 
 timeout -s KILL 60m time mpiexec --np 4 $mpi_options python3 -m mpi4py ./run.py -qmp-geom 1 1 1 4 --mpi 1.1.1.4 >log.full.txt 2>&1
 
 name="$(basename "$PWD")"
 
-grep "^CHECK: \|^INFO: " log.full.txt >log.txt
-grep "^CHECK: " log.txt >log.check.txt.new
+grep -a "^CHECK: \|^INFO: " log.full.txt >log.txt
+grep -a "^CHECK: " log.txt >log.check.txt.new
 
 if diff log.check.txt log.check.txt.new ; then
     echo passed
