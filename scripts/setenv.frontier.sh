e27f15c0936fbfd15533e7b353a221cb83d28efb
Luchang Jin <ljin.luchang@gmail.com>
2025-01-26 18:50:09 -0500

    small

--- scripts/setenv.frontier.sh
+++ scripts/setenv.frontier.sh
@@ -14,17 +14,20 @@ if [ -z "\$num_proc" ] ; then
     export num_proc=8
 fi
 # module purge
+#
 if [ "\$PRG_ENV" = GNU ] ; then
     module load PrgEnv-gnu
 else
     module load PrgEnv-amd
-    module load craype-accel-amd-gfx90a
-    module load rocm
-    export MPICH_GPU_SUPPORT_ENABLED=1
-    export CFLAGS="-I\${ROCM_PATH}/include"
-    export CXXFLAGS="-I\${ROCM_PATH}/include"
-    export LDFLAGS="-L\${ROCM_PATH}/lib -lamdhip64"
 fi
+#
+module load craype-accel-amd-gfx90a
+module load rocm
+export MPICH_GPU_SUPPORT_ENABLED=1
+export CFLAGS="-I\${ROCM_PATH}/include"
+export CXXFLAGS="-I\${ROCM_PATH}/include"
+export LDFLAGS="-L\${ROCM_PATH}/lib -lamdhip64"
+#
 module load craype-x86-trento
 module load xpmem
 module load perftools-base
