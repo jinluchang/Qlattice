d409b01ca38b551efebe0f530c77e6c85388ce12
Luchang Jin <ljin.luchang@gmail.com>
2025-02-18 00:01:39 -0500

    small

--- nixpkgs/build-many-qlat-pkgs.sh
+++ nixpkgs/build-many-qlat-pkgs.sh
@@ -16,49 +16,45 @@ cd "$dst"
 time (
 for name in \
     "" \
-    "-cu" \
-    "-cuda" \
-    "-cudasupport" \
     "-std" \
     "-gridless" \
     "-cpsless" \
+    "-cu" \
+    "-cuda" \
+    "-cudasupport" \
     "-gridless-cubaquadless" \
-    "-std-clang" \
+    "-gridless-clang" \
     "-ucxless" \
     "-pypi" \
+    \
     "-std-ucxless" \
     "-std-clang-ucxless" \
-    "-cu-ucxless" \
-    "-cuda-ucxless" \
+    \
     "-std-cu" \
     "-std-cuda" \
-    "-std-cu-ucxless" \
-    "-std-cuda-ucxless" \
-    "-pypi" \
+    "-std-cudasupport" \
     ; do
     time nix-build "$src"/q-pkgs.nix -A qlat-jhub-tests"$name" -o result-24-11"$name" --arg nixpkgs 'import (fetchTarball "https://channels.nixos.org/nixos-24.11/nixexprs.tar.xz")' "$@"
 done
 for name in \
     "" \
-    "-cu" \
-    "-cuda" \
-    "-cudasupport" \
     "-std" \
     "-gridless" \
     "-cpsless" \
+    "-cu" \
+    "-cuda" \
+    "-cudasupport" \
     "-gridless-cubaquadless" \
-    "-std-clang" \
+    "-gridless-clang" \
     "-ucxless" \
     "-pypi" \
+    \
     "-std-ucxless" \
     "-std-clang-ucxless" \
-    "-cu-ucxless" \
-    "-cuda-ucxless" \
+    \
     "-std-cu" \
     "-std-cuda" \
-    "-std-cu-ucxless" \
-    "-std-cuda-ucxless" \
-    "-pypi" \
+    "-std-cudasupport" \
     ; do
     time nix-build "$src"/q-pkgs.nix -A qlat-jhub-tests"$name" -o result-24-05"$name" --arg nixpkgs 'import (fetchTarball "https://channels.nixos.org/nixos-24.05/nixexprs.tar.xz")' "$@"
 done
