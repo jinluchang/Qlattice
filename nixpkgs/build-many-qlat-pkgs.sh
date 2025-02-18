b8c905c1d388eaa7ec6fd0dd851e512e60a3a144
Luchang Jin <ljin.luchang@gmail.com>
2025-02-18 12:50:57 -0500

    small fix

--- nixpkgs/build-many-qlat-pkgs.sh
+++ nixpkgs/build-many-qlat-pkgs.sh
@@ -18,7 +18,15 @@ time (
 
 for nix_version in "24.11" "24.05" ; do
     time nix-build "$src"/q-pkgs.nix -A qlat-name-list-file -o result-qlat-name-"$nix_version" --arg nixpkgs "import (fetchTarball \"https://channels.nixos.org/nixos-$nix_version/nixexprs.tar.xz\")" "$@"
+    echo
+    echo result-qlat-name-"$nix_version"
+    echo
+    cat result-qlat-name-"$nix_version"
+    echo
     for name in $(cat result-qlat-name-"$nix_version") ; do
+        echo
+        echo "Building $nix_version $name"
+        echo
         time nix-build "$src"/q-pkgs.nix -A q-pkgs"$name" -o result-"$nix_version$name" --arg nixpkgs "import (fetchTarball \"https://channels.nixos.org/nixos-$nix_version/nixexprs.tar.xz\")" "$@"
     done
 done
