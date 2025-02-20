14ddecd50b86bc20c63ba7f4527c1299cb2d5ce7
Luchang Jin <ljin.luchang@gmail.com>
2025-02-20 09:21:56 -0500

    add some docs

--- nixpkgs/install-py-local-kernel-with-nix.sh
+++ nixpkgs/install-py-local-kernel-with-nix.sh
@@ -1,17 +1,34 @@
 #!/usr/bin/env bash
 
+cat <<EOF
+Usage:
+
+    name='' ./install-py-local-kernel-with-nix.sh
+    # Install a local kernel with Qlattice and Grid/GPT.
+
+    name='-cu' ./install-py-local-kernel-with-nix.sh
+    # Install a local kernel with some CUDA utilities. Qlattice and Grid/GPT are NOT compiled with CUDA.
+
+    name='-cuda' ./install-py-local-kernel-with-nix.s
+    # Install a local kernel with some CUDA utilities. Qlattice and Grid/GPT are compiled with CUDA.
+
+    name='-cudasupport' ./install-py-local-kernel-with-nix.sh
+    # Install a local kernel with full CUDA support when possible.
+
+EOF
+
 script_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 if [ -f "$script_path"/q-pkgs.nix ]; then
     :
 else
-    echo "Need to run the script inside its original directory, which also have the file q-pkgs.nix and other files."
+    echo "Need to place the script inside its original directory, which also have the file q-pkgs.nix and other files."
     exit 1
 fi
 
-if [ -z "$name" ] ; then
+if [ -z "${name+x}" ] ; then
     name=""
-    echo "You can export name=-cuda to enable CUDA support."
+    echo "You can use 'export name=-cuda' to compile Qlattice with CUDA."
 else
     echo "name='$name'"
     echo "Trying to build 'pkgs$name.qlat-jhub-env'."
@@ -25,6 +42,7 @@ mkdir -p "$dst"
 time nix-build "$src"/q-pkgs.nix -A pkgs$name.qlat-jhub-env -o "$dst/result" "$@"
 if [ ! -e "$dst"/result/bin/python3 ] ; then
     rmdir "$dst"
+    exit 1
 fi
 ls -l "$dst"
 "$dst"/result/bin/python3 -m ipykernel \
