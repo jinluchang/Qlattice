25b2adde795426cbf6100657a2f040e999dc77ca
Luchang Jin <ljin.luchang@gmail.com>
2025-02-28 00:15:16 -0500

    improve pipi-qed example

--- examples-py-gpt/gpt-qlat-data-gen-pipi-qed.py
+++ examples-py-gpt/gpt-qlat-data-gen-pipi-qed.py
@@ -29,10 +29,11 @@ is_cython = False
 load_path_list[:] = [
         "results",
         "qcddata",
-        os.path.join(q.getenv("HOME"), "share/qcddata-16IH2/16IH2"),
         "/lustre20/volatile/qcdqedta/qcddata",
         "/lustre20/volatile/decay0n2b/qcddata",
         "/lustre20/volatile/pqpdf/ljin/qcddata",
+        "/data1/qcddata3",
+        "/data2/qcddata3-prop",
         ]
 
 # ----
@@ -134,7 +135,7 @@ def get_cexpr_pipi_corr():
         diagram_type_dict = dict()
         exprs = [
                 mk_fac(1) + f"1",
-                mk_pipi_i22("y_1", "y_2", True) * mk_pipi_i22("x_1", "x_2") + f"pipi_i22+^dag(0) * pipi_i22(-tsep)",
+                mk_pipi_i22("snk_1", "snk_2", True) * mk_pipi_i22("src_1", "src_2") + f"pipi_i22+^dag(0) * pipi_i22(-tsep)",
                 ]
         cexpr = contract_simplify_compile(*exprs, is_isospin_symmetric_limit=True, diagram_type_dict=diagram_type_dict)
         return cexpr
@@ -146,7 +147,7 @@ def auto_contract_pipi_corr(job_tag, traj, get_get_prop, get_psel_prob, get_fsel
     fn = f"{job_tag}/auto-contract/traj-{traj}/pipi_corr.lat"
     if get_load_path(fn) is not None:
         return
-    cexpr = get_cexpr_meson_corr()
+    cexpr = get_cexpr_pipi_corr()
     expr_names = get_expr_names(cexpr)
     total_site = q.Coordinate(get_param(job_tag, "total_site"))
     t_size = total_site[3]
@@ -180,10 +181,10 @@ def auto_contract_pipi_corr(job_tag, traj, get_get_prop, get_psel_prob, get_fsel
     def feval(args):
         t_snk, t_src, t_sep_idx, = args
         pd = {
-                "y_1" : ("wall", t_snk,),
-                "y_2" : ("wall", (t_snk + pipi_op_t_sep) % t_size,),
-                "x_1" : ("wall", t_src,),
-                "x_2" : ("wall", (t_src - pipi_op_t_sep) % t_size,),
+                "snk_1" : ("wall", t_snk,),
+                "snk_2" : ("wall", (t_snk + pipi_op_t_sep) % t_size,),
+                "src_1" : ("wall", t_src,),
+                "src_2" : ("wall", (t_src - pipi_op_t_sep) % t_size,),
                 "size" : total_site,
                 }
         val = eval_cexpr(cexpr, positions_dict=pd, get_prop=get_prop)
@@ -343,7 +344,7 @@ def auto_contract_meson_jj(job_tag, traj, get_get_prop, get_psel_prob, get_fsel_
     psel_prob_arr = psel_prob[:].ravel()
     xg_fsel_arr = fsel.to_psel_local()[:]
     xg_psel_arr = psel[:]
-    tsep = get_param(job_tag, "measurement", "meson_tensor_tsep")
+    t_sep = get_param(job_tag, "measurement", "meson_tensor_t_sep")
     geo = q.Geometry(total_site)
     total_volume = geo.total_volume
     r_list = get_r_list(job_tag)
@@ -370,8 +371,8 @@ def auto_contract_meson_jj(job_tag, traj, get_get_prop, get_psel_prob, get_fsel_
             x_rel_t = x_rel[3]
             x_2_t = xg_src[3]
             x_1_t = x_2_t + x_rel_t
-            t_2 = (max(x_1_t, x_2_t) + tsep) % total_site[3]
-            t_1 = (min(x_1_t, x_2_t) - tsep) % total_site[3]
+            t_2 = (max(x_1_t, x_2_t) + t_sep) % total_site[3]
+            t_1 = (min(x_1_t, x_2_t) - t_sep) % total_site[3]
             pd = {
                     "x_2" : ("point-snk", xg_snk,),
                     "x_1" : ("point", xg_src,),
@@ -399,6 +400,200 @@ def auto_contract_meson_jj(job_tag, traj, get_get_prop, get_psel_prob, get_fsel_
     res_sum *= 1.0 / total_volume
     ld_sum = q.mk_lat_data([
         [ "expr_name", len(expr_names), expr_names, ],
+        [ "t", t_size, [ str(q.rel_mod(t, t_size)) for t in range(t_size) ], ],
+        [ "r", len(r_list), [ f"{r:.5f}" for r in r_list ], ],
+        ])
+    ld_sum.from_numpy(res_sum)
+    ld_sum.save(get_save_path(fn))
+    json_results_append(f"{fname}: ld_sum sig", q.get_data_sig(ld_sum, q.RngState()))
+    for i, en in enumerate(expr_names):
+        json_results_append(f"{fname}: ld_sum '{en}' sig", q.get_data_sig(ld_sum[i], q.RngState()))
+
+# ----
+
+@q.timer
+def get_cexpr_pipi_jj():
+    fn_base = "cache/auto_contract_cexpr/get_cexpr_pipi_jj"
+    def calc_cexpr():
+        diagram_type_dict = dict()
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'src_2'), 1), (('src_1', 'snk_2'), 1), (('src_2', 'x_1'), 1), (('x_1', 'x_2'), 1), (('x_2', 'snk_1'), 1))] = 'Type1'
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'src_2'), 1), (('src_1', 'snk_1'), 1), (('src_2', 'x_1'), 1), (('x_1', 'x_2'), 1), (('x_2', 'snk_2'), 1))] = 'Type2'
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'x_1'), 1), (('src_1', 'snk_2'), 1), (('src_2', 'snk_1'), 1), (('x_1', 'x_2'), 1), (('x_2', 'src_2'), 1))] = 'Type3'
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'x_1'), 1), (('src_1', 'snk_1'), 1), (('src_2', 'snk_2'), 1), (('x_1', 'x_2'), 1), (('x_2', 'src_2'), 1))] = 'Type4'
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'x_1'), 1), (('src_1', 'snk_2'), 1), (('src_2', 'x_2'), 1), (('x_1', 'src_2'), 1), (('x_2', 'snk_1'), 1))] = 'Type5'
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'src_2'), 1), (('src_1', 'x_1'), 1), (('src_2', 'x_2'), 1), (('x_1', 'snk_1'), 1), (('x_2', 'snk_2'), 1))] = 'Type6'
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'x_1'), 1), (('src_1', 'x_2'), 1), (('src_2', 'snk_2'), 1), (('x_1', 'src_2'), 1), (('x_2', 'snk_1'), 1))] = 'Type7'
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'src_2'), 1), (('src_1', 'x_1'), 1), (('src_2', 'x_2'), 1), (('x_1', 'snk_2'), 1), (('x_2', 'snk_1'), 1))] = 'Type8'
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'x_1'), 1), (('src_1', 'x_2'), 1), (('src_2', 'snk_1'), 1), (('x_1', 'src_2'), 1), (('x_2', 'snk_2'), 1))] = 'Type9'
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'x_1'), 1), (('src_1', 'snk_1'), 1), (('src_2', 'x_2'), 1), (('x_1', 'src_2'), 1), (('x_2', 'snk_2'), 1))] = 'Type10'
+        diagram_type_dict[((('snk_1', 'x_1'), 1), (('snk_2', 'x_2'), 1), (('src_1', 'snk_1'), 1), (('src_2', 'snk_2'), 1), (('x_1', 'src_1'), 1), (('x_2', 'src_2'), 1))] = 'Type11'
+        diagram_type_dict[((('snk_1', 'x_1'), 1), (('snk_2', 'x_2'), 1), (('src_1', 'snk_1'), 1), (('src_2', 'snk_2'), 1), (('x_1', 'src_2'), 1), (('x_2', 'src_1'), 1))] = 'Type12'
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'src_2'), 1), (('src_1', 'snk_2'), 1), (('src_2', 'x_1'), 1), (('x_1', 'snk_1'), 1), (('x_2', 'x_2'), 1))] = None
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'src_2'), 1), (('src_1', 'snk_1'), 1), (('src_2', 'x_1'), 1), (('x_1', 'snk_2'), 1), (('x_2', 'x_2'), 1))] = None
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'x_1'), 1), (('src_1', 'snk_2'), 1), (('src_2', 'snk_1'), 1), (('x_1', 'src_2'), 1), (('x_2', 'x_2'), 1))] = None
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'x_1'), 1), (('src_1', 'snk_1'), 1), (('src_2', 'snk_2'), 1), (('x_1', 'src_2'), 1), (('x_2', 'x_2'), 1))] = None
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'src_2'), 1), (('src_1', 'snk_1'), 1), (('src_2', 'snk_2'), 1), (('x_1', 'x_1'), 1), (('x_2', 'x_2'), 1))] = None
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'src_2'), 1), (('src_1', 'snk_2'), 1), (('src_2', 'snk_1'), 1), (('x_1', 'x_1'), 1), (('x_2', 'x_2'), 1))] = None
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'src_2'), 1), (('src_1', 'snk_1'), 1), (('src_2', 'snk_2'), 1), (('x_1', 'x_2'), 1), (('x_2', 'x_1'), 1))] = None
+        diagram_type_dict[((('snk_1', 'src_1'), 1), (('snk_2', 'src_2'), 1), (('src_1', 'snk_2'), 1), (('src_2', 'snk_1'), 1), (('x_1', 'x_2'), 1), (('x_2', 'x_1'), 1))] = None
+        jj_list = [
+                sum([ mk_j_mu("x_2", mu) * mk_j_mu("x_1", mu) for mu in range(4) ])
+                + "j_mu(x) * j_mu(0)",
+                #
+                mk_j_mu("x_2", 3) * mk_j_mu("x_1", 3)
+                + "j_t(x) * j_t(0)",
+                #
+                sum([ mk_j_mu("x_2", mu) * mk_j_mu("x_1", mu) for mu in range(3) ])
+                + "j_i(x) * j_i(0)",
+                #
+                sum([
+                    mk_fac(f"rel_mod_sym(x_2[1][{mu}] - x_1[1][{mu}], size[{mu}])")
+                    * mk_fac(f"rel_mod_sym(x_2[1][{nu}] - x_1[1][{nu}], size[{nu}])")
+                    * mk_j_mu("x_2", mu) * mk_j_mu("x_1", nu)
+                    for mu in range(3) for nu in range(3) ])
+                + "x[i] * x[j] * j_i(x) * j_j(0)",
+                #
+                sum([
+                    mk_fac(f"rel_mod_sym(x_2[1][{mu}] - x_1[1][{mu}], size[{mu}])")
+                    * mk_j_mu("x_2", mu) * mk_j_mu("x_1", 3)
+                    for mu in range(3) ])
+                + "x[i] * j_i(x) * j_t(0)",
+                #
+                sum([
+                    mk_fac(f"rel_mod_sym(x_1[1][{mu}] - x_2[1][{mu}], size[{mu}])")
+                    * mk_j_mu("x_1", mu) * mk_j_mu("x_2", 3)
+                    for mu in range(3) ])
+                + "-x[i] * j_i(-x) * j_t(0)",
+                ]
+        assert len(jj_list) == 6
+        m2_list = [
+                mk_m("u", "x_2") + "ubar_u(x)",
+                mk_m("d", "x_2") + "dbar_d(x)",
+                mk_m("s", "x_2") + "sbar_s(x)",
+                ]
+        assert len(m2_list) == 3
+        m1_list = [
+                mk_m("u", "x_1") + "ubar_u(0)",
+                mk_m("d", "x_1") + "dbar_d(0)",
+                mk_m("s", "x_1") + "sbar_s(0)",
+                ]
+        assert len(m1_list) == 3
+        m1m2_list = [ m2 * m1 for m2 in m2_list for m1 in m1_list ]
+        assert len(m1m2_list) == 9
+        op_list = jj_list + m1m2_list
+        assert len(op_list) == 15
+        mm_list = [
+                mk_pipi_i22("snk_1", "snk_2", True) * mk_pipi_i22("src_1", "src_2") + f"pipi_i22+^dag(0) * pipi_i22(-tsep)",
+                ]
+        exprs_self_energy = [ op * mm for mm in mm_list for op in op_list ]
+        assert len(exprs_self_energy) == 15
+        exprs = [ mk_fac(1) + f"1", ]
+        exprs += exprs_self_energy
+        assert len(exprs) == 16
+        cexpr = contract_simplify_compile(*exprs, is_isospin_symmetric_limit=True, diagram_type_dict=diagram_type_dict)
+        return cexpr
+    return cache_compiled_cexpr(calc_cexpr, fn_base, is_cython=is_cython)
+
+@q.timer_verbose
+def auto_contract_pipi_jj(job_tag, traj, get_get_prop, get_psel_prob, get_fsel_prob):
+    fname = q.get_fname()
+    fn = f"{job_tag}/auto-contract/traj-{traj}/pipi_jj.lat"
+    if get_load_path(fn) is not None:
+        return
+    cexpr = get_cexpr_pipi_jj()
+    expr_names = get_expr_names(cexpr)
+    total_site = q.Coordinate(get_param(job_tag, "total_site"))
+    t_size = total_site[3]
+    get_prop = get_get_prop()
+    psel_prob = get_psel_prob()
+    fsel_prob = get_fsel_prob()
+    psel = psel_prob.psel
+    fsel = fsel_prob.fsel
+    if not fsel.is_containing(psel):
+        q.displayln_info(-1, f"WARNING: fsel is not containing psel. The probability weighting may be wrong.")
+    fsel_n_elems = fsel.n_elems
+    fsel_prob_arr = fsel_prob[:].ravel()
+    psel_prob_arr = psel_prob[:].ravel()
+    xg_fsel_arr = fsel.to_psel_local()[:]
+    xg_psel_arr = psel[:]
+    geo = q.Geometry(total_site)
+    total_volume = geo.total_volume
+    pipi_op_t_sep = get_param(job_tag, "measurement", "pipi_op_t_sep")
+    pipi_tensor_t_sep_list = get_param(job_tag, "measurement", "pipi_tensor_t_sep_list")
+    pipi_tensor_t_max = get_param(job_tag, "measurement", "pipi_tensor_t_max")
+    pipi_tensor_r_max = get_param(job_tag, "measurement", "pipi_tensor_r_max")
+    r_list = get_r_list(job_tag)
+    r_sq_interp_idx_coef_list = get_r_sq_interp_idx_coef_list(job_tag)
+    def load_data():
+        for pidx in range(len(xg_psel_arr)):
+            yield pidx
+    @q.timer
+    def feval(args):
+        pidx = args
+        xg_src = tuple(xg_psel_arr[pidx])
+        prob_src = psel_prob_arr[pidx]
+        res_list = []
+        for idx in range(len(xg_fsel_arr)):
+            xg_snk = tuple(xg_fsel_arr[idx])
+            if xg_snk == xg_src:
+                prob_snk = 1.0
+            else:
+                prob_snk = fsel_prob_arr[idx]
+            prob = prob_src * prob_snk
+            x_rel = [ q.rel_mod(xg_snk[mu] - xg_src[mu], total_site[mu]) for mu in range(4) ]
+            x_rel_t = x_rel[3]
+            r_sq = q.get_r_sq(x_rel)
+            r_norm = np.sqrt(r_sq)
+            t_norm = abs(x_rel_t)
+            if t_norm > pipi_tensor_t_max:
+                continue
+            if r_norm > pipi_tensor_r_max:
+                continue
+            r_idx_low, r_idx_high, coef_low, coef_high = r_sq_interp_idx_coef_list[r_sq]
+            x_2_t = xg_src[3]
+            x_1_t = x_2_t + x_rel_t
+            val = np.zeros((len(pipi_tensor_t_sep_list), len(pipi_tensor_t_sep_list), len(expr_names),), dtype=complex)
+            for t_sep_1_idx, t_sep_1 in enumerate(pipi_tensor_t_sep_list):
+                for t_sep_2_idx, t_sep_2 in enumerate(pipi_tensor_t_sep_list):
+                    t_snk = (max(x_1_t, x_2_t) + t_sep_2) % total_site[3]
+                    t_src = (min(x_1_t, x_2_t) - t_sep_1) % total_site[3]
+                    pd = {
+                            "x_2" : ("point-snk", xg_snk,),
+                            "x_1" : ("point", xg_src,),
+                            "snk_1" : ("wall", t_snk,),
+                            "snk_2" : ("wall", (t_snk + pipi_op_t_sep) % t_size,),
+                            "src_1" : ("wall", t_src,),
+                            "src_2" : ("wall", (t_src - pipi_op_t_sep) % t_size,),
+                            "size" : total_site,
+                            }
+                    t = x_rel_t % t_size
+                    val[t_sep_1_idx, t_sep_2_idx] = eval_cexpr(cexpr, positions_dict=pd, get_prop=get_prop)
+            res_list.append((val / prob, t, r_idx_low, r_idx_high, coef_low, coef_high,))
+        return res_list
+    def sum_function(val_list):
+        values = np.zeros((
+            t_size,
+            len(r_list),
+            len(pipi_tensor_t_sep_list),
+            len(pipi_tensor_t_sep_list),
+            len(expr_names),
+            ), dtype=complex)
+        for idx, res_list in enumerate(val_list):
+            for val, t, r_idx_low, r_idx_high, coef_low, coef_high in res_list:
+                values[t, r_idx_low] += coef_low * val
+                values[t, r_idx_high] += coef_high * val
+            q.displayln_info(f"{fname}: {idx+1}/{len(xg_psel_arr)}")
+        return q.glb_sum(values.transpose(4, 2, 3, 0, 1))
+    q.timer_fork(0)
+    res_sum = q.parallel_map_sum(feval, load_data(), sum_function=sum_function, chunksize=1)
+    q.displayln_info(f"{fname}: timer_display for parallel_map_sum")
+    q.timer_display()
+    q.timer_merge()
+    res_sum *= 1.0 / total_volume
+    ld_sum = q.mk_lat_data([
+        [ "expr_name", len(expr_names), expr_names, ],
+        [ "t_sep_1", len(pipi_tensor_t_sep_list), pipi_tensor_t_sep_list, ],
+        [ "t_sep_2", len(pipi_tensor_t_sep_list), pipi_tensor_t_sep_list, ],
         [ "t", t_size, [ str(q.rel_mod(t, t_size)) for t in range(t_size) ], ],
         [ "r", len(r_list), [ f"{r:.5f}" for r in r_list ], ],
         ])
@@ -574,6 +769,7 @@ def run_job_contract(job_tag, traj):
                 auto_contract_meson_corr(job_tag, traj, get_get_prop, get_psel_prob, get_fsel_prob)
                 auto_contract_meson_jj(job_tag, traj, get_get_prop, get_psel_prob, get_fsel_prob)
                 auto_contract_pipi_corr(job_tag, traj, get_get_prop, get_psel_prob, get_fsel_prob)
+                auto_contract_pipi_jj(job_tag, traj, get_get_prop, get_psel_prob, get_fsel_prob)
                 #
                 q.qtouch_info(get_save_path(fn_checkpoint))
                 q.displayln_info("timer_display for runjob")
@@ -594,7 +790,7 @@ def get_all_cexpr():
 ### ------
 
 set_param("test-4nt8", "trajs")(list(range(1000, 1001)))
-set_param("test-4nt8", "measurement", "meson_tensor_tsep")(1)
+set_param("test-4nt8", "measurement", "meson_tensor_t_sep")(1)
 set_param("test-4nt8", "measurement", "auto_contractor_chunk_size")(2)
 
 set_param("test-4nt8", "mk_sample_gauge_field", "rand_n_step")(2)
@@ -627,15 +823,15 @@ set_param("test-4nt8", "fermion_params", 1, 2, "Ls")(8)
 set_param("test-4nt8", "fermion_params", 2, 2, "Ls")(8)
 
 set_param("24D", "trajs")([ 2430, 2550, 2590, 2610, 2630, 2940, 2960, ])
-set_param("24D", "measurement", "meson_tensor_tsep")(8)
+set_param("24D", "measurement", "meson_tensor_t_sep")(8)
 set_param("24D", "measurement", "auto_contractor_chunk_size")(128)
 
 set_param("48I", "trajs")(list(range(1000, 2000, 20)))
-set_param("48I", "measurement", "meson_tensor_tsep")(12)
+set_param("48I", "measurement", "meson_tensor_t_sep")(12)
 set_param("48I", "measurement", "auto_contractor_chunk_size")(128)
 
 set_param("64I", "trajs")(list(range(1200, 3000, 40)))
-set_param("64I", "measurement", "meson_tensor_tsep")(18)
+set_param("64I", "measurement", "meson_tensor_t_sep")(18)
 set_param("64I", "measurement", "auto_contractor_chunk_size")(128)
 
 # ----
@@ -692,9 +888,12 @@ set_param(job_tag, "prob_acc_1_psrc")(0.25)
 set_param(job_tag, "prob_acc_2_psrc")(0.10)
 
 set_param(job_tag, "measurement", "auto_contractor_chunk_size")(2)
-set_param(job_tag, "measurement", "meson_tensor_tsep")(1)
+set_param(job_tag, "measurement", "meson_tensor_t_sep")(1)
 set_param(job_tag, "measurement", "pipi_op_t_sep")(1)
 set_param(job_tag, "measurement", "pipi_corr_t_sep_list")([ 1, 2, 3, 4, ])
+set_param(job_tag, "measurement", "pipi_tensor_t_sep_list")([ 1, 2, 3, ])
+set_param(job_tag, "measurement", "pipi_tensor_t_max")(3)
+set_param(job_tag, "measurement", "pipi_tensor_r_max")(4)
 
 # ----
 
