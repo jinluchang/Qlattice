f9e90563ddd2c9978c9ca3fce8977c0978101227
Luchang Jin <ljin.luchang@gmail.com>
2025-03-07 16:00:49 -0500

    small fix

--- nixpkgs/q-pkgs-24-05.nix
+++
@@ -1,8 +0,0 @@
-{
-  nixpkgs ? import ./nixpkgs.nix { version = "24.05"; },
-  ngpu ? null, # adjust with desired number of GPUs. E.g. "2"
-  cudaCapability ? null, # adjust with desired cudaCapability. E.g. "8.6"
-  cudaForwardCompat ? null, # adjust with desired cudaForwardCompat. E.g. false
-}@args:
-
-import ./q-pkgs.nix { inherit nixpkgs ngpu cudaCapability cudaForwardCompat; }
