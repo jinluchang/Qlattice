5a487192f3098c341a5075f9642f163c277dcc45
Luchang Jin <ljin.luchang@gmail.com>
2025-03-10 21:10:33 -0400

    small

--- examples-py-gpt/gpt-qlat-data-gen-eta-c.py
+++ examples-py-gpt/gpt-qlat-data-gen-eta-c.py
@@ -319,14 +319,20 @@ set_param("16IH2", "trajs")(list(range(1000, 4020, 10)))
 set_param("16IH2", "measurement", "auto_contractor_chunk_size")(128)
 set_param("16IH2", "measurement", "charm_quark_mass_list")([ 0.04, 0.05, 0.1, 0.15, 0.2, ])
 set_param("16IH2", "measurement", "num_charm_wall_src")(2)
+set_param("16IH2", f"cg_params-2-2", "maxiter")(200)
+set_param("16IH2", f"cg_params-2-2", "maxcycle")(50)
 
 set_param("24D", "trajs")(list(range(1000, 4000, 10)))
 set_param("24D", "measurement", "charm_quark_mass_list")([ 0.0850, 0.05, 0.1, 0.15, 0.2, ])
 set_param("24D", "measurement", "num_charm_wall_src")(2)
+set_param("24D", f"cg_params-2-2", "maxiter")(200)
+set_param("24D", f"cg_params-2-2", "maxcycle")(50)
 
 set_param("32Dfine", "trajs")(list(range(1000, 4000, 10)))
 set_param("32Dfine", "measurement", "charm_quark_mass_list")([ 0.045, 0.05, 0.1, 0.15, 0.2, ])
 set_param("32Dfine", "measurement", "num_charm_wall_src")(2)
+set_param("32Dfine", f"cg_params-2-2", "maxiter")(200)
+set_param("32Dfine", f"cg_params-2-2", "maxcycle")(50)
 
 # ----
 
