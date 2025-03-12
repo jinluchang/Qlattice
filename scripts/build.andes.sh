8cef364c048d520792edfab1452ecba40dd5c831
Luchang Jin <ljin.luchang@gmail.com>
2025-03-12 19:05:27 -0400

    add scripts

---
+++ scripts/build.andes.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e
+
+{
+    ./scripts/setenv.andes.sh
+    ./scripts/qcore.sh
+
+    ./scripts/gsl.sh
+    ./scripts/cuba.sh
+    ./scripts/zlib.sh
+    ./scripts/eigen.sh
+    ./scripts/openssl.sh
+    ./scripts/libffi.sh
+    ./scripts/hdf5.sh
+
+    ./scripts/python.sh
+    ./scripts/python-pip.sh
+    ./scripts/re2c.sh
+    ./scripts/ninja.sh
+    ./scripts/ninja-script.sh
+    ./scripts/python-meson.sh
+    ./scripts/python-meson-py.sh
+    ./scripts/python-packages.sh
+
+    ./scripts/autoconf.sh
+    ./scripts/automake.sh
+    ./scripts/gmp.sh
+    ./scripts/mpfr.sh
+    ./scripts/fftw.sh
+    ./scripts/c-lime.sh
+
+    ./scripts/qmp.sh
+    ./scripts/qio.sh
+    ./scripts/cps.sh
+
+    ./scripts/grid-clehner.avx2.sh
+    ./scripts/gpt.sh
+
+    ./scripts/qlat-utils.sh
+    ./scripts/qlat.sh
+    ./scripts/qlat-grid.sh
+    ./scripts/qlat-cps.sh
+
+    date
+} 2>&1 | tee $prefix/log.build.txt
---
+++ scripts/setenv.andes.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+name=setenv-andes
+
+source qcore/set-prefix.sh
+
+{ time {
+    echo "!!!! build $name !!!!"
+    source qcore/conf.sh
+
+#
+cat >"$prefix/setenv.sh" <<EOF
+if [ -z "\$num_proc" ] ; then
+    export num_proc=8
+fi
+# module purge
+module load gcc/14.2.0
+module load openmpi/4.1.2
+module list
+if [ -z "\$USE_COMPILER" ] ; then
+    export USE_COMPILER=gcc
+fi
+EOF
+
+    #
+    "$wd"/qcore/bin/mk-setenv-dir.sh --keep
+    echo "!!!! $name build !!!!"
+} } 2>&1 | tee $prefix/log.$name.txt
