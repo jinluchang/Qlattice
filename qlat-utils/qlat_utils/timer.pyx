b60c6209196af4ceb7a0e42e7823a58c11971f48
Luchang Jin <ljin.luchang@gmail.com>
2025-05-07 12:34:35 -0400

    small

--- qlat-utils/qlat_utils/timer.pyx
+++ qlat-utils/qlat_utils/timer.pyx
@@ -57,7 +57,7 @@ def display(level, *args):
     """
     if isinstance(level, int):
         if level <= cc.get_verbose_level():
-            print(*args, end='', flush=True)
+            print(f"Qlat {q.get_total_time():15.3f}:", *args, end='', flush=True)
     else:
         print(level, *args, end='', flush=True)
 
@@ -79,7 +79,7 @@ def displayln(level, *args):
         if level <= cc.get_verbose_level():
             print(f"Qlat {q.get_total_time():15.3f}:", *args, flush=True)
     else:
-        print(f"Qlat {q.get_total_time():15.3f}:", level, *args, flush=True)
+        print(level, *args, flush=True)
 
 def displayln_info(*args):
     """
