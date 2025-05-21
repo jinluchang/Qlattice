90144c41ac5d43258d5a70e19786de512ff1da09
Luchang Jin <ljin.luchang@gmail.com>
2025-05-21 01:01:28 -0400

    small fix

--- qlat/qlat/lib/selected-shuffle.cpp
+++ qlat/qlat/lib/selected-shuffle.cpp
@@ -209,12 +209,16 @@ void shuffle_selected_points_char(SelectedPoints<Char>& spc,
   {
     TIMER("shuffle_selected_points_char(spc,spc0,ssp)-mpi");
     const MpiDataType& mpi_dtype = get_mpi_data_type_contiguous(multiplicity);
+    vector<Char> send_buffer(sp0.points.size());
+    vector<Char> recv_buffer(sp.points.size());
+    assign(send_buffer, sp0.points);
     {
       TIMER("shuffle_selected_points_char(spc,spc0,ssp)-MPI_Alltoallv");
-      mpi_alltoallv(sp0.points.data(), ssp.sendcounts.data(), ssp.sdispls.data(),
-          mpi_dtype.mpi_dtype, sp.points.data(), ssp.recvcounts.data(),
+      mpi_alltoallv(send_buffer.data(), ssp.sendcounts.data(), ssp.sdispls.data(),
+          mpi_dtype.mpi_dtype, recv_buffer.data(), ssp.recvcounts.data(),
           ssp.rdispls.data(), mpi_dtype.mpi_dtype, get_comm());
     }
+    assign(sp.points, recv_buffer);
   }
   // In case `ssp.shuffle_idx_points_recv` is defined, perform final reordering.
   if (spi_r.initialized) {
