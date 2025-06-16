b8eaa905cb1cb49db415b848588a4fc4980f09c9
Luchang Jin <ljin.luchang@gmail.com>
2025-06-16 12:08:46 -0400

    small

---
+++ nixpkgs/install-py-local-kernel-with-nix-cuda.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+script_path="$( cd -- "$(dirname -- "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+if [ -f "$script_path"/q-pkgs.nix ]; then
+    :
+else
+    echo "Need to place the script inside its original directory, which also have the file q-pkgs.nix and other files."
+    exit 1
+fi
+
+time (
+  time name="" "$script_path"/install-py-local-kernel-with-nix.sh "$@"
+  time name="-cu" "$script_path"/install-py-local-kernel-with-nix.sh "$@"
+  time name="-cuda" "$script_path"/install-py-local-kernel-with-nix.sh "$@"
+  time name="-cudasupport" "$script_path"/install-py-local-kernel-with-nix.sh "$@"
+)
