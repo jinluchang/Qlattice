1d17dce34f8121bb7616e8f81ea8f12f8f7da443
Luchang Jin <ljin.luchang@gmail.com>
2025-06-18 13:46:02 -0400

    small fix

--- qlat/qlat_scripts/v1/jobs.py
+++ qlat/qlat_scripts/v1/jobs.py
@@ -616,7 +616,7 @@ def compute_eig(job_tag, gf, inv_type=0, inv_acc=0, *, path=None, pc_ne=None):
     g.mem_report()
     # evec, evals = ru.mk_eig(job_tag, gf, inv_type, inv_acc)
     basis, cevec, smoothed_evals = ru.mk_ceig(job_tag, gf, inv_type, inv_acc, pc_ne=pc_ne)
-    eig = [ basis, cevec, smoothed_evals, ]
+    eig = (basis, cevec, smoothed_evals,)
     ru.save_ceig(get_save_path(path + ".partial"), eig, job_tag, inv_type, inv_acc);
     q.qrename_info(get_save_path(path + ".partial"), get_save_path(path))
     test_eig(gf, eig, job_tag, inv_type)
