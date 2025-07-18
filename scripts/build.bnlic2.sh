91fc54b6780d31b02656744728554325a09ebfe3
Luchang Jin <ljin.luchang@gmail.com>
2025-07-18 01:10:12 -0400

    add bnlic2

---
+++ scripts/build.bnlic2.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -e
+
+{
+    ./scripts/setenv.bnlic2.sh
+    ./scripts/qcore.sh
+
+    export USE_COMPILER=clang
+
+    ./scripts/tar.sh
+    ./scripts/zlib.sh
+    ./scripts/openssl.sh
+    ./scripts/libffi.sh
+    ./scripts/openblas.sh
+    ./scripts/hdf5.sh
+    ./scripts/python.sh
+    ./scripts/python-pip.sh
+    ./scripts/re2c.sh
+    ./scripts/ninja.sh
+    ./scripts/ninja-script.sh
+    ./scripts/python-meson.sh
+    ./scripts/python-meson-py.sh
+    ./scripts/python-packages.sh
+
+    ./scripts/fftw.sh
+    ./scripts/cuba.sh
+    ./scripts/eigen.sh
+
+    ./scripts/automake.sh
+    ./scripts/autoconf.sh
+
+    ./scripts/qmp.sh
+    ./scripts/qio.sh
+    ./scripts/cps.sh
+
+    # ./scripts/c-lime.sh
+    ./scripts/grid-clehner.avx512.sh
+    ./scripts/gpt.sh
+
+    ./scripts/qlat-utils.sh
+    ./scripts/qlat.sh
+    ./scripts/qlat-cps.sh
+    ./scripts/qlat-grid.sh
+
+    ./scripts/qlat-examples-py.sh
+    ./scripts/qlat-examples-cpp.sh
+    ./scripts/qlat-examples-py-gpt.sh
+    ./scripts/qlat-examples-py-cps.sh
+    ./scripts/qlat-examples-cpp-grid.sh
+
+    date
+} 2>&1 | tee $prefix/log.build.txt
---
+++ scripts/setenv.bnlic2.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+name=setenv-bnlic2
+
+source qcore/set-prefix.sh
+
+{ time {
+    echo "!!!! build $name !!!!"
+    source qcore/conf.sh
+
+#
+cat >"$prefix/setenv.sh" <<EOF
+if [ -z "\$num_proc" ] ; then
+    export num_proc=8
+fi
+export PYTHONPATH=
+module purge
+module add openmpi
+module add llvm
+module list
+EOF
+
+    #
+    "$wd"/qcore/bin/mk-setenv-dir.sh --keep
+    echo "!!!! $name build !!!!"
+} } 2>&1 | tee $prefix/log.$name.txt
