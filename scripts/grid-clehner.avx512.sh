382c76898a175865345f917c6223c4b08466be03
Luchang Jin <ljin.luchang@gmail.com>
2025-07-19 22:50:04 -0400

    small

--- scripts/grid-clehner.avx512.sh
+++ scripts/grid-clehner.avx512.sh
@@ -28,8 +28,7 @@ source qcore/set-prefix.sh $name
         ln -vs "${INITDIR}/Eigen/unsupported/Eigen" "${INITDIR}/Grid/Eigen/unsupported"
     fi
 
-    # export CXXFLAGS="$CXXFLAGS -fPIC -std=c++17 -w -Wno-psabi -mfma"
-    export CXXFLAGS="$CXXFLAGS -fPIC -std=c++17 -w -Wno-psabi"
+    export CXXFLAGS="$CXXFLAGS -fPIC -std=c++17 -w -Wno-psabi -mfma"
 
     opts=""
     if [ -n "$(find-library.sh libgmp.a)" ] ; then
