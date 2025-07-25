630abcfead0dff06683cf0f186de6689becd5312
Luchang Jin <ljin.luchang@gmail.com>
2025-07-25 12:40:57 -0400

    small fix

--- examples-py-gpt/gpt-qlat-data-gen-pipi-qed.py
+++ examples-py-gpt/gpt-qlat-data-gen-pipi-qed.py
@@ -12,7 +12,7 @@ import qlat_gpt as qg
 from qlat_scripts.v1 import *
 from auto_contractor.operators import *
 
-is_cython = False
+is_cython = not is_test()
 
 # ----
 
@@ -1066,7 +1066,7 @@ def run_auto_contraction(
 def run_job_inversion(job_tag, traj):
     #
     traj_gf = traj
-    if job_tag[:5] == "test-":
+    if is_test():
         # ADJUST ME
         traj_gf = 1000
         #
@@ -1158,7 +1158,7 @@ def run_job_contraction(job_tag, traj):
     use_fsel_prop = get_param(job_tag, "measurement", "use_fsel_prop", default=True)
     #
     traj_gf = traj
-    if job_tag[:5] == "test-":
+    if is_test():
         # ADJUST ME
         traj_gf = 1000
         #
@@ -1363,59 +1363,64 @@ set_param(job_tag, "measurement", "pipi_tensor_r_max")(4)
 
 # ----
 
+##################### CMD options #####################
+
+job_tag_list_default = [
+        "test-4nt8-checker",
+        ]
+job_tag_list_str_default = ",".join(job_tag_list_default)
+job_tag_list = q.get_arg("--job_tag_list", default=job_tag_list_str_default).split(",")
+
+is_performing_inversion = not q.get_option("--no-inversion")
+
+is_performing_contraction = not q.get_option("--no-contraction")
+
+#######################################################
+
 def gracefully_finish():
+    q.displayln_info("Begin to gracefully_finish.")
     q.timer_display()
+    if is_test():
+        q.json_results_append(f"q.obtained_lock_history_list={q.obtained_lock_history_list}")
+        q.check_log_json(__file__)
     qg.end_with_gpt()
     q.displayln_info("CHECK: finished successfully.")
     exit()
 
+def try_gracefully_finish():
+    """
+    Call `gracefully_finish` if not test and if some work is done (q.obtained_lock_history_list != [])
+    """
+    if (not is_test()) and (len(q.obtained_lock_history_list) > 0):
+        gracefully_finish()
+
 if __name__ == "__main__":
 
     qg.begin_with_gpt()
-
-    ##################### CMD options #####################
-
-    job_tag_list = q.get_arg("--job_tag_list", default="").split(",")
-
-    is_performing_inversion = not q.get_option("--no-inversion")
-
-    is_performing_contraction = not q.get_option("--no-contraction")
-
-    #######################################################
-
-    job_tag_list_default = [
-            "test-4nt8-checker",
-            ]
-
-    if job_tag_list == [ "", ]:
-        job_tag_list = job_tag_list_default
-    else:
-        is_cython = True
-
     q.check_time_limit()
-
     get_all_cexpr()
 
+    job_tag_traj_list = []
     for job_tag in job_tag_list:
         run_params(job_tag)
-        for traj in get_param(job_tag, "traj_list"):
-            if is_performing_inversion:
-                q.check_time_limit()
-                run_job_inversion(job_tag, traj)
-                if q.obtained_lock_history_list:
-                    q.json_results_append(f"q.obtained_lock_history_list={q.obtained_lock_history_list}")
-                    if job_tag[:5] != "test-":
-                        gracefully_finish()
-        for traj in get_param(job_tag, "traj_list"):
-            if is_performing_contraction:
-                q.check_time_limit()
-                run_job_contraction(job_tag, traj)
-                if q.obtained_lock_history_list:
-                    q.json_results_append(f"q.obtained_lock_history_list={q.obtained_lock_history_list}")
-                    if job_tag[:5] != "test-":
-                        gracefully_finish()
-
-    q.check_log_json(__file__, check_eps=5e-5)
+        traj_list = get_param(job_tag, "traj_list")
+        for traj in traj_list:
+            job_tag_traj_list.append((job_tag, traj,))
+    if not is_test():
+        # job_tag_traj_list = q.random_permute(job_tag_traj_list, q.RngState(f"{q.get_time()}"))
+        job_tag_traj_list = q.get_comm().bcast(job_tag_traj_list)
+    for job_tag, traj in job_tag_traj_list:
+        if is_performing_inversion:
+            q.check_time_limit()
+            run_job_inversion(job_tag, traj)
+            q.clean_cache()
+            try_gracefully_finish()
+    for job_tag, traj in job_tag_traj_list:
+        if is_performing_contraction:
+            q.check_time_limit()
+            run_job_contraction(job_tag, traj)
+            q.clean_cache()
+            try_gracefully_finish()
 
     gracefully_finish()
 
