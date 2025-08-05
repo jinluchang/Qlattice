bfa34231f898b998ee91670b15b574de93212ce6
Luchang Jin <ljin.luchang@gmail.com>
2025-08-04 22:41:32 -0400

    small

---
+++ nixpkgs/build-many-qlat-pkgs-cuda-small.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+script_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+if [ -f "$script_path"/q-pkgs.nix ]; then
+    :
+else
+    echo "Need to run the script inside its original directory, which also have the file q-pkgs.nix and other files."
+    exit 1
+fi
+
+set -ex
+
+src="$script_path"
+dst="$HOME/qlat-build/nix"
+mkdir -p "$dst"
+cd "$dst"
+
+nix_version_list=(
+    "25.05"
+    "24.11"
+)
+
+name_list=(
+    q-pkgs
+    q-pkgs-cudasupport
+)
+
+time (
+
+    for nix_version in "${nix_version_list[@]}" ; do
+        build_list=()
+        for name in "${name_list[@]}" ; do
+            build_list+=("-A" "$name.qlat-tests")
+            build_list+=("-A" "$name.qlat-env")
+        done
+        time nix-build "$src"/q-pkgs.nix "${build_list[@]}" --no-out-link --argstr version "$nix_version" -k "$@"
+        for name in "${name_list[@]}" ; do
+            echo
+            echo "Building $nix_version $name"
+            echo
+            time nix-build "$src"/q-pkgs.nix -A "$name".qlat-tests -A "$name".qlat-env -o result-"$nix_version-$name" --argstr version "$nix_version" "$@" &
+        done
+        time wait
+    done
+
+)
