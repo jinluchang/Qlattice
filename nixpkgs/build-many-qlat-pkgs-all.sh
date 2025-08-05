8cefa8011ea9db77f9a95a496dd6fd202851f435
Luchang Jin <ljin.luchang@gmail.com>
2025-08-04 22:41:17 -0400

    small

--- nixpkgs/build-many-qlat-pkgs-all.sh
+++ nixpkgs/build-many-qlat-pkgs-all.sh
@@ -18,20 +18,20 @@ cd "$dst"
 
 time (
 
-for nix_version in "25.05" "24.11" ; do
-    time nix-build "$src"/q-pkgs.nix -A qlat-name-list-file -o result-qlat-name-"$nix_version" --argstr version "$nix_version" "$@"
-    echo
-    echo result-qlat-name-"$nix_version"
-    echo
-    cat result-qlat-name-"$nix_version"
-    yes q-pkgs | head -n "$(cat result-qlat-name-"$nix_version" | wc -l)" | paste -d '' - result-qlat-name-"$nix_version" >q-pkgs-list-"$nix_version".txt
-    echo
-    for name in $(cat q-pkgs-list-"$nix_version".txt) ; do
+    for nix_version in "25.05" "24.11" ; do
+        time nix-build "$src"/q-pkgs.nix -A qlat-name-list-file -o result-qlat-name-"$nix_version" --argstr version "$nix_version" "$@"
         echo
-        echo "Building $nix_version $name"
+        echo result-qlat-name-"$nix_version"
         echo
-        time nix-build "$src"/q-pkgs.nix -A "$name".qlat-tests -A "$name".qlat-env -o result-"$nix_version-$name" --argstr version "$nix_version" "$@"
+        cat result-qlat-name-"$nix_version"
+        yes q-pkgs | head -n "$(cat result-qlat-name-"$nix_version" | wc -l)" | paste -d '' - result-qlat-name-"$nix_version" >q-pkgs-list-"$nix_version".txt
+        echo
+        for name in $(cat q-pkgs-list-"$nix_version".txt) ; do
+            echo
+            echo "Building $nix_version $name"
+            echo
+            time nix-build "$src"/q-pkgs.nix -A "$name".qlat-tests -A "$name".qlat-env -o result-"$nix_version-$name" --argstr version "$nix_version" "$@"
+        done
     done
-done
 
 )
--- nixpkgs/build-many-qlat-pkgs-cuda.sh
+++ nixpkgs/build-many-qlat-pkgs-cuda.sh
@@ -17,35 +17,35 @@ mkdir -p "$dst"
 cd "$dst"
 
 nix_version_list=(
-  "25.05"
-  "24.11"
+    "25.05"
+    "24.11"
 )
 
 name_list=(
-  q-pkgs
-  q-pkgs-clang
-  q-pkgs-cu
-  q-pkgs-cudasupport
-  q-pkgs-ucxless
-  q-pkgs-pypi
+    q-pkgs
+    q-pkgs-clang
+    q-pkgs-cu
+    q-pkgs-cudasupport
+    q-pkgs-ucxless
+    q-pkgs-pypi
 )
 
 time (
 
-  for nix_version in "${nix_version_list[@]}" ; do
-    build_list=()
-    for name in "${name_list[@]}" ; do
-      build_list+=("-A" "$name.qlat-tests")
-      build_list+=("-A" "$name.qlat-env")
+    for nix_version in "${nix_version_list[@]}" ; do
+        build_list=()
+        for name in "${name_list[@]}" ; do
+            build_list+=("-A" "$name.qlat-tests")
+            build_list+=("-A" "$name.qlat-env")
+        done
+        time nix-build "$src"/q-pkgs.nix "${build_list[@]}" --no-out-link --argstr version "$nix_version" -k "$@"
+        for name in "${name_list[@]}" ; do
+            echo
+            echo "Building $nix_version $name"
+            echo
+            time nix-build "$src"/q-pkgs.nix -A "$name".qlat-tests -A "$name".qlat-env -o result-"$nix_version-$name" --argstr version "$nix_version" "$@" &
+        done
+        time wait
     done
-    time nix-build "$src"/q-pkgs.nix "${build_list[@]}" --no-out-link --argstr version "$nix_version" -k "$@"
-    for name in "${name_list[@]}" ; do
-      echo
-      echo "Building $nix_version $name"
-      echo
-      time nix-build "$src"/q-pkgs.nix -A "$name".qlat-tests -A "$name".qlat-env -o result-"$nix_version-$name" --argstr version "$nix_version" "$@" &
-    done
-    time wait
-  done
 
 )
