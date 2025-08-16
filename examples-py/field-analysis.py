c2951895cce0f0db5183a63a202ff7f0de5987cf
Luchang Jin <ljin.luchang@gmail.com>
2025-08-16 02:26:20 -0400

    small

--- examples-py/field-analysis.py
+++ examples-py/field-analysis.py
@@ -35,9 +35,13 @@ q.json_results_append("f2 data sig", q.get_data_sig(f2, rs), check_eps)
 radius = 2.0
 sf1 = q.smear_field(f1, radius)
 sf2 = q.smear_field(f2, radius, is_only_spatial=True)
-
 q.json_results_append("sf1 data sig", q.get_data_sig(sf1, rs), check_eps)
 q.json_results_append("sf2 data sig", q.get_data_sig(sf2, rs), check_eps)
+
+sf1p = q.smear_field(f2, radius)
+sf2p = q.smear_field(f1, radius, is_only_spatial=True)
+q.json_results_append("sf1p data sig", q.get_data_sig(sf1p, rs), check_eps)
+q.json_results_append("sf2p data sig", q.get_data_sig(sf2p, rs), check_eps)
 
 idx1 = np.array([ 0, 1, ], dtype=np.int32)
 idx2 = np.array([ 0, 2, ], dtype=np.int32)
