436f0d831b8594753ebf18bf20ea8ca9128dc3af
Luchang Jin <ljin.luchang@gmail.com>
2025-11-24 22:27:28 -0500

    small

--- scripts/setenv.uconn.sh
+++ scripts/setenv.uconn.sh
@@ -23,8 +23,12 @@ module add gcc/12.2.0
 module add openmpi/5.0.6
 module add cuda/12.3
 module list
-# ``OMPI_MCA_pml=ucx'' does not work for mpi4py
-export OMPI_MCA_pml=
+#
+export OMPI_MCA_pml=ucx
+#
+# ``OMPI_MCA_pml=ucx'' does not work for mpi4py (need to turn of threading)
+export MPI4PY_RC_THREADS=0
+#
 EOF
 
     #
