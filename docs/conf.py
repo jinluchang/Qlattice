5a33f096b3dea1bb2f0e447ce380adb8c7d75fa9
Luchang Jin <ljin.luchang@gmail.com>
2025-12-27 00:44:59 -0500

    Upgrade version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Qlattice'
 copyright = 'Luchang Jin'
 author = 'Luchang Jin'
-release = '0.86'
+release = '0.87'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
