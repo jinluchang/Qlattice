6400457447984b44a74c2e75cae8121a882607c5
Luchang Jin <ljin.luchang@gmail.com>
2025-12-26 22:20:58 -0500

    Upgrade version

--- nixpkgs/q-pkgs.nix
+++ nixpkgs/q-pkgs.nix
@@ -18,7 +18,7 @@ let
   nixpkgs-wd = if nixpkgs == null then nixpkgs-default else nixpkgs;
   import-nixpkgs-wd = import nixpkgs-wd;
 
-  version-pypi = "0.84";
+  version-pypi = "0.85";
 
   o-pkgs = import-nixpkgs-wd {
     config.allowUnfree = true;
