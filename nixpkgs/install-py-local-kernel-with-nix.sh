58356faf7dd77cc33a65f99c6db8fe00310ed93e
Luchang Jin <ljin.luchang@gmail.com>
2026-03-30 02:52:14 -0400

    fix: correct build symlink name from build-* to result-*

--- nixpkgs/install-py-local-kernel-with-nix.sh
+++ nixpkgs/install-py-local-kernel-with-nix.sh
@@ -49,8 +49,8 @@ if [ ! -e "$dst"/result/bin/python3 ] ; then
     rmdir "$dst"
     exit 1
 fi
-rm -fv build-$py_kernel_name
-ln -sv "$dst" build-$py_kernel_name
+rm -fv result-$py_kernel_name
+ln -sv "$dst"/result result-$py_kernel_name
 ls -l "$dst"
 if [ "$name" == "-cudasupport" ] ; then
     "$dst"/result/bin/python3 -m ipykernel \
