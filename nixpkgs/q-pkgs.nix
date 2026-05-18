c759db482617296ed9971c654b58e16bff0a6cdf
Luchang Jin <ljin.luchang@gmail.com>
2026-05-18 01:28:06 -0400

    Upgrade version to v1.03

--- nixpkgs/q-pkgs.nix
+++ nixpkgs/q-pkgs.nix
@@ -18,7 +18,7 @@ let
   nixpkgs-wd = if nixpkgs == null then nixpkgs-default else nixpkgs;
   import-nixpkgs-wd = import nixpkgs-wd;
 
-  version-pypi = "1.02";
+  version-pypi = "1.03";
 
   o-pkgs = import-nixpkgs-wd {
     config.allowUnfree = true;
